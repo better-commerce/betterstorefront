f589f232656c0b7d0fb4896d19844d54f75ec136
Adrian Mustea <mustea.adrian95@gmail.com>
2021-10-07 14:05:08 +0300

    feat(layout changes) added header and footer

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -1,53 +1,223 @@
-import { FC } from 'react'
-import Link from 'next/link'
-import s from './Navbar.module.css'
-import NavbarRoot from './NavbarRoot'
-import { Logo, Container } from '@components/ui'
+import { FC, Fragment } from 'react'
+import config from './config'
+import { classNames } from '../../utils'
+import { Popover, Transition } from '@headlessui/react'
+import { SearchIcon, ShoppingBagIcon } from '@heroicons/react/outline'
 import { Searchbar, UserNav } from '@components/common'
+import { Logo } from '@components/ui'
+import Link from 'next/link'
 
-interface Link {
-  href: string
-  label: string
-}
-interface NavbarProps {
-  links?: Link[]
-}
-
-const Navbar: FC<NavbarProps> = ({ links }) => (
-  <NavbarRoot>
-    <Container>
-      <div className={s.nav}>
-        <div className="flex items-center flex-1">
-          <Link href="/">
-            <a className={s.logo} aria-label="Logo">
-              <Logo />
-            </a>
-          </Link>
-          <nav className={s.navMenu}>
-            <Link href="/search">
-              <a className={s.link}>All</a>
-            </Link>
-            {links?.map((l) => (
-              <Link href={l.href} key={l.href}>
-                <a className={s.link}>{l.label}</a>
+const Navbar: FC = ({}) => {
+  return (
+    <div className="bg-white">
+      <header className="relative bg-white">
+        <nav aria-label="Top" className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div className="border-b border-gray-200 px-4 pb-14 sm:px-0 sm:pb-0">
+            <div className="h-16 flex items-center justify-between">
+              {/* Logo */}
+              <Link href="#">
+                <div className="flex-1 flex cursor-pointer">
+                  <span className="sr-only">Workflow</span>
+                  <Logo />
+                </div>
               </Link>
-            ))}
-          </nav>
-        </div>
-        {process.env.COMMERCE_SEARCH_ENABLED && (
-          <div className="justify-center flex-1 hidden lg:flex">
-            <Searchbar />
-          </div>
-        )}
-        <div className="flex items-center justify-end flex-1 space-x-8">
-          <UserNav />
-        </div>
-      </div>
-      <div className="flex pb-4 lg:px-6 lg:hidden">
-        <Searchbar id="mobile-search" />
-      </div>
-    </Container>
-  </NavbarRoot>
-)
 
+              {/* Flyout menus */}
+              <Popover.Group className="absolute bottom-0 inset-x-0 sm:static sm:flex-1 sm:self-stretch">
+                <div className="border-t h-14 px-4 flex space-x-8 overflow-x-auto pb-px sm:h-full sm:border-t-0 sm:justify-center sm:overflow-visible sm:pb-0">
+                  {config.categories.map((category, categoryIdx) => (
+                    <Popover key={categoryIdx} className="flex">
+                      {({ open }) => (
+                        <>
+                          <div className="relative flex">
+                            <Popover.Button
+                              className={classNames(
+                                open
+                                  ? 'border-indigo-600 text-indigo-600'
+                                  : 'border-transparent text-gray-700 hover:text-gray-800',
+                                'relative z-10 flex items-center transition-colors ease-out duration-200 text-sm font-medium border-b-2 -mb-px pt-px'
+                              )}
+                            >
+                              {category.name}
+                            </Popover.Button>
+                          </div>
+                          <Transition
+                            as={Fragment}
+                            enter="transition ease-out duration-200"
+                            enterFrom="opacity-0"
+                            enterTo="opacity-100"
+                            leave="transition ease-in duration-150"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                          >
+                            <Popover.Panel className="absolute top-full z-50 inset-x-0 text-gray-500 sm:text-sm">
+                              {/* Presentational element used to render the bottom shadow, if we put the shadow on the actual panel it pokes out the top, so we use this shorter element to hide the top of the shadow */}
+                              <div
+                                className="absolute inset-0 top-1/2 bg-white shadow"
+                                aria-hidden="true"
+                              />
+
+                              <div className="relative bg-white">
+                                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                                  <div className="grid grid-cols-1 items-start gap-y-10 gap-x-6 pt-10 pb-12 md:grid-cols-2 lg:gap-x-8">
+                                    <div className="grid grid-cols-1 gap-y-10 gap-x-6 lg:gap-x-8">
+                                      <div>
+                                        <p
+                                          id="clothing-heading"
+                                          className="font-medium text-gray-900"
+                                        >
+                                          Clothing
+                                        </p>
+                                        <div className="mt-4 border-t border-gray-200 pt-6 sm:grid sm:grid-cols-2 sm:gap-x-6">
+                                          <ul
+                                            role="list"
+                                            aria-labelledby="clothing-heading"
+                                            className="space-y-6 sm:space-y-4"
+                                          >
+                                            {category.clothing[0].map(
+                                              (item) => (
+                                                <li
+                                                  key={item.name}
+                                                  className="flex"
+                                                >
+                                                  <Link
+                                                    href={item.href}
+                                                    passHref
+                                                  >
+                                                    <a className="hover:text-gray-800">
+                                                      {item.name}
+                                                    </a>
+                                                  </Link>
+                                                </li>
+                                              )
+                                            )}
+                                          </ul>
+                                          <ul
+                                            role="list"
+                                            aria-label="More clothing"
+                                            className="mt-6 space-y-6 sm:mt-0 sm:space-y-4"
+                                          >
+                                            {category.clothing[1].map(
+                                              (item) => (
+                                                <li
+                                                  key={item.name}
+                                                  className="flex"
+                                                >
+                                                  <Link
+                                                    passHref
+                                                    href={item.href}
+                                                  >
+                                                    <a className="hover:text-gray-800">
+                                                      {item.name}
+                                                    </a>
+                                                  </Link>
+                                                </li>
+                                              )
+                                            )}
+                                          </ul>
+                                        </div>
+                                      </div>
+                                    </div>
+                                    <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:gap-x-8">
+                                      <div>
+                                        <p
+                                          id="accessories-heading"
+                                          className="font-medium text-gray-900"
+                                        >
+                                          Accessories
+                                        </p>
+                                        <ul
+                                          role="list"
+                                          aria-labelledby="accessories-heading"
+                                          className="mt-4 border-t border-gray-200 pt-6 space-y-6 sm:space-y-4"
+                                        >
+                                          {category.accessories.map((item) => (
+                                            <li
+                                              key={item.name}
+                                              className="flex"
+                                            >
+                                              <Link href={item.href} passHref>
+                                                <a className="hover:text-gray-800">
+                                                  {item.name}
+                                                </a>
+                                              </Link>
+                                            </li>
+                                          ))}
+                                        </ul>
+                                      </div>
+                                      <div>
+                                        <p
+                                          id="categories-heading"
+                                          className="font-medium text-gray-900"
+                                        >
+                                          Categories
+                                        </p>
+                                        <ul
+                                          role="list"
+                                          aria-labelledby="categories-heading"
+                                          className="mt-4 border-t border-gray-200 pt-6 space-y-6 sm:space-y-4"
+                                        >
+                                          {category.categories.map((item) => (
+                                            <li
+                                              key={item.name}
+                                              className="flex"
+                                            >
+                                              <Link href={item.href} passHref>
+                                                <a className="hover:text-gray-800">
+                                                  {item.name}
+                                                </a>
+                                              </Link>
+                                            </li>
+                                          ))}
+                                        </ul>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </Popover.Panel>
+                          </Transition>
+                        </>
+                      )}
+                    </Popover>
+                  ))}
+
+                  {config.other.map((item) => (
+                    <Link href={item.href} key={item.name} passHref>
+                      <a
+                        href={item.href}
+                        className="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800"
+                      >
+                        {item.name}
+                      </a>
+                    </Link>
+                  ))}
+                </div>
+              </Popover.Group>
+
+              <div className="flex-1 flex items-center justify-end">
+                {/* Search */}
+                {true && <Searchbar />}
+
+                {/* Cart */}
+                <div className="ml-4 flow-root lg:ml-8">
+                  <button className="group -m-2 p-2 flex items-center">
+                    <ShoppingBagIcon
+                      className="flex-shrink-0 h-6 w-6 text-gray-400 group-hover:text-gray-500"
+                      aria-hidden="true"
+                    />
+                    <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
+                      0
+                    </span>
+                    <span className="sr-only">items in cart, view bag</span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </header>
+    </div>
+  )
+}
 export default Navbar
---
+++ components/common/Navbar/config.ts
@@ -0,0 +1,77 @@
+//@TODO to be removed when we connect BE
+const navigation = {
+  categories: [
+    {
+      name: 'Women',
+      clothing: [
+        [
+          { name: 'Tops', href: '#' },
+          { name: 'Dresses', href: '#' },
+          { name: 'Pants', href: '#' },
+          { name: 'Denim', href: '#' },
+          { name: 'Sweaters', href: '#' },
+          { name: 'T-Shirts', href: '#' },
+        ],
+        [
+          { name: 'Jackets', href: '#' },
+          { name: 'Activewear', href: '#' },
+          { name: 'Shorts', href: '#' },
+          { name: 'Swimwear', href: '#' },
+          { name: 'Browse All', href: '#' },
+        ],
+      ],
+      accessories: [
+        { name: 'Shoes', href: '#' },
+        { name: 'Jewelry', href: '#' },
+        { name: 'Handbags', href: '#' },
+        { name: 'Socks', href: '#' },
+        { name: 'Hats', href: '#' },
+        { name: 'Browse All', href: '#' },
+      ],
+      categories: [
+        { name: 'New Arrivals', href: '#' },
+        { name: 'Sale', href: '#' },
+        { name: 'Basic Tees', href: '#' },
+        { name: 'Artwork Tees', href: '#' },
+      ],
+    },
+    {
+      name: 'Men',
+      clothing: [
+        [
+          { name: 'Dress Shirts', href: '#' },
+          { name: 'Pants', href: '#' },
+          { name: 'Jackets', href: '#' },
+          { name: 'T-Shirts', href: '#' },
+          { name: 'Jeans', href: '#' },
+          { name: 'Hoodies', href: '#' },
+        ],
+        [
+          { name: 'Vests', href: '#' },
+          { name: 'Kilts', href: '#' },
+          { name: 'Outdoors', href: '#' },
+          { name: 'Capes', href: '#' },
+          { name: 'Browse All', href: '#' },
+        ],
+      ],
+      accessories: [
+        { name: 'Watches', href: '/product/watches' },
+        { name: 'Boots', href: '#' },
+        { name: 'Fanny Packs', href: '#' },
+        { name: 'Sunglasses', href: '#' },
+        { name: 'Browse All', href: '#' },
+      ],
+      categories: [
+        { name: 'Just Added', href: '#' },
+        { name: 'Clearance', href: '#' },
+        { name: 'Graphic Tees', href: '#' },
+      ],
+    },
+  ],
+  other: [
+    { name: 'Company', href: '#' },
+    { name: 'Stores', href: '#' },
+  ],
+}
+
+export default navigation;
