ab2b1f6b8c464439374e02f27c83bd5e4bde8574
Adrian Mustea <mustea.adrian95@gmail.com>
2021-10-14 15:04:49 +0300

    search page

--- pages/api/catalog/products.ts
+++ pages/api/catalog/products.ts
@@ -2,8 +2,9 @@ import commerce from '@lib/api/commerce'
 
 export default async (req: any, res: any) => {
   try {
-    console.log(req.body)
-    const response = await commerce.getAllProducts()
+    const response = await commerce.getAllProducts({
+      query: JSON.stringify(req.query),
+    })
     res.status(200).json(response)
   } catch (error) {
     res.status(500).json({ error })
