9fe969174bef6875b25026e74c99724a99692954
Adrian Mustea <mustea.adrian95@gmail.com>
2021-10-23 03:03:58 +0300

    related products bug

--- components/product/RelatedProducts/index.tsx
+++ components/product/RelatedProducts/index.tsx
@@ -29,63 +29,63 @@ export default function RelatedProducts({
         Customers also bought
       </h2>
       <div>
-        {computedItems.map((relatedItem: any) => {
+        {computedItems.map((relatedItem: any, relatedItemIdx: number) => {
           return (
-            <>
-              <h2
-                id="related-heading"
-                className="text-xl mt-6 font-bold text-gray-900"
-              >
-                {relatedItem.name}
-              </h2>
+            <div key={`relatedItemIdx-${relatedItemIdx}`}>
+              {relatedItem.relatedProducts && (
+                <h2
+                  id="related-heading"
+                  className="text-xl mt-6 font-bold text-gray-900"
+                >
+                  {relatedItem.name}
+                </h2>
+              )}
               <div className="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
-                {relatedItem.relatedProducts.map((product: any) => (
-                  <div key={product.id}>
-                    <div className="relative">
-                      <div className="relative w-full h-72 rounded-lg overflow-hidden">
-                        <Link href={`/${product.slug}`} passHref>
-                          <img
-                            src={product.image}
-                            alt={product.name}
-                            className="w-full h-full object-center object-cover"
+                {relatedItem.relatedProducts &&
+                  relatedItem.relatedProducts.map((product: any) => (
+                    <div key={product.id}>
+                      <div className="relative">
+                        <div className="relative w-full h-72 rounded-lg overflow-hidden">
+                          <Link href={`/${product.slug}`} passHref>
+                            <img
+                              src={product.image}
+                              alt={product.name}
+                              className="w-full h-full object-center object-cover"
+                            />
+                          </Link>
+                        </div>
+                        <div className="relative mt-4">
+                          <h3 className="text-sm font-medium text-gray-900">
+                            <Link href={`/${product.slug}`} passHref>
+                              <a href={`/${product.slug}`}>{product.name}</a>
+                            </Link>
+                          </h3>
+                        </div>
+                        <div className="absolute top-0 inset-x-0 h-72 rounded-lg p-4 flex items-end justify-end overflow-hidden">
+                          <div
+                            aria-hidden="true"
+                            className="absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50"
                           />
+                          <p className="relative text-lg font-semibold text-white">
+                            {product.price.formatted.withTax}
+                          </p>
+                        </div>
+                      </div>
+                      <div className="mt-6">
+                        <Link href={`/${product.slug}`} passHref>
+                          <a
+                            href={product.slug}
+                            className="relative flex bg-gray-100 border border-transparent rounded-md py-2 px-8 items-center justify-center text-sm font-medium text-gray-900 hover:bg-gray-200"
+                          >
+                            Add to bag
+                            <span className="sr-only">, {product.name}</span>
+                          </a>
                         </Link>
                       </div>
-                      <div className="relative mt-4">
-                        <h3 className="text-sm font-medium text-gray-900">
-                          <Link href={`/${product.slug}`} passHref>
-                            <a href={`/${product.slug}`}>{product.name}</a>
-                          </Link>
-                        </h3>
-                        {/* <p className="mt-1 text-sm text-gray-500">
-                {product.color}
-              </p> */}
-                      </div>
-                      <div className="absolute top-0 inset-x-0 h-72 rounded-lg p-4 flex items-end justify-end overflow-hidden">
-                        <div
-                          aria-hidden="true"
-                          className="absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50"
-                        />
-                        <p className="relative text-lg font-semibold text-white">
-                          {product.price.formatted.withTax}
-                        </p>
-                      </div>
                     </div>
-                    <div className="mt-6">
-                      <Link href={`/${product.slug}`} passHref>
-                        <a
-                          href={product.slug}
-                          className="relative flex bg-gray-100 border border-transparent rounded-md py-2 px-8 items-center justify-center text-sm font-medium text-gray-900 hover:bg-gray-200"
-                        >
-                          Add to bag
-                          <span className="sr-only">, {product.name}</span>
-                        </a>
-                      </Link>
-                    </div>
-                  </div>
-                ))}
+                  ))}
               </div>
-            </>
+            </div>
           )
         })}
       </div>
