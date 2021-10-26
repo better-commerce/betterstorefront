e2157ed0908da7ec7b23b6662c4c98bd18b59f21
Adrian Mustea <mustea.adrian95@gmail.com>
2021-10-26 15:43:57 +0300

    changed slugs, added snippets & added tabs

--- components/utils/constants.ts
+++ components/utils/constants.ts
@@ -1,4 +1,4 @@
-export const HOMEPAGE_SLUG = '?slug=/'
+export const HOMEPAGE_SLUG = '/'
 export const NAV_ENDPOINT = '/api/v1/content/nav'
 export const SEARCH_MINIMAL_ENDPOINT = '/api/v1/catalog/search/advanced/minimal'
 export const PRODUCT_API_ENDPOINT = '/api/v1/catalog/product/'
