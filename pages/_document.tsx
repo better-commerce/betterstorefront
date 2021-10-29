bce2d72909a81263b5e5afe883d8d18d6bdacf9a
Adrian Mustea <mustea.adrian95@gmail.com>
2021-10-29 10:27:02 +0300

    added canishub script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,11 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
-import Cookies from 'cookie'
+import Script from 'next/script'
+
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head />
-
         <body className="loading">
           <Main />
           <NextScript />
