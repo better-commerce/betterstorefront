ac30e8d87fc2bfe9846d3d496599454018e0d5c2
Adrian Mustea <mustea.adrian95@gmail.com>
2021-11-24 17:09:29 +0200

    fixed links

--- pages/my-account/index.tsx
+++ pages/my-account/index.tsx
@@ -6,20 +6,11 @@ import { config } from '@components/utils/myAccount'
 import COMPONENTS_MAP from '@components/account'
 import withAuth from '@components/utils/withAuth'
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 function MyAccount({ defaultView }: any) {
   const [view, setView] = useState(defaultView)
   const router = useRouter()
 
-  const handleTabChange = (index: number) => {
-    router.push(
-      {
-        pathname: router.pathname,
-        query: { ...router.query, view: config[index].props },
-      },
-      undefined,
-      { shallow: true }
-    )
-  }
   useEffect(() => {
     if (router.query.view && view !== router.query.view) {
       setView(router.query.view)
@@ -30,26 +21,34 @@ function MyAccount({ defaultView }: any) {
     <section className="text-gray-900 relative py-10">
       <div className="w-full">
         <div className="justify-between px-10 flex flex-col md:flex-row">
-          <Tab.Group
-            onChange={handleTabChange}
-            vertical
-            defaultIndex={defaultView}
-          >
+          <Tab.Group vertical defaultIndex={defaultView}>
             <Tab.List className="sticky top-0 flex flex-col w-full md:w-1/4 bg-gray-200 h-full rounded-lg">
               {config.map((item: any, idx: number) => {
                 return (
-                  <Tab
-                    key={`my-acc-${idx}`}
-                    className={({ selected }: any) => {
-                      return `${
-                        selected
-                          ? 'bg-white text-indigo-600 border border-indigo-600'
-                          : ''
-                      } w-full px-5 py-5 hover:bg-white hover:text-indigo-600 border border-transparent hover:border-indigo-600 text-lg leading-5 font-medium text-gray-700 font-bold rounded-lg focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60`
+                  <Link
+                    key={idx}
+                    href={{
+                      pathname: router.pathname,
+                      query: { ...router.query, view: item.props },
                     }}
+                    shallow={true}
                   >
-                    {item.text}
-                  </Tab>
+                    <Tab
+                      key={`my-acc-${idx}`}
+                      as="a"
+                      href="#"
+                      onClick={(e: any) => e.preventDefault()}
+                      className={({ selected }: any) => {
+                        return `${
+                          selected
+                            ? 'bg-white text-indigo-600 border border-indigo-600'
+                            : ''
+                        } w-full px-5 py-5 hover:bg-white hover:text-indigo-600 border border-transparent hover:border-indigo-600 text-lg leading-5 font-medium text-gray-700 font-bold rounded-lg focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60`
+                      }}
+                    >
+                      {item.text}
+                    </Tab>
+                  </Link>
                 )
               })}
             </Tab.List>
