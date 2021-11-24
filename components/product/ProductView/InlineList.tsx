33e6d4eb2da0dc398ab40e874114d8aa56e15f5d
Adrian Mustea <mustea.adrian95@gmail.com>
2021-11-24 17:09:29 +0200

    fixed links

--- components/product/ProductView/InlineList.tsx
+++ components/product/ProductView/InlineList.tsx
@@ -1,27 +1,26 @@
 import { RadioGroup } from '@headlessui/react'
 import classNames from '@components/utils/classNames'
-
+import Link from 'next/link'
 export default function InlineList({
   items = [],
   onChange = () => {},
   label = 'Choose a color',
   fieldCode = 'global.colour',
   currentAttribute = 'black',
+  generateLink = () => {},
 }: any) {
   const handleChange = (value: any) => {
     return onChange(fieldCode, value)
   }
+
   return (
     <>
       <h3 className="text-sm text-gray-600">{label}</h3>
-      <RadioGroup
-        value={'ring-gray-700'}
-        onChange={handleChange}
-        className="mt-2"
-      >
+      <RadioGroup value={'ring-gray-700'} onChange={() => {}} className="mt-2">
         <RadioGroup.Label className="sr-only">{label}</RadioGroup.Label>
         <div className="flex items-center space-x-3">
           {items.map((item: any, idx: any) => {
+            const path = generateLink(fieldCode, item.fieldValue)
             return (
               <RadioGroup.Option
                 key={idx}
@@ -38,13 +37,16 @@ export default function InlineList({
                 <RadioGroup.Label as="p" className="sr-only">
                   {item.fieldName}
                 </RadioGroup.Label>
-                <span
-                  aria-hidden="true"
-                  className={classNames(
-                    item.fieldvalue,
-                    'h-8 w-8 border border-black border-opacity-10 rounded-full'
-                  )}
-                />
+                <Link href={`/${path}`} passHref>
+                  <a
+                    aria-hidden="true"
+                    href={`/${path}`}
+                    className={classNames(
+                      item.fieldvalue,
+                      'h-8 w-8 border border-black border-opacity-10 rounded-full'
+                    )}
+                  />
+                </Link>
               </RadioGroup.Option>
             )
           })}
