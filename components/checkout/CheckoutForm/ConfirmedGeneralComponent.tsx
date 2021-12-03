066950f98d9d09bc94865a5a0e4fe035f45d9fc8
Adrian Mustea <mustea.adrian95@gmail.com>
2021-12-03 17:07:09 +0200

    build fix

--- components/checkout/CheckoutForm/ConfirmedGeneralComponent.tsx
+++ components/checkout/CheckoutForm/ConfirmedGeneralComponent.tsx
@@ -6,7 +6,11 @@ export default function ConfirmedGeneralComponent({
     <div className="text-gray-900 flex justify-between items-center">
       <ul className={`text-gray-900 mt-10`}>
         {Object.keys(content).map((item: any, idx: number) => {
-          return <li className="font-semibold">{content[item]}</li>
+          return (
+            <li key={idx} className="font-semibold">
+              {content[item]}
+            </li>
+          )
         })}
       </ul>
       <button onClick={onStateChange} className="btn btn" type="button">
