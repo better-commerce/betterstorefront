f88385c6e478979dd00260aacbbf737fb29387ef
Adrian Mustea <mustea.adrian95@gmail.com>
2021-12-18 20:47:14 +0200

    configurable infinite scorll / pagination

---
+++ components/ui/InfiniteScroll/index.tsx
@@ -0,0 +1,24 @@
+import InfiniteScroll from 'react-infinite-scroll-component'
+
+export default function InfiniteScrollComponent({
+  component,
+  total,
+  fetchData,
+  currentNumber,
+}: any) {
+  return (
+    <InfiniteScroll
+      dataLength={total} //This is important field to render the next data
+      next={fetchData}
+      hasMore={currentNumber < total}
+      loader={<h4>Loading...</h4>}
+      endMessage={
+        <p style={{ textAlign: 'center' }}>
+          <b>You have seen it all</b>
+        </p>
+      }
+    >
+      {component}
+    </InfiniteScroll>
+  )
+}
