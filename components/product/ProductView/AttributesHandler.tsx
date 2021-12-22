c41e6c4ff7df9a8e05e1f1062895c9e959fcf665
Adrian Mustea <mustea.adrian95@gmail.com>
2021-12-22 20:59:22 +0200

    attributes custom'

--- components/product/ProductView/AttributesHandler.tsx
+++ components/product/ProductView/AttributesHandler.tsx
@@ -1,6 +1,8 @@
 import Dropdown from './Dropdown'
 import InlineList from './InlineList'
 import { useRouter } from 'next/router'
+import { useState } from 'react'
+import attributesGenerator from '@components/utils/attributesGenerator'
 
 const ATTR_COMPONENTS: any = {
   Dropdown: (props: any) => <Dropdown {...props} />,
@@ -20,6 +22,27 @@ export default function AttributesHandler({
   const { attributes, variantProductsAttribute = [], variantProducts } = product
 
   const router = useRouter()
+
+  const getAttributesFromSlug = () => {
+    const slug = `products/${router.query.slug}`
+    return variantProducts.reduce((acc: any, obj: any) => {
+      if (obj.slug === slug) {
+        obj.variantAttributes.forEach((varAttr: any) => {
+          acc[varAttr.fieldCode] = varAttr.fieldValue
+        })
+      }
+      return acc
+    }, {})
+  }
+
+  const originalAttributes = getAttributesFromSlug()
+
+  const generatedAttrCombination = Object.fromEntries(
+    Object.entries(originalAttributes).slice(0, 1)
+  )
+  const [attrCombination, setAttrCombination] = useState(
+    generatedAttrCombination
+  )
 
   const generateLink = (fieldCode: any, value: any) => {
     let slug = ''
@@ -41,20 +64,6 @@ export default function AttributesHandler({
       router.push(`/${slug}`)
     }
   }
-
-  const getAttributesFromSlug = () => {
-    const slug = `products/${router.query.slug}`
-    return variantProducts.reduce((acc: any, obj: any) => {
-      if (obj.slug === slug) {
-        obj.variantAttributes.forEach((varAttr: any) => {
-          acc[varAttr.fieldCode] = varAttr.fieldValue
-        })
-      }
-      return acc
-    }, {})
-  }
-
-  const originalAttributes = getAttributesFromSlug()
 
   const getStockPerAttribute = (key: string, variant: string) => {
     let productData = {
@@ -80,22 +89,83 @@ export default function AttributesHandler({
     return productData
   }
 
+  const isCustomAttr = product.stockCode === '123N'
+
+  const generateOptions = (option: any) => {
+    const isInOrder =
+      Object.keys(originalAttributes).findIndex(
+        (i: string) => i === option.fieldCode
+      ) -
+        Object.keys(attrCombination).length ===
+        0 || Object.keys(attrCombination).includes(option.fieldCode)
+
+    if (isInOrder) {
+      if (isCustomAttr && Object.keys(attrCombination).length > 1) {
+        const entriesFromCombination = () => {
+          return Object.fromEntries(
+            Object.entries(attrCombination).slice(
+              1,
+              Object.keys(attrCombination).length
+            )
+          )
+        }
+        const generatedAttributes = attributesGenerator(
+          entriesFromCombination(),
+          variantProducts
+        )
+        return generatedAttributes
+          .map((item: any) => {
+            if (option.fieldCode === item.fieldCode) {
+              return item
+            }
+          })
+          .filter((el) => el)
+      } else return option.fieldValues
+    } else return []
+  }
+
+  const handleAttrCombinations = (key: string, value: any) => {
+    setAttrCombination((prevValue: any) => {
+      let newValue = prevValue
+      const existingValueIndex = Object.keys(prevValue).findIndex(
+        (i: any) => i === key
+      )
+      newValue[key] = value
+      if (existingValueIndex > -1) {
+        newValue = Object.fromEntries(
+          Object.entries(newValue).slice(0, existingValueIndex + 1)
+        )
+      }
+      return newValue
+    })
+  }
+
   const DefaultComponent: any = () => null
+  const stateAttributes: any = attrCombination
   return (
     <>
       {variantProductsAttribute?.map((option: any, idx: number) => {
-        const Component = TEMP_MAP[option.fieldCode] || DefaultComponent
+        const optionsToPass = generateOptions(option)
+        const originalAttribute = isCustomAttr
+          ? stateAttributes[option.fieldCode]
+          : originalAttributes[option.fieldCode]
+        const Component =
+          ATTR_COMPONENTS[option.inputType] ||
+          TEMP_MAP[option.fieldCode] ||
+          DefaultComponent
         return (
           <div key={idx} className="py-3">
             <Component
-              currentAttribute={originalAttributes[option.fieldCode]}
+              currentAttribute={originalAttribute}
               getStockPerAttribute={getStockPerAttribute}
-              items={option.fieldValues}
+              items={optionsToPass}
               label={option.fieldName}
+              isDisabled={!optionsToPass.length}
               onChange={handleChange}
               setSelectedAttrData={setSelectedAttrData}
               fieldCode={option.fieldCode}
               productId={product.id}
+              setAttrCombination={handleAttrCombinations}
               generateLink={generateLink}
             />
           </div>
