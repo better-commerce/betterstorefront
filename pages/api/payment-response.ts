dee940089c3252d9b0c64347ad9defa3bf7c33cb
Adrian Mustea <mustea.adrian95@gmail.com>
2021-12-26 15:33:22 +0200

    payment COD method + half external

---
+++ pages/api/payment-response.ts
@@ -0,0 +1,13 @@
+import { putPaymentResponse } from '@framework/payment'
+export default async (req: any, res: any) => {
+  const { orderId, model }: any = req.body
+  try {
+    const response = await putPaymentResponse()({
+      orderId,
+      model,
+    })
+    res.status(200).json(response)
+  } catch (error) {
+    res.status(500).json({ error })
+  }
+}
