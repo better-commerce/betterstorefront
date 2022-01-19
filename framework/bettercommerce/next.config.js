6936dd181d9eb453940eefeee555108b89527c9f
Adrian Mustea <mustea.adrian95@gmail.com>
2022-01-19 22:45:04 +0200

    redirects

--- framework/bettercommerce/next.config.js
+++ framework/bettercommerce/next.config.js
@@ -13,44 +13,120 @@ const fs = require('fs')
 
 const url = new URL('oAuth/token', AUTH_URL)
 
-console.log(path.join(__dirname, '/config/seo.json'))
-axios({
-  url: url.href,
-  method: 'post',
-  data: `client_id=${CLIENT_ID}&client_secret=${SHARED_SECRET}&grant_type=client_credentials`,
-})
-  .then((res) => {
-    const INFRA_URL = new URL(INFRA_ENDPOINT, BASE_URL)
-    axios({
+const getToken = async () => {
+  try {
+    const response = await axios({
+      url: url.href,
+      method: 'post',
+      data: `client_id=${CLIENT_ID}&client_secret=${SHARED_SECRET}&grant_type=client_credentials`,
+    })
+    return response.data.access_token
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+const getSeoConfig = async function (token) {
+  try {
+    const INFRA_URL = new URL(INFRA_ENDPOINT, BASE_URL).href
+    // const token = await getToken()
+    const infra = await axios({
       url: `${INFRA_URL}`,
       method: 'get',
       headers: {
         DomainId: process.env.NEXT_PUBLIC_DOMAIN_ID,
-        Authorization: 'Bearer ' + res.data.access_token,
+        Authorization: 'Bearer ' + token,
       },
     })
-      .then((infra) => {
-        const seoConfig = infra.data.result.configSettings
-          .find((i) => i.configType === 'SeoSettings')
-          ?.configKeys.reduce((acc, obj) => {
-            if (obj.key === 'SeoSettings.DefaultTitle')
-              acc['title'] = obj.value || JSON.stringify(obj.value)
-            if (obj.key === 'SeoSettings.DefaultMetaDescription')
-              acc['description'] = obj.value || JSON.stringify(obj.value)
-            if (obj.key === 'SeoSettings.DefaultMetaKeywords')
-              acc['keywords'] = obj.value || JSON.stringify(obj.value)
-            return acc
-          }, {})
-        fs.writeFileSync(
-          path.join(__dirname, '/config/seo.json'),
-          JSON.stringify(seoConfig),
-          (err) => console.log(err)
-        )
-      })
-      .catch((err) => console.log(err))
+    const seoConfig = infra.data.result.configSettings
+      .find((i) => i.configType === 'SeoSettings')
+      ?.configKeys.reduce((acc, obj) => {
+        if (obj.key === 'SeoSettings.DefaultTitle')
+          acc['title'] = obj.value || JSON.stringify(obj.value)
+        if (obj.key === 'SeoSettings.DefaultMetaDescription')
+          acc['description'] = obj.value || JSON.stringify(obj.value)
+        if (obj.key === 'SeoSettings.DefaultMetaKeywords')
+          acc['keywords'] = obj.value || JSON.stringify(obj.value)
+        return acc
+      }, {})
+    fs.writeFileSync(
+      path.join(__dirname, '/config/seo.json'),
+      JSON.stringify(seoConfig),
+      (err) => console.log(err)
+    )
+  } catch (error) {}
+}
+// getToken().then((res) => {
+//   const INFRA_URL = new URL(INFRA_ENDPOINT, BASE_URL).href
+// axios({
+//   method: 'get',
+//   url: new URL('/api/v2/content/redirects', BASE_URL).href,
+//   headers: {
+//     DomainId: process.env.NEXT_PUBLIC_DOMAIN_ID,
+//     Authorization: 'Bearer ' + res.data.access_token,
+//   },
+//   }).then((redirects) => console.log(redirects))
+// axios({
+//   url: `${INFRA_URL}`,
+//   method: 'get',
+//   headers: {
+//     DomainId: process.env.NEXT_PUBLIC_DOMAIN_ID,
+//     Authorization: 'Bearer ' + res.data.access_token,
+//   },
+// })
+//   .then((infra) => {
+//     const seoConfig = infra.data.result.configSettings
+//       .find((i) => i.configType === 'SeoSettings')
+//       ?.configKeys.reduce((acc, obj) => {
+//         if (obj.key === 'SeoSettings.DefaultTitle')
+//           acc['title'] = obj.value || JSON.stringify(obj.value)
+//         if (obj.key === 'SeoSettings.DefaultMetaDescription')
+//           acc['description'] = obj.value || JSON.stringify(obj.value)
+//         if (obj.key === 'SeoSettings.DefaultMetaKeywords')
+//           acc['keywords'] = obj.value || JSON.stringify(obj.value)
+//         return acc
+//       }, {})
+//     fs.writeFileSync(
+//       path.join(__dirname, '/config/seo.json'),
+//       JSON.stringify(seoConfig),
+//       (err) => console.log(err)
+//     )
+//   })
+//     .catch((err) => console.log(err))
+// })
+
+const handler = async () => {
+  const token = await getToken()
+  await Promise.all([getSeoConfig(token)])
+}
+
+const getKeywords = async function () {
+  const token = await getToken()
+  const response = await axios({
+    method: 'get',
+    url: new URL('/api/v2/content/redirects', BASE_URL).href,
+    headers: {
+      DomainId: process.env.NEXT_PUBLIC_DOMAIN_ID,
+      Authorization: 'Bearer ' + token,
+    },
   })
-  .catch((err) => console.log(err))
-console.log('axios', axios)
+  return response.data.result.map((item) => {
+    return {
+      source: new URL(item.oldUrl).pathname,
+      destination: item.newUrl,
+      permanent: true,
+    }
+  })
+}
+
+handler()
+getKeywords().then((res) => console.log(res))
+
 module.exports = {
+  //https://nextjs.org/docs/api-reference/next.config.js/redirects nextjs documentation on redirects
   commerce,
+  async redirects() {
+    const keywords = await getKeywords()
+    return keywords
+  },
 }
