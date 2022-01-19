22b26db3041dd81dd0042326ad9e87b7660954d5
Adrian Mustea <mustea.adrian95@gmail.com>
2022-01-19 23:25:20 +0200

    feat multisites'

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -134,8 +134,9 @@ const Layout: FC<Props> = ({
   }, [])
 
   const { acceptedCookies, onAcceptCookies } = useAcceptCookies()
-  const { locale = 'en-US' } = useRouter()
+  const { locale = 'en-US', ...rest } = useRouter()
 
+  console.log(useRouter())
   const sortedData = data.nav.sort(
     (a: any, b: any) => a.displayOrder - b.displayOrder
   )
