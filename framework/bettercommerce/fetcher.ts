853f66f34d4e82bae229ecf4ca35f4018fc3725b
Adrian Mustea <mustea.adrian95@gmail.com>
2022-02-19 09:59:27 +0200

    col page, category index pg

--- framework/bettercommerce/fetcher.ts
+++ framework/bettercommerce/fetcher.ts
@@ -101,7 +101,6 @@ const fetcher = async ({
   if (Object.keys(data).length) {
     config.data = data
   }
-  console.log(config)
   try {
     const response = await axiosInstance(config)
     return response.data
