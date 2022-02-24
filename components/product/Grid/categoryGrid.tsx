1e02c55ddfa7b55260b3a716916ff86b3a9ded6e
Amit Kumar <amit.kumar@bettercommerce.io>
2022-02-24 11:56:46 +0530

    Update Category Index and grid size in category detail

---
+++ components/product/Grid/categoryGrid.tsx
@@ -0,0 +1,98 @@
+import Pagination from '@components/product/Pagination'
+import rangeMap from '@lib/range-map'
+import ProductCard from '@components/product/ProductCard/ProductCard'
+import InfiniteScroll from '@components/ui/InfiniteScroll'
+import { TITLE_PRODUCTS } from '@components/utils/textVariables'
+
+interface Props {
+  products: any
+  currentPage: number | string
+  handlePageChange?: any
+  handleInfiniteScroll: any
+}
+
+export default function CategoryGrid({
+  products,
+  currentPage,
+  handlePageChange = () => {},
+  handleInfiniteScroll,
+}: Props) {
+  const IS_INFINITE_SCROLL =
+    process.env.NEXT_PUBLIC_ENABLE_INFINITE_SCROLL === 'true'
+
+  return (
+    <section
+      aria-labelledby="products-heading"
+      className="max-w-7xl overflow-hidden sm:px-6 lg:px-8 mx-auto"
+    >
+      <h2 id="products-heading" className="sr-only">
+        {TITLE_PRODUCTS}
+      </h2>
+
+      {IS_INFINITE_SCROLL && (
+        <InfiniteScroll
+          fetchData={handleInfiniteScroll}
+          total={products.total}
+          currentNumber={products.results.length}
+          component={
+            <div
+              className={`-mx-px border-l border-t border-gray-200 grid grid-cols-2 sm:mx-0 md:grid-cols-4 ${
+                products.results.length < 3
+                  ? `lg:grid-cols-2`
+                  : 'lg:grid-cols-4'
+              }`}
+            >
+              {!products.results.length &&
+                rangeMap(12, (i) => (
+                  <div
+                    key={i}
+                    className="shadow-md w-60 h-72 rounded-md mx-auto mt-20"
+                  >
+                    <div className="flex animate-pulse flex-row items-center h-full justify-center space-x-5">
+                      <div className="flex flex-col space-y-3">
+                        <div className="w-full bg-gray-100 h-48 rounded-md "></div>
+                        <div className="w-36 bg-gray-100 h-6 mt-40 rounded-md "></div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              {products.results.map((product: any, productIdx: number) => (
+                <ProductCard key={productIdx} product={product} />
+              ))}
+            </div>
+          }
+        />
+      )}
+      {!IS_INFINITE_SCROLL && (
+        <>
+          <div className="-mx-px border-l border-t border-gray-200 grid grid-cols-2 sm:mx-0 md:grid-cols-3 lg:grid-cols-4">
+            {!products.results.length &&
+              rangeMap(12, (i) => (
+                <div
+                  key={i}
+                  className="shadow-md w-60 h-72 rounded-md mx-auto mt-20"
+                >
+                  <div className="flex animate-pulse flex-row items-center h-full justify-center space-x-5">
+                    <div className="flex flex-col space-y-3">
+                      <div className="w-full bg-gray-100 h-48 rounded-md "></div>
+                      <div className="w-36 bg-gray-100 h-6 mt-40 rounded-md "></div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            {products.results.map((product: any, productIdx: number) => (
+              <ProductCard key={productIdx} product={product} />
+            ))}
+          </div>
+          {products.pages > 1 && (
+            <Pagination
+              currentPage={currentPage}
+              onPageChange={handlePageChange}
+              pageCount={products.pages}
+            />
+          )}
+        </>
+      )}
+    </section>
+  )
+}
--- pages/category/index.tsx
+++ pages/category/index.tsx
@@ -12,7 +12,7 @@ export default function CategoryList(props: any) {
           Categories
         </h2>
 
-        <div className="py-10 grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:gap-x-8">
+        <div className="py-2 grid grid-cols-1 gap-y-5 sm:grid-cols-3 gap-x-6 lg:grid-cols-5 xl:gap-x-5">
           {props.data.map((category: any, key: number) => (
             <Link key={key} href={`/${category.link}`}>
               <a
@@ -20,22 +20,22 @@ export default function CategoryList(props: any) {
                 href={`/${category.link}`}
                 className="group"
               >
-                <div className="relative w-full aspect-w-1 aspect-h-1 rounded-lg overflow-hidden sm:aspect-w-2 sm:aspect-h-3">
+                <div className="relative w-full aspect-w-1 bg-gray-200 aspect-h-1 rounded-lg overflow-hidden sm:aspect-w-3 sm:aspect-h-3">
                   <img
                     src={
                       category.image ||
-                      'https://tailwindui.com/img/ecommerce-images/category-page-01-image-card-01.jpg'
+                      '/noimagefound.png'
                     }
                     // alt={category.imageAlt}
                     className="w-full h-full object-center object-cover group-hover:opacity-75"
                   />
                   <h1
                     style={{
-                      backgroundColor: 'rgba(255,255,255,0.4',
+                      backgroundColor: 'rgba(255,255,255,0.6',
                       paddingLeft: '5%',
-                      top: '90%',
+                      top: '85%',
                     }}
-                    className="w-8/10 px-2 absolute text-gray-900 font-bold text-xl"
+                    className="w-8/10 px-2 absolute uppercase text-gray-900 font-semibold text-md"
                   >
                     {category.name}
                   </h1>
