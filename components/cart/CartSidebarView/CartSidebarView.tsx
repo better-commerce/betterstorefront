632f69ecd5d3e1f3f81cdda4d6d5c20de95d12c5
Amit Kumar <amit.kumar@bettercommerce.io>
2022-03-08 20:00:39 +0530

    Update CartSidebarView.tsx

--- components/cart/CartSidebarView/CartSidebarView.tsx
+++ components/cart/CartSidebarView/CartSidebarView.tsx
@@ -162,18 +162,18 @@ const CartSidebarView: FC = () => {
                           {cartItems.lineItems?.map((product: any) => (
                             <li key={product.id} className="">
                               <div className="py-6 flex">
-                                <div className="flex-shrink-0 w-24 h-24 border border-gray-200 rounded-md overflow-hidden image-container">
-                                  <Image 
+                                <div className="flex-shrink-0 w-24 h-24 border border-gray-200 rounded-md overflow-hidden">
+                                  {/* <Image 
                                       layout='fill'
                                       src={product.image}
                                       alt={product.name}
                                       className="w-full h-full object-center object-cover image"
-                                  ></Image>
-                                  {/* <img
+                                  ></Image> */}
+                                  <img
                                     src={product.image}
                                     alt={product.name}
                                     className="w-full h-full object-center object-cover"
-                                  /> */}
+                                  />
                                 </div>
 
                                 <div className="ml-4 flex-1 flex flex-col">
