0a138afa72260cdb333483cf32c9934acec7e390
Adrian Mustea <mustea.adrian95@gmail.com>
2022-03-31 13:42:09 +0300

    categ & collection build fix

--- pages/category/index.tsx
+++ pages/category/index.tsx
@@ -3,7 +3,10 @@ import { getAllCategories } from '@framework/category'
 import Link from 'next/link'
 import Image from 'next/image'
 import { Layout } from '@components/common'
-import { IMG_PLACEHOLDER, SHOP_BY_CATEGORY } from '@components/utils/textVariables'
+import {
+  IMG_PLACEHOLDER,
+  SHOP_BY_CATEGORY,
+} from '@components/utils/textVariables'
 export default function CategoryList(props: any) {
   console.log(props.data)
   return (
@@ -19,37 +22,39 @@ export default function CategoryList(props: any) {
               <div className="sm:py-6 py-1 grid grid-cols-2 sm:gap-y-8 gap-y-6 sm:grid-cols-4 gap-x-6 lg:grid-cols-5 xl:gap-x-8">
                 {props.data.map((category: any, key: number) => (
                   <Link key={key} href={`/${category.link}`}>
-                      <a
-                        key={category.id}
-                        href={`/${category.link}`}
-                        className="relative sm:w-56 sm:h-80 h-60 w-full rounded-lg p-6 flex flex-col overflow-hidden hover:opacity-75 xl:w-auto"
-                      >
-                        <span
-                      aria-hidden="true"
-                      className="absolute inset-x-0 top-0 h-full bg-gradient-to-b from-gray-100 opacity-90"
-                    />
-                    <span aria-hidden="true" className="absolute inset-0">
-                      <div className='image-container'>
-                        <Image 
-                            src={category.image || IMG_PLACEHOLDER } 
+                    <a
+                      key={category.id}
+                      href={`/${category.link}`}
+                      className="relative sm:w-56 sm:h-80 h-60 w-full rounded-lg p-6 flex flex-col overflow-hidden hover:opacity-75 xl:w-auto"
+                    >
+                      <span
+                        aria-hidden="true"
+                        className="absolute inset-x-0 top-0 h-full bg-gradient-to-b from-gray-100 opacity-90"
+                      />
+                      <span aria-hidden="true" className="absolute inset-0">
+                        <div className="image-container">
+                          <Image
+                            src={category.image || IMG_PLACEHOLDER}
                             alt={category.name}
                             className="w-full h-full object-center object-cover group-hover:opacity-75 image"
-                            layout='fill'
-                        ></Image>  
-                      </div>
-                    </span>
-                    <span
-                      aria-hidden="true"
-                      className="absolute inset-x-0 bottom-0 h-1/3 bg-gradient-to-t from-gray-900 opacity-40"
-                    />
-                    <span className="relative mt-auto text-center sm:text-xl text-sm font-bold text-white">{category.name}</span>
-                  </a>
+                            layout="fill"
+                          ></Image>
+                        </div>
+                      </span>
+                      <span
+                        aria-hidden="true"
+                        className="absolute inset-x-0 bottom-0 h-1/3 bg-gradient-to-t from-gray-900 opacity-40"
+                      />
+                      <span className="relative mt-auto text-center sm:text-xl text-sm font-bold text-white">
+                        {category.name}
+                      </span>
+                    </a>
                   </Link>
                 ))}
               </div>
             </div>
           </div>
-        </div>       
+        </div>
       </section>
     </main>
   )
@@ -66,7 +71,7 @@ export async function getStaticProps({
   const data = await getAllCategories()
   return {
     props: {
-      data,
+      data: data || [],
     },
     revalidate: 200,
   }
