75557b2f882de2c1de616c9268ceeac2b8a63f8e
Amit Kumar <amit.kumar@bettercommerce.io>
2022-04-25 19:41:58 +0530

    Revert back changes for store locator

--- pages/store-locator/[name].tsx
+++ pages/store-locator/[name].tsx
@@ -1,45 +1,39 @@
-export default function StoreLocatorDetailsPage(){
-  return(
-    <></>
+import { Layout } from '@components/common'
+import type { GetStaticPropsContext } from 'next'
+import getStores from '@framework/storeLocator/getStores'
+
+interface Props {
+  data: any
+}
+
+export default function StoreLocatorDetailsPage({ data }: Props) {
+  if (!data) return null
+
+  return (
+    <div className="text-gray-900">
+      <h1>{data.name}</h1>
+      <h1>{data.availableToCollectIn}</h1>
+
+      <h1>{data.postCode}</h1>
+    </div>
   )
 }
 
-// import { Layout } from '@components/common'
-// import type { GetStaticPropsContext } from 'next'
-// import getStores from '@framework/storeLocator/getStores'
+export async function getStaticPaths() {
+  //this would require an endpoint to fetch all existing stores
+  const response = await getStores('')
+  return {
+    paths: response.map((store: any) => `/store-locator/${store.name}`),
+    fallback: 'blocking',
+  }
+}
 
-// interface Props {
-//   data: any
-// }
+export async function getStaticProps({ params }: GetStaticPropsContext) {
+  //this would require an endpoint to fetch a specific store
+  const response = await getStores('')
+  return {
+    props: { data: response[0] },
+  }
+}
 
-// export default function StoreLocatorDetailsPage({ data }: Props) {
-//   if (!data) return null
-
-//   return (
-//     <div className="text-gray-900">
-//       <h1>{data.name}</h1>
-//       <h1>{data.availableToCollectIn}</h1>
-
-//       <h1>{data.postCode}</h1>
-//     </div>
-//   )
-// }
-
-// export async function getStaticPaths() {
-//   //this would require an endpoint to fetch all existing stores
-//   const response = await getStores('')
-//   return {
-//     paths: response.map((store: any) => `/store-locator/${store.name}`),
-//     fallback: 'blocking',
-//   }
-// }
-
-// export async function getStaticProps({ params }: GetStaticPropsContext) {
-//   //this would require an endpoint to fetch a specific store
-//   const response = await getStores('')
-//   return {
-//     props: { data: response[0] },
-//   }
-// }
-
-// StoreLocatorDetailsPage.Layout = Layout
+StoreLocatorDetailsPage.Layout = Layout
