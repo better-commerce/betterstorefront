c7aad0a954a5a46e47465397d7fc3880b9728227
Amit Kumar <amit.kumar@bettercommerce.io>
2022-04-25 19:00:30 +0530

    Update constants.ts

--- components/utils/constants.ts
+++ components/utils/constants.ts
@@ -106,4 +106,3 @@ export const NEXT_GEO_ENDPOINT =
   process.env.NEXT_PUBLIC_GEO_ENDPOINT ||
   'https://omnilytics.bettercommerce.io/api/v1/IpInfo?ipAddress='
 export const UPDATE_ORDER_STATUS = '/api/update-order-status'
-
