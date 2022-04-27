c3cedd8a58b3e104d6acd178a2c314b7dce0c02c
Gagan <103169116+gagandeep-gingh-bt@users.noreply.github.com>
2022-04-27 16:43:50 +0530

    #174 snippet injections for header & footer + refactoring

---
+++ framework/commerce/utils/dom-util.ts
@@ -0,0 +1,108 @@
+// Package Imports
+//import { DOMParser } from "@xmldom/xmldom";
+
+export enum TagNameType {
+    SCRIPT = "SCRIPT",
+    STYLE = "STYLE",
+    HTML = "HTML",
+    OTHER = "OTHER"
+}
+
+export type HtmlElementPosition =
+| "beforebegin"
+| "afterbegin"
+| "beforeend"
+| "afterend";
+
+export interface IDomReference {
+    readonly element: any;
+    readonly type: string;
+}
+
+export const domReader = (content: string) => {
+    //debugger;
+    const doc = new DOMParser().parseFromString(content, "text/html");
+    try {
+        if (doc) {
+            //debugger;
+            const domElements = [...doc.head.querySelectorAll("*"), ...doc.body.querySelectorAll("*")];
+            if (domElements && domElements.length) {
+                let arrDOM = new Array<IDomReference>();
+                domElements.forEach((elem: any) => {
+                    if (elem.tagName === TagNameType.SCRIPT) {
+                        arrDOM.push({
+                            type: elem.tagName,
+                            element: elem
+                        });
+                    } else if (elem.tagName === TagNameType.STYLE) {
+                        arrDOM.push({
+                            type: elem.tagName,
+                            element: elem
+                        });
+                    } else {
+                        arrDOM.push({
+                            type: TagNameType.HTML,
+                            element: elem
+                        });
+                    }
+                });
+                return { elements: arrDOM };
+            }
+        } else {
+            return { error: "Error parsing DOM input." };
+        }
+    } catch (e) {
+        return { error: e };
+    }
+
+    return {};
+};
+
+export const domElementLoader = (element: any, insertAtTop: boolean, attrs?: object, node?: HTMLElement, skip?: boolean) => {
+    //debugger;
+    const loadDOM = (element: any, insertAtTop: boolean, attrs?: object, parentNode?: HTMLElement) => {
+        return new Promise((resolve, reject) => {
+            //const style = Object.create(element); // JSON.parse(JSON.stringify(element));
+
+            for (const [k, v] of Object.entries(attrs || {})) {
+                element.setAttribute(k, v);
+            }
+
+            const node = parentNode || document.head; // || document.getElementsByTagName('head')[0];
+            if (insertAtTop) {
+                node.insertBefore(element, node.firstChild);
+                resolve(true);
+            } else {
+                node.appendChild(element);
+                resolve(true);
+            }
+        });
+    };
+
+    if (!skip) {
+        return loadDOM(element, insertAtTop, attrs, node)
+            .then(style => {
+                return { style: style as HTMLStyleElement };
+            })
+            .catch((e: { message: string }) => {
+                console.error(e);
+                return { error: e };
+            });
+    }
+
+    return {};
+};
+
+export const insertAdjacentHTML = (content: string, node: HTMLElement, attrs: Object, position: HtmlElementPosition) => {
+    let container = document.createElement("div");
+    container.insertAdjacentHTML("beforeend", content);
+    const arrNodes = container.querySelectorAll("*");
+    if (arrNodes && arrNodes.length) {
+        arrNodes.forEach(node => {
+            for (const [k, v] of Object.entries(attrs || {})) {
+                node.setAttribute(k, v);
+            }
+        });
+    }
+    node.insertAdjacentHTML(position, container.innerHTML);
+};
