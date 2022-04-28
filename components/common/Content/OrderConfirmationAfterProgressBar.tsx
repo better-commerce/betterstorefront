d51e0755271d7246046ac7af24019e32dda2fbce
Gagan <103169116+gagandeep-gingh-bt@users.noreply.github.com>
2022-04-28 10:47:15 +0530

    #174 snippet injection on thank you page.

---
+++ components/common/Content/OrderConfirmationAfterProgressBar.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react";
+
+const OrderConfirmationAfterProgressBar: FC = (props: any) => {
+    
+    {/* Placeholder for order confirmation after progress bar snippet */ }
+    return (
+        <div className="data-bc-snippet-ordconf-aftprgbar"></div>
+    )
+};
+
+export default OrderConfirmationAfterProgressBar;
