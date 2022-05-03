bc126abcc8760a063b0062fa5bd9e200e7c662b1
Amit Kumar <amit.kumar@bettercommerce.io>
2022-05-03 15:25:33 +0530

    Update ProductView.tsx

--- components/product/ProductView/ProductView.tsx
+++ components/product/ProductView/ProductView.tsx
@@ -278,37 +278,21 @@ export default function ProductView({
         StockCode: obj.stockCode,
         ItemType: obj.itemType || 0,
         CustomInfo1: values.line1 || null,
-
         CustomInfo2: values.line2 || null,
-
         CustomInfo3: values.line3 || null,
-
         CustomInfo4: values.line4 || null,
-
         CustomInfo5: values.line5 || null,
-
         ProductName: obj.name,
-
         ManualUnitPrice: obj.manualUnitPrice || 0.0,
-
         PostCode: obj.postCode || null,
-
         IsSubscription: obj.subscriptionEnabled || false,
-
         IsMembership: obj.hasMembership || false,
-
         SubscriptionPlanId: obj.subscriptionPlanId || null,
-
         SubscriptionTermId: obj.subscriptionTermId || null,
-
         UserSubscriptionPricing: obj.userSubscriptionPricing || 0,
-
         GiftWrapId: obj.giftWrapConfig || null,
-
         IsGiftWrapApplied: obj.isGiftWrapApplied || false,
-
         ItemGroupId: obj.itemGroupId || 0,
-
         PriceMatchReqId:
           obj.priceMatchReqId || '00000000-0000-0000-0000-000000000000',
       })
