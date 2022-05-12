118da7780fcdf2fb0fb00de41afd99ab3f94e21e
Gagan <103169116+gagandeep-gingh-bt@users.noreply.github.com>
2022-05-12 14:16:23 +0530

    #183 Fix: Display variant price

---
+++ pages/api/catalog/get-product-preview.ts
@@ -0,0 +1,14 @@
+import commerce from '@lib/api/commerce'
+
+export default async (req: any, res: any) => {
+  try {
+    const response = await commerce.getProductPreview({
+      query: req.body.slug,
+      cookies: req.cookies,
+    })
+    res.status(200).json(response)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error })
+  }
+}
