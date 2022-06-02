a7574d9499b5660d48ff9ab67c22673c24ec9a13
Gagan <103169116+gagandeep-gingh-bt@users.noreply.github.com>
2022-06-02 14:30:51 +0530

    Store locator api fix

--- framework/bettercommerce/storeLocator/getStores.ts
+++ framework/bettercommerce/storeLocator/getStores.ts
@@ -3,13 +3,15 @@ import { OMS_BASE_URL } from '@framework/utils/constants'
 import { STORE_LOCATOR_API } from '@components/utils/constants'
 export default async function getStores(postCode: string) {
   try {
-    //const storeURL = new URL(STORE_LOCATOR_API, OMS_BASE_URL)
     const response: any = await fetcher({
-      url: STORE_LOCATOR_API,
-      method: 'post',
-      data: { postCode: postCode },
-      baseUrl: OMS_BASE_URL
+      url: `${STORE_LOCATOR_API}/${postCode}`,
+      method: 'get',
+      //data: { postCode: postCode },
+      baseUrl: OMS_BASE_URL,
+      logRequest: true,
     })
     return response.result
-  } catch (error) { }
+  } catch (error) {
+    console.log(error)
+  }
 }
