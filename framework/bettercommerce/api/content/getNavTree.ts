1a78149fe04e57f082053124a5c193957f1537f8
Gagan <103169116+gagandeep-gingh-bt@users.noreply.github.com>
2022-06-02 18:49:50 +0530

    Changes to check getNav call on preview link

--- framework/bettercommerce/api/content/getNavTree.ts
+++ framework/bettercommerce/api/content/getNavTree.ts
@@ -1,19 +1,12 @@
 import fetcher from '../../fetcher'
 import { NAV_ENDPOINT } from '@components/utils/constants'
-//import { cachedGetData } from '../utils/cached-fetch'
+import { addCache, cachedGetData, lookupCache } from '../utils/cached-fetch'
 
 export default function getNavTree(cookies?: any) {
   async function getNavTreeAsync() {
     try {
       // TODO: This is not working on preview/prod. Works well on localhost.
-      //const response: any = await cachedGetData(NAV_ENDPOINT, cookies)
-      /////////////////////////////////////////////////////////////////////
-      
-      const response: any = await fetcher({
-        url: NAV_ENDPOINT,
-        method: 'GET',
-        cookies,
-      })
+      const response: any = await cachedGetData(NAV_ENDPOINT, cookies)
       return response.result
     } catch (error: any) {
       console.log(error)
