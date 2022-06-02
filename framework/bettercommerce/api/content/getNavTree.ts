fba528b907130745df182542b75de9c3da78b1be
Gagan <103169116+gagandeep-gingh-bt@users.noreply.github.com>
2022-06-02 18:25:20 +0530

    Comments added to indicate non-functional changes on preview/prod.

--- framework/bettercommerce/api/content/getNavTree.ts
+++ framework/bettercommerce/api/content/getNavTree.ts
@@ -1,9 +1,19 @@
+import fetcher from '../../fetcher'
 import { NAV_ENDPOINT } from '@components/utils/constants'
-import { cachedGetData } from '../utils/cached-fetch'
+//import { cachedGetData } from '../utils/cached-fetch'
+
 export default function getNavTree(cookies?: any) {
   async function getNavTreeAsync() {
     try {
-      const response: any = await cachedGetData(NAV_ENDPOINT, cookies)
+      // TODO: This is not working on preview/prod. Works well on localhost.
+      //const response: any = await cachedGetData(NAV_ENDPOINT, cookies)
+      /////////////////////////////////////////////////////////////////////
+      
+      const response: any = await fetcher({
+        url: NAV_ENDPOINT,
+        method: 'GET',
+        cookies,
+      })
       return response.result
     } catch (error: any) {
       console.log(error)
