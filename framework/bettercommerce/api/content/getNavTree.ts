9b9d0dffef5f5299a80f93aa9311a9ecf3711b75
Gagan <103169116+gagandeep-gingh-bt@users.noreply.github.com>
2022-06-02 17:43:26 +0530

    Restored getNavigation changes

--- framework/bettercommerce/api/content/getNavTree.ts
+++ framework/bettercommerce/api/content/getNavTree.ts
@@ -1,11 +1,8 @@
-import { NAV_ENDPOINT } from '@components/utils/constants'
-//import { cachedGetData } from '../utils/cached-fetch';
 import fetcher from '../../fetcher'
-
+import { NAV_ENDPOINT } from '@components/utils/constants'
 export default function getNavTree(cookies?: any) {
   async function getNavTreeAsync() {
     try {
-      //const response: any = await cachedGetData(NAV_ENDPOINT, cookies);
       const response: any = await fetcher({
         url: NAV_ENDPOINT,
         method: 'GET',
--- pages/api/get-navigation.ts
+++ pages/api/get-navigation.ts
@@ -1,10 +1,7 @@
-// import useNavTree from '@framework/api/endpoints/nav-tree'
 import getNavTree from '@framework/api/content/getNavTree'
 
 export default async (req: any, res: any) => {
   try {
-    // const response: any = await useNavTree(cookies)
-    // res.status(200).json({ nav: response?.result?.header, footer: response?.result?.footer })
     const response = await getNavTree(req.cookies)
     res.status(200).json({ nav: response.header, footer: response.footer })
   } catch (error) {
