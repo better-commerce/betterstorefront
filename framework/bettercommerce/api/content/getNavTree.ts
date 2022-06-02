0a382ea9fc505b45dba074c8b4237cf13e0a794b
Gagan <103169116+gagandeep-gingh-bt@users.noreply.github.com>
2022-06-02 18:08:37 +0530

    Fix for getNavigation not working on preview link

--- framework/bettercommerce/api/content/getNavTree.ts
+++ framework/bettercommerce/api/content/getNavTree.ts
@@ -1,13 +1,9 @@
-import fetcher from '../../fetcher'
 import { NAV_ENDPOINT } from '@components/utils/constants'
+import { cachedGetData } from '../utils/cached-fetch'
 export default function getNavTree(cookies?: any) {
   async function getNavTreeAsync() {
     try {
-      const response: any = await fetcher({
-        url: NAV_ENDPOINT,
-        method: 'GET',
-        cookies,
-      })
+      const response: any = await cachedGetData(NAV_ENDPOINT, cookies)
       return response.result
     } catch (error: any) {
       console.log(error)
