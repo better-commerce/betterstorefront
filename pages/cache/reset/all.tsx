fbc52e4c7d6208f8e0b22c29fd5f02eb0e0ffa33
Gagan <103169116+gagandeep-gingh-bt@users.noreply.github.com>
2022-06-07 10:41:09 +0530

    Added pages for cache reset.

---
+++ pages/cache/reset/all.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import { Layout } from '@components/common'
+
+export default function ResetAllPage() {
+
+    return (
+        <></>
+    )
+}
+
+ResetAllPage.Layout = Layout
