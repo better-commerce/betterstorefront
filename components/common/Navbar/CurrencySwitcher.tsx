cff2e2bcb9cb941c38fa2f56589e604c3ac46659
Amit Kumar <amit.kumar@bettercommerce.io>
2023-01-31 18:57:34 +0530

    Update

--- components/common/Navbar/CurrencySwitcher.tsx
+++ components/common/Navbar/CurrencySwitcher.tsx
@@ -5,13 +5,13 @@ import { setCookie } from '@components/utils/cookieHandler'
 
 export default function CurrencySwitcher({ config = [], title, action }: any) {
   return (
-    <Menu as="div" className="relative inline-block text-left px-1 sm:w-16 w-10 flow-root">
-      <Menu.Button className="group grid grid-cols-1 items-center text-center align-center justify-center flex-col mx-auto" aria-label="Currency">
+    <Menu as="div" className="relative flow-root w-10 px-1 text-left sm:w-16">
+      <Menu.Button className="grid flex-col items-center justify-center grid-cols-1 mx-auto text-center group align-center" aria-label="Currency">
         <CurrencyDollarIcon
-          className="flex-shrink-0 h-6 w-6 block text-black group-hover:text-gray-500 mx-auto"
+          className="flex-shrink-0 block w-6 h-6 mx-auto text-black group-hover:text-gray-500"
           aria-hidden="true" aria-label="Currency"
         />
-         <span className='font-normal sm:block hidden text-sm text-black'>Currency</span>
+         <span className='hidden text-sm font-normal text-black sm:block'>Currency</span>
       </Menu.Button>
       <Transition
         as={Fragment}
@@ -22,13 +22,13 @@ export default function CurrencySwitcher({ config = [], title, action }: any) {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute z-50 right-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <div className="px-1 py-1 flex flex-col text-gray-900 divide-y divide-gray-100">
+        <Menu.Items className="absolute right-0 z-50 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="flex flex-col px-1 py-1 text-gray-900 divide-y divide-gray-100">
             <Menu.Item>
               {({ active }) => {
                 return (
                   <>
-                    <h1 className="text-left font-bold p-2">{title}</h1>
+                    <h1 className="p-2 font-bold text-left">{title}</h1>
                     {config.map((item: any, idx: number) => {
                       return (
                         <div
