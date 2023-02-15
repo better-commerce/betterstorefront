1c1e793ed75f672619a4533c168b988c523a6ce8
Amit Kumar <amit.kumar@bettercommerce.io>
2023-02-15 13:48:17 +0530

    Update Build

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const isProduction = process.env.NODE_ENV == "production";
 const stylesHandler = MiniCssExtractPlugin.loader;
 
 const config = {
-  entry: "",
+  entry: "./pages/index.tsx",
   output: {
     path: path.resolve(__dirname, "dist"),
   },
