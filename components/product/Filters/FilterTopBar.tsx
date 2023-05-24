d1ec70ce270127eec6208e90938774755f88b4b2
Amit Kumar <amit.kumar@omnicx.com>
2023-05-24 05:52:46 +0000

    Merged PR 23529: [AK-6737] Update DOM for search result page
    - Update DOM for search result page
    - Update PLP
    - Fix SEO
    
    Related work items: #6737

--- components/product/Filters/FilterTopBar.tsx
+++ components/product/Filters/FilterTopBar.tsx
@@ -24,20 +24,22 @@ export default function FiltersRightOpen({ products = { filters: [] }, handleSor
   )
 
   return (
-    <section aria-labelledby="filter-heading" className="items-center justify-between flex-1 hidden max-w-3xl py-1 text-center border-b border-gray-200 sm:flex sm:pl-2 lg:max-w-7xl lg:pl-4">
+    <section aria-labelledby="filter-heading" className="items-center justify-between flex-1 hidden max-w-3xl py-1 text-center sm:mb-0 sm:flex sm:pl-2 lg:max-w-7xl lg:pl-4">
       <div className="relative col-start-1 row-start-1 py-2">
-        <div className="flex px-0 mx-auto space-x-6 text-sm divide-x divide-gray-200 max-w-7xl sm:px-0 lg:px-0">
-          <button onClick={clearAll} type="button" className="text-gray-500">{BTN_CLEAR_ALL}</button>
-          <div className="grid grid-cols-5 pl-2">
-            {appliedFilters?.map((appliedFilter: any, idx: number) => (
-              <div key={`applied-filter-${idx}`} className="flex justify-center text-gray-600">
-                {appliedFilter.name ? (
-                  <span className="px-3 text-sm font-medium text-gray-600 border border-gray-200 bg-gray-50 rounded-2xl">{appliedFilter.Value}</span>
-                ) : null}
-              </div>
-            ))}
+        {appliedFilters?.length > 0 ? (
+          <div className="flex px-0 mx-auto space-x-6 text-sm divide-x divide-gray-200 max-w-7xl sm:px-0 lg:px-0">
+            <button onClick={clearAll} type="button" className="text-gray-500">{BTN_CLEAR_ALL}</button>
+            <div className="grid grid-cols-5 pl-2">
+              {appliedFilters?.map((appliedFilter: any, idx: number) => (
+                <div key={`applied-filter-${idx}`} className="flex justify-center text-gray-600">
+                  {appliedFilter.name ? (
+                    <span className="px-3 text-sm font-medium text-gray-600 border border-gray-200 bg-gray-50 rounded-2xl">{appliedFilter.Value}</span>
+                  ) : null}
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
+        ) : null}
       </div>
       <ProductSort routerSortOption={routerSortOption} products={products} action={handleSortBy} />
     </section>
