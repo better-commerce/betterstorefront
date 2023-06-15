895a9492f48d23dcc21d4d01148ed7856a7b79c5
Gagan <gagandeep.singh@bettercommerce.io>
2023-06-16 04:24:20 +0530

    Resolved lint & compilation errors

--- next.config.js
+++ next.config.js
@@ -22,6 +22,7 @@ module.exports = withCommerceConfig({
       'liveocx.imgix.net',
       'devocxblob.blob.core.windows.net',
       'img.ffx.co.uk',
+      'ffxcdn.azureedge.net',
     ],
     // for trident need to add domain ('res.cloudinary.com', '99yrs.co.in') for images
   },
