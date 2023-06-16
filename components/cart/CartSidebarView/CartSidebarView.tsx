1588dd4a124f41a923b786e401d86e26960a2ff5
Shubham-betcom <125452919+Shubham-betcom@users.noreply.github.com>
2023-06-16 12:54:54 +0530

    Remove from cart not working Updates: 1) Remove from cart not working If more than one product is in cart, fxed

--- components/cart/CartSidebarView/CartSidebarView.tsx
+++ components/cart/CartSidebarView/CartSidebarView.tsx
@@ -924,92 +924,6 @@ const CartSidebarView: FC<React.PropsWithChildren<IExtraProps>> = ({
                                           {/* <p className="text-gray-500">Qty {product.quantity}</p> */}
 
                                           <div className="flex items-center justify-between w-full">
-                                            <Transition
-                                              appear
-                                              show={isOpen}
-                                              as={Fragment}
-                                            >
-                                              <Dialog
-                                                as="div"
-                                                open={isOpen}
-                                                className="relative z-9999"
-                                                onClose={closeModal}
-                                              >
-                                                <Transition.Child
-                                                  as={Fragment}
-                                                  enter="ease-out duration-300"
-                                                  enterFrom="opacity-0"
-                                                  enterTo="opacity-30"
-                                                  leave="ease-in duration-300"
-                                                  leaveFrom="opacity-30"
-                                                  leaveTo="opacity-0"
-                                                >
-                                                  <div className="fixed inset-0 bg-black " />
-                                                </Transition.Child>
-
-                                                <div className="fixed inset-0 overflow-y-auto">
-                                                  <div className="flex items-center justify-center min-h-full p-4 text-center">
-                                                    <Transition.Child
-                                                      as={Fragment}
-                                                      enter="transition duration-100 ease-out"
-                                                      enterFrom="transform scale-95 opacity-0"
-                                                      enterTo="transform scale-100 opacity-100"
-                                                      leave="transition duration-75 ease-out"
-                                                      leaveFrom="transform scale-100 opacity-100"
-                                                      leaveTo="transform scale-95 opacity-0"
-                                                    >
-                                                      <Dialog.Panel className="w-full max-w-md pb-6 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl">
-                                                        <Dialog.Title
-                                                          as="div"
-                                                          className="flex justify-between w-full px-6 py-3 text-lg font-medium leading-6 text-gray-900 border-b-2 shadow xsm:text-md border-gray-50"
-                                                        >
-                                                          Remove this Item?
-                                                          <XMarkIcon
-                                                            className="w-5 h-5 text-gray-500 hover:text-gray-400"
-                                                            onClick={closeModal}
-                                                          ></XMarkIcon>
-                                                        </Dialog.Title>
-                                                        {/* <hr className="w-full my-2 shadow-md "></hr> */}
-                                                        <p className="p-6 text-sm font-normal text-black">
-                                                          Are you sure you don't
-                                                          want this product? You
-                                                          may move it to
-                                                          Wishlist and buy
-                                                          later.
-                                                        </p>
-                                                        <div className="flex items-center justify-around w-full px-6 mt-2">
-                                                          <button
-                                                            onClick={() => {
-                                                              handleItem(
-                                                                itemClicked,
-                                                                'delete'
-                                                              )
-                                                            }}
-                                                            className="flex items-center justify-center w-full h-16 px-6 py-2 mx-3 text-sm font-medium text-red-700 bg-white border border-gray-300 shadow-sm lg:text-md hover:bg-gray-100 md:w-full"
-                                                          >
-                                                            {GENERAL_REMOVE +
-                                                              '$'}
-                                                          </button>
-                                                          <button
-                                                            onClick={() => {
-                                                              handleWishList(
-                                                                itemClicked
-                                                              )
-                                                            }}
-                                                            className="flex items-center justify-center w-full h-16 px-6 py-2 mx-3 text-sm font-medium text-gray-700 bg-white border border-gray-300 shadow-sm hover:bg-gray-100 md:w-full"
-                                                          >
-                                                            {
-                                                              BTN_MOVE_TO_WISHLIST
-                                                            }
-                                                          </button>
-                                                        </div>
-                                                      </Dialog.Panel>
-                                                    </Transition.Child>
-                                                  </div>
-                                                </div>
-                                              </Dialog>
-                                            </Transition>
-
                                             {product?.variantProducts?.length >
                                             0 ? (
                                               <div></div>
@@ -1104,6 +1018,77 @@ const CartSidebarView: FC<React.PropsWithChildren<IExtraProps>> = ({
                     <div className="flex flex-col">
                       <div className="section-devider-sm"></div>
                     </div>
+
+                    <Transition appear show={isOpen} as={Fragment}>
+                      <Dialog
+                        as="div"
+                        open={isOpen}
+                        className="relative z-9999"
+                        onClose={closeModal}
+                      >
+                        <Transition.Child
+                          as={Fragment}
+                          enter="ease-out duration-300"
+                          enterFrom="opacity-0"
+                          enterTo="opacity-30"
+                          leave="ease-in duration-300"
+                          leaveFrom="opacity-30"
+                          leaveTo="opacity-0"
+                        >
+                          <div className="fixed inset-0 bg-black " />
+                        </Transition.Child>
+
+                        <div className="fixed inset-0 overflow-y-auto">
+                          <div className="flex items-center justify-center min-h-full p-4 text-center">
+                            <Transition.Child
+                              as={Fragment}
+                              enter="transition duration-100 ease-out"
+                              enterFrom="transform scale-95 opacity-0"
+                              enterTo="transform scale-100 opacity-100"
+                              leave="transition duration-75 ease-out"
+                              leaveFrom="transform scale-100 opacity-100"
+                              leaveTo="transform scale-95 opacity-0"
+                            >
+                              <Dialog.Panel className="w-full max-w-md pb-6 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl">
+                                <Dialog.Title
+                                  as="div"
+                                  className="flex justify-between w-full px-6 py-3 text-lg font-medium leading-6 text-gray-900 border-b-2 shadow xsm:text-md border-gray-50"
+                                >
+                                  Remove this Item?
+                                  <XMarkIcon
+                                    className="w-5 h-5 text-gray-500 hover:text-gray-400"
+                                    onClick={closeModal}
+                                  ></XMarkIcon>
+                                </Dialog.Title>
+                                {/* <hr className="w-full my-2 shadow-md "></hr> */}
+                                <p className="p-6 text-sm font-normal text-black">
+                                  Are you sure you don't want this product? You
+                                  may move it to Wishlist and buy later.
+                                </p>
+                                <div className="flex items-center justify-around w-full px-6 mt-2">
+                                  <button
+                                    onClick={() => {
+                                      handleItem(itemClicked, 'delete')
+                                    }}
+                                    className="flex items-center justify-center w-full h-16 px-6 py-2 mx-3 text-sm font-medium text-red-700 bg-white border border-gray-300 shadow-sm lg:text-md hover:bg-gray-100 md:w-full"
+                                  >
+                                    {GENERAL_REMOVE}
+                                  </button>
+                                  <button
+                                    onClick={() => {
+                                      handleWishList(itemClicked)
+                                    }}
+                                    className="flex items-center justify-center w-full h-16 px-6 py-2 mx-3 text-sm font-medium text-gray-700 bg-white border border-gray-300 shadow-sm hover:bg-gray-100 md:w-full"
+                                  >
+                                    {BTN_MOVE_TO_WISHLIST}
+                                  </button>
+                                </div>
+                              </Dialog.Panel>
+                            </Transition.Child>
+                          </div>
+                        </div>
+                      </Dialog>
+                    </Transition>
 
                     {!isEmpty && (
                       <div className="sticky bottom-0 pt-4 pb-1 mt-2 ml-5 bg-white">
