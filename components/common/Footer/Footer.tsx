1eacaf0d6036114623b9436fdf175671120703be
Vinay Sharma <vinay.sharma@bettercommerce.io>
2023-06-19 11:42:20 +0530

    Fix Home Page Heading Issue

--- components/common/Footer/Footer.tsx
+++ components/common/Footer/Footer.tsx
@@ -54,6 +54,9 @@ const Footer: FC<Props & IExtraProps> = ({ config, deviceInfo }) => {
       aria-labelledby="footer-heading"
       className="pt-10 bg-gray-100 shadow-inner sm:h-96 sm:pt-16 sm:mt-2 bg-footer-color"
     >
+      <h3 id="footer-heading" className="sr-only">
+        {GENERAL_FOOOTER}
+      </h3>
       <div className="container grid grid-cols-1 mx-auto sm:grid-cols-12">
         <div className="sm:col-span-1">
           <Logo />
@@ -129,9 +132,6 @@ const Footer: FC<Props & IExtraProps> = ({ config, deviceInfo }) => {
           </p>
         </div>
       </div>
-      <h2 id="footer-heading" className="sr-only">
-        {GENERAL_FOOOTER}
-      </h2>
     </footer>
   )
 }
