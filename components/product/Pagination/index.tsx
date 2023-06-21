7b43ef7b4ed9be5392634c9fc571d3cd8cebc65a
Gagan <gagandeep.singh@bettercommerce.io>
2023-06-21 10:46:50 +0530

    Enable hrefs in link for crawling

--- components/product/Pagination/index.tsx
+++ components/product/Pagination/index.tsx
@@ -10,15 +10,15 @@ interface Props {
 }
 const Pagination: FC<React.PropsWithChildren<Props>> = ({
   currentPage = '1',
-  onPageChange = () => {},
+  onPageChange = () => { },
   pageCount = 20,
 }) => {
   return (
     <>
       <ReactPaginate
         previousLabel={'Previous'}
-        previousClassName={classNames(Number(currentPage)===1?'cursor-not-allowed opacity-30':'',` py-1 mr-4 inline-flex items-center px-4 h-10 border border-gray-300 rounded-md bg-white hover:bg-gray-100 focus:outline-none focus:border-indigo-600 focus:ring-2 focus:ring-offset-1 focus:ring-offset-indigo-600 focus:ring-indigo-600 focus:ring-opacity-25`)}
-        nextClassName={classNames(Number(currentPage)===pageCount?'cursor-not-allowed opacity-30':'',"py-1 ml-4 inline-flex items-center px-4 h-10 border border-gray-300 rounded-md bg-white hover:bg-gray-100 focus:outline-none focus:border-indigo-600 focus:ring-2 focus:ring-offset-1 focus:ring-offset-indigo-600 focus:ring-indigo-600 focus:ring-opacity-25")}
+        previousClassName={classNames(Number(currentPage) === 1 ? 'cursor-not-allowed opacity-30' : '', ` py-1 mr-4 inline-flex items-center px-4 h-10 border border-gray-300 rounded-md bg-white hover:bg-gray-100 focus:outline-none focus:border-indigo-600 focus:ring-2 focus:ring-offset-1 focus:ring-offset-indigo-600 focus:ring-indigo-600 focus:ring-opacity-25`)}
+        nextClassName={classNames(Number(currentPage) === pageCount ? 'cursor-not-allowed opacity-30' : '', "py-1 ml-4 inline-flex items-center px-4 h-10 border border-gray-300 rounded-md bg-white hover:bg-gray-100 focus:outline-none focus:border-indigo-600 focus:ring-2 focus:ring-offset-1 focus:ring-offset-indigo-600 focus:ring-indigo-600 focus:ring-opacity-25")}
         nextLabel={'Next'}
         breakLabel={'...'}
         breakClassName={'inline-flex items-center text-gray-500 px-1.5 h-10'}
@@ -32,6 +32,12 @@ const Pagination: FC<React.PropsWithChildren<Props>> = ({
         pageLinkClassName={
           'inline-flex items-center px-4 h-10 border border-gray-300 rounded-md bg-white hover:bg-gray-100 focus:outline-none focus:border-indigo-600 focus:ring-2 focus:ring-offset-1 focus:ring-offset-indigo-600 focus:ring-indigo-600 focus:ring-opacity-25'
         }
+        hrefBuilder={(pageIndex: number, pageCount: number, selectedPage: number) => {
+          if (typeof window !== "undefined") {
+            return `${window.location.pathname}?currentPage=${pageIndex}`
+          }
+          return `#`
+        }}
         onPageChange={onPageChange}
         containerClassName={
           'max-w-7xl mx-auto px-4 mt-6 flex justify-center items-center text-sm font-medium text-gray-700 sm:px-6 lg:px-8'
