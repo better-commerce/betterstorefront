738c0b6f0efbb4261ffa1fdb84650674ecdb6ab3
Gagan <gagandeep.singh@bettercommerce.io>
2023-07-04 22:45:22 +0530

    b2b Price display on brands page

--- pages/brands/[...brand].tsx
+++ pages/brands/[...brand].tsx
@@ -271,8 +271,8 @@ function BrandDetailPage({
 
   const handleInfiniteScroll = () => {
     if (
-      data.products.pages &&
-      data.products.currentPage < data.products.pages
+      data?.products?.pages &&
+      data?.products?.currentPage < data?.products?.pages
     ) {
       dispatch({ type: PAGE, payload: data.products.currentPage + 1 })
     }
@@ -360,10 +360,10 @@ function BrandDetailPage({
     })
   }, [])
 
-  const productDataToPass = productListMemory.products
-  /*const productDataToPass = IS_INFINITE_SCROLL
+  //const productDataToPass = productListMemory.products
+  const productDataToPass = IS_INFINITE_SCROLL
     ? productListMemory.products
-    : data.products*/
+    : data?.products
 
   useEffect(() => {
     setRecommendedProducts(productDataToPass.results.slice(0, 8))
