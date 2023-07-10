900c110ad3e055936dc28baef8d83e18ac5882d7
Gagan <gagandeep.singh@bettercommerce.io>
2023-07-10 16:34:19 +0530

    Pass cookies to api middlewares

--- pages/api/catalog/get-product-quick-view.ts
+++ pages/api/catalog/get-product-quick-view.ts
@@ -5,11 +5,12 @@ const GetProductQuickviewApiMiddleware = async (req: any, res: any) => {
   try {
     const response = await commerce.getProductQuickview({
       query: req.body.slug,
+      cookies: req?.cookies,
     })
     res.status(200).json(response)
   } catch (error) {
     apiMiddlewareErrorHandler(req, res, error)
   }
-};
+}
 
-export default GetProductQuickviewApiMiddleware;
+export default GetProductQuickviewApiMiddleware
