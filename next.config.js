723024532f0d826c7c4edca9d9be2edf59933944
Amit Kumar <amit.kumar@bettercommerce.io>
2023-07-11 18:04:02 +0530

    Fix Domain

--- next.config.js
+++ next.config.js
@@ -26,7 +26,7 @@ module.exports = withCommerceConfig({
       'ffxlivestorage.blob.core.windows.net',
       'livekstmcdn.azureedge.net',
       'devocxstorage.blob.core.windows.net',
-      'imagedelivery.space'
+      'www.imagedelivery.space',
     ],
     // for trident need to add domain ('res.cloudinary.com', '99yrs.co.in') for images
   },
@@ -92,6 +92,7 @@ module.exports = withCommerceConfig({
     PDP_SIZE_OPTIONS_COUNT: process.env.PDP_SIZE_OPTIONS_COUNT,
     PRODUCT_IMAGE_CDN_URL: process.env.PRODUCT_IMAGE_CDN_URL,
     OMNILYTICS_DISABLED: process.env.OMNILYTICS_DISABLED,
+    ENABLE_ELASTIC_SEARCH: process.env.ENABLE_ELASTIC_SEARCH,
   },
   async headers() {
     return [
