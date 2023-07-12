71cd432ea37873e99d65eb4997f6145d1aa248f5
Shubham-betcom <125452919+Shubham-betcom@users.noreply.github.com>
2023-07-12 16:52:33 +0530

    message case change

--- components/utils/textVariables.ts
+++ components/utils/textVariables.ts
@@ -326,7 +326,7 @@ export const GENERAL_DELIVERED_BY = 'Delivered by'
 export const GENERAL_SUMMARY = 'Summary'
 export const GENERAL_NEXT_ORDER_PROMO = 'Congratulations! You can get a discount on your next order using code'
 export const VALIDATION_YOU_ARE_ALREADY_LOGGED_IN = "You're already logged in"
-export const OFFER_VALIDITY='Your offer is valid for {days} Days'
+export const OFFER_VALIDITY='Your offer is valid for {days} days'
 export const VALIDATION_NO_ACCOUNT_FOUND =
   'No account has been found with this email/password'
 export const VALIDATION_NO_ACCOUNT_FOUND_VIA_OTP =
