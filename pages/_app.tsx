fc8271fd83b0d15d45cb242fb2d1958f8d4ea5e1
Gagan <gagandeep.singh@bettercommerce.io>
2023-07-13 18:44:40 +0530

    Revert "Merged PR 24305: Modify Search UI"
    This reverts commit 2191a2457b44289d7193d38598e12438d709d64a, reversing
    changes made to ef585b6f1fc0c063b7cc37fcf2ea37dca7ea6292.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -55,6 +55,31 @@ import { tryParseJson } from '@framework/utils/parse-util'
 import { maxBasketItemsCount } from '@framework/utils/app-util'
 import { SessionProvider } from 'next-auth/react'
 import { OMNILYTICS_DISABLED } from '@framework/utils/constants'
+import { SearchProvider } from '@elastic/react-search-ui'
+
+import {
+  buildAutocompleteQueryConfig,
+  buildFacetConfigFromConfig,
+  buildSearchOptionsFromConfig,
+  getConfig,
+} from '@components/config/config-helper'
+import AppSearchAPIConnector from '@elastic/search-ui-app-search-connector'
+const { hostIdentifier, searchKey, endpointBase, engineName } = getConfig()
+const connector = new AppSearchAPIConnector({
+  searchKey,
+  engineName,
+  hostIdentifier,
+  endpointBase,
+})
+const elasticConfig = {
+  searchQuery: {
+    facets: buildFacetConfigFromConfig(),
+    ...buildSearchOptionsFromConfig(),
+  },
+  autocompleteQuery: buildAutocompleteQueryConfig(),
+  apiConnector: connector,
+  alwaysSearchOnInitialLoad: true,
+}
 
 const tagManagerArgs: any = {
   gtmId: process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID,
@@ -309,34 +334,36 @@ function MyApp({
       <Head {...appConfig}></Head>
       {OMNILYTICS_DISABLED ? null : <div id="google_translate_element" />}
 
-      <ManagedUIContext>
-        {snippets ? <ContentSnippet {...{ snippets }} /> : <></>}
-        <CustomCacheBuster buildVersion={buildVersion} />
-        <InitDeviceInfo setDeviceInfo={setDeviceInfo} />
-        <ErrorBoundary>
-          <Layout
-            nav={nav}
-            footer={footer}
-            config={appConfig}
-            pageProps={pageProps}
-            keywords={keywordsData}
-            deviceInfo={deviceInfo}
-            maxBasketItemsCount={maxBasketItemsCount(appConfig)}
-          >
-            <OverlayLoader />
-            <SessionProvider session={pageProps?.session}>
-              <Component
-                {...pageProps}
-                location={location}
-                ipAddress={location.Ip}
-                config={appConfig}
-                deviceInfo={deviceInfo}
-              />
-            </SessionProvider>
-            {/* <RedirectIntercept /> */}
-          </Layout>
-        </ErrorBoundary>
-      </ManagedUIContext>
+      <SearchProvider config={elasticConfig}>
+        <ManagedUIContext>
+          {snippets ? <ContentSnippet {...{ snippets }} /> : <></>}
+          <CustomCacheBuster buildVersion={buildVersion} />
+          <InitDeviceInfo setDeviceInfo={setDeviceInfo} />
+          <ErrorBoundary>
+            <Layout
+              nav={nav}
+              footer={footer}
+              config={appConfig}
+              pageProps={pageProps}
+              keywords={keywordsData}
+              deviceInfo={deviceInfo}
+              maxBasketItemsCount={maxBasketItemsCount(appConfig)}
+            >
+              <OverlayLoader />
+              <SessionProvider session={pageProps?.session}>
+                <Component
+                  {...pageProps}
+                  location={location}
+                  ipAddress={location.Ip}
+                  config={appConfig}
+                  deviceInfo={deviceInfo}
+                />
+              </SessionProvider>
+              {/* <RedirectIntercept /> */}
+            </Layout>
+          </ErrorBoundary>
+        </ManagedUIContext>
+      </SearchProvider>
     </>
   )
 }
