42c49ee6d0a2be8c75cda85d9fc7551666bb9f51
Gagan <gagandeep.singh@bettercommerce.io>
2023-07-19 18:13:53 +0530

    Added component for payment link redirect

---
+++ components/payment/PaymentLinkRedirect.tsx
@@ -0,0 +1,62 @@
+// Base Imports
+import React, { useEffect, useState } from 'react'
+
+// Component Imports
+import Spinner from '@components/ui/Spinner'
+
+// Other Imports
+import { useUI } from '@components/ui'
+
+const PAYMENT_LINK_ALLOWED_URLS = [
+  '/checkout',
+  '/payment-notification',
+  '/thank-you',
+  '/payment-failed',
+]
+
+const PaymentLinkRedirect = ({ router }: any) => {
+  const { isPaymentLink } = useUI()
+  //const [showLoader, setShowLoader] = useState(false)
+
+  const redirect = (isPaymentLink: boolean, url: string) => {
+    if (isPaymentLink) {
+      const newUrl = url === '/' ? '' : url
+      const lookupUrl = PAYMENT_LINK_ALLOWED_URLS?.find((x: string) =>
+        x?.startsWith(newUrl)
+      )
+
+      if (!newUrl || !lookupUrl) {
+        //setShowLoader(true)
+        router.push('/checkout')
+      }
+    }
+  }
+
+  useEffect(() => {
+    redirect(isPaymentLink, window.location.pathname)
+  }, [])
+
+  useEffect(() => {
+    // Listener for snippet injector reset.
+    router.events.on('routeChangeStart', (newUrl: string, options: any) => {
+      redirect(isPaymentLink, newUrl)
+    })
+
+    // Dispose listener.
+    return () => {
+      router.events.off('routeChangeStart', () => {})
+    }
+  }, [router.events])
+
+  return (
+    <>
+      {/*{
+                showLoader && (
+                    <Spinner />
+                )
+            }*/}
+    </>
+  )
+}
+
+export default PaymentLinkRedirect
--- pages/quote/[...paymentCode].tsx
+++ pages/quote/[...paymentCode].tsx
@@ -1,24 +1,106 @@
 // Base Imports
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 // Package Imports
 import axios from 'axios'
+import Router from 'next/router'
 import { GetServerSideProps } from 'next'
+import { signOut } from 'next-auth/react'
 
 // Component Imports
 import Spinner from '@components/ui/Spinner'
 
 // Other Imports
-import { NEXT_VALIDATE_PAYMENT_LINK } from '@components/utils/constants'
+import { Guid } from '@commerce/types'
+import { useUI } from '@components/ui'
+import {
+  Messages,
+  NEXT_GET_CART,
+  NEXT_GET_CUSTOMER_DETAILS,
+  NEXT_VALIDATE_PAYMENT_LINK,
+} from '@components/utils/constants'
+import { AlertType } from '@framework/utils/enums'
 
 const PaymentLinkPage = ({ paymentCode, deviceInfo, config }: any) => {
+  const {
+    user,
+    setUser,
+    deleteUser,
+    setBasketId,
+    setCartItems,
+    setIsPaymentLink,
+    setAlert,
+  } = useUI()
+  const [showLoader, setShowLoader] = useState(true)
+
+  const linkExpired = () => {
+    setShowLoader(false)
+    setAlert({
+      type: AlertType.ERROR,
+      msg: Messages.Validations.PaymentLink['LINK_EXPIRED'],
+    })
+  }
+
   useEffect(() => {
     const asyncHandler = async (paymentLink: string) => {
-      const validatePaymentLinkResult: any = await axios.post(
+      const { data: validatePaymentLinkResult }: any = await axios.post(
         NEXT_VALIDATE_PAYMENT_LINK,
         { data: paymentLink }
       )
-      console.log('validatePaymentLinkResult', validatePaymentLinkResult)
+
+      if (
+        !validatePaymentLinkResult?.result?.quoteId ||
+        (validatePaymentLinkResult?.result?.quoteId &&
+          validatePaymentLinkResult?.result?.quoteId === Guid.empty)
+      ) {
+        // If quote id not received.
+        linkExpired()
+      } else {
+        const userId = validatePaymentLinkResult?.result?.customerId
+        const basketId = validatePaymentLinkResult?.result?.quoteId
+        if (
+          basketId &&
+          basketId !== Guid.empty &&
+          userId &&
+          userId !== Guid.empty
+        ) {
+          // If there is a user logged-in.
+          if (user?.userId) {
+            deleteUser({
+              isSilentLogout: true,
+            })
+
+            if (user?.socialData?.socialMediaType) {
+              await signOut()
+            }
+          }
+
+          const { data: getCustomerResult }: any = await axios.post(
+            `${NEXT_GET_CUSTOMER_DETAILS}?customerId=${userId}`
+          )
+
+          const { data: getBasketResult }: any = await axios.get(
+            NEXT_GET_CART,
+            {
+              params: { basketId },
+            }
+          )
+          if (getBasketResult && getBasketResult?.id) {
+            setCartItems(getBasketResult)
+            setBasketId(getBasketResult?.id)
+            getCustomerResult.isAssociated = true
+          } else {
+            getCustomerResult.isAssociated = false
+          }
+          setUser(getCustomerResult)
+          setIsPaymentLink(true)
+          setTimeout(() => {
+            Router.push('/checkout')
+          }, 200)
+        } else {
+          linkExpired()
+        }
+      }
     }
 
     if (paymentCode) {
@@ -26,11 +108,7 @@ const PaymentLinkPage = ({ paymentCode, deviceInfo, config }: any) => {
     }
   }, [paymentCode])
 
-  return (
-    <>
-      <Spinner />
-    </>
-  )
+  return <>{showLoader && <Spinner />}</>
 }
 
 export const getServerSideProps: GetServerSideProps = async (context: any) => {
