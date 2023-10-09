41eec923791fc10ebc030a440cd9b33a58775582
Amit Kumar <amit.kumar@bettercommerce.io>
2023-10-09 13:41:24 +0530

    Update Content Snippet

--- components/common/Content/ContentSnippet.tsx
+++ components/common/Content/ContentSnippet.tsx
@@ -1,15 +1,18 @@
 // Base Imports
-import React, { memo } from "react";
+import React, { memo } from 'react'
+
+// Package Imports
+import { uniqBy } from 'lodash'
 
 // Other Imports
-import { useContentSnippet } from "@framework";
+import { useContentSnippet } from '@framework'
 
 const ContentSnippet: React.FC<React.PropsWithChildren<any>> = (props: any) => {
-    const { snippets } = props;
-    useContentSnippet(snippets);
-    return (
-        <></>
-    );
-};
+  const { refs } = props
+  let { snippets } = props
+  snippets = uniqBy(snippets, 'name') // to prevent duplicity
+  useContentSnippet(snippets, refs)
+  return <></>
+}
 
-export default memo(ContentSnippet);
+export default memo(ContentSnippet)
