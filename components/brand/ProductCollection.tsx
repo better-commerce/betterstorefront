1231460eeef627338182d09d7b61ea03124e3809
Amit Kumar <amit.kumar@bettercommerce.io>
2023-10-17 17:34:45 +0530

    Update PLP Sort By Issue Fix

--- components/brand/ProductCollection.tsx
+++ components/brand/ProductCollection.tsx
@@ -52,13 +52,13 @@ const DEFAULT_STATE = {
 function reducer(state: stateInterface, { type, payload }: actionInterface) {
   switch (type) {
     case SORT_BY:
-      return { ...state, sortBy: payload }
+      return { ...state, sortBy: payload, currentPage: 1 }
     case PAGE:
       return { ...state, currentPage: payload }
     case SORT_ORDER:
       return { ...state, sortOrder: payload }
     case CLEAR:
-      return { ...state, filters: [] }
+      return { ...state, currentPage: 1, filters: [] }
     case HANDLE_FILTERS_UI:
       return { ...state, areFiltersOpen: payload }
     case ADD_FILTERS:
--- pages/collection/[...collection].tsx
+++ pages/collection/[...collection].tsx
@@ -92,13 +92,13 @@ const DEFAULT_STATE = {
 function reducer(state: stateInterface, { type, payload }: actionInterface) {
   switch (type) {
     case SORT_BY:
-      return { ...state, sortBy: payload }
+      return { ...state, sortBy: payload, currentPage: 1 }
     case PAGE:
       return { ...state, currentPage: payload }
     case SORT_ORDER:
       return { ...state, sortOrder: payload }
     case CLEAR:
-      return { ...state, filters: [] }
+      return { ...state, currentPage: 1, filters: [] }
     case HANDLE_FILTERS_UI:
       return { ...state, areFiltersOpen: payload }
     case ADD_FILTERS:
@@ -552,7 +552,7 @@ export default function CollectionPage(props: any) {
         </div>
 
         {productDataToPass?.results?.length > 0 && (
-          <div className="grid grid-cols-1 lg:grid-cols-12 md:grid-cols-3 gap-1 overflow-hidden sm:grid-cols-3">
+          <div className="grid grid-cols-1 gap-1 overflow-hidden lg:grid-cols-12 md:grid-cols-3 sm:grid-cols-3">
             {props?.allowFacets ? (
               <>
                 {isMobile ? (
