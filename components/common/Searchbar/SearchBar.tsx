d9203714de6643dd7b24b02edef98f718ab79752
Amit Kumar <amit.kumar@bettercommerce.io>
2023-10-18 10:51:00 +0530

    Update typo

--- components/common/Searchbar/SearchBar.tsx
+++ components/common/Searchbar/SearchBar.tsx
@@ -68,27 +68,13 @@ const SearchBar: FC<React.PropsWithChildren<ISearchBarProps>> = ({
   }
 
   return (
-    <>
-      <div className="flex flex-row rounded-sm sm:mr-8 sm:pr-2 sm:bg-gray-100 sm:border">
-        <button
-          onClick={onClick}
-          className="relative pr-3 text-gray-400 sm:p-1 sm:pl-3 sm:pr-16 hover:text-gray-500"
-          aria-label="Search"
-        >
-          <span className="sr-only" aria-label="Search">
-            {BTN_SEARCH}
-          </span>
-          <span className="hidden pr-2 text-sm font-normal text-black sm:inline-block sm:pr-32">
-            Search
-          </span>
-          <MagnifyingGlassIcon
-            className="w-6 h-6 text-black sm:w-4 sm:h-4 sm:absolute sm:top-2 sm:right-0 sm:text-gray-400"
-            aria-hidden="true"
-            aria-label="Search"
-          />
-        </button>
-      </div>
-    </>
+    <div className="flex flex-row rounded-sm sm:mr-8 sm:pr-2 sm:bg-gray-100 sm:border">
+      <button onClick={onClick} className="relative pr-3 text-gray-400 sm:p-1 sm:pl-3 sm:pr-16 hover:text-gray-500" aria-label="Search">
+        <span className="sr-only" aria-label="Search">{BTN_SEARCH}</span>
+        <span className="hidden pr-2 text-sm font-normal text-black sm:inline-block sm:pr-32">Search</span>
+        <MagnifyingGlassIcon className="w-6 h-6 text-black sm:w-4 sm:h-4 sm:absolute sm:top-2 sm:right-0 sm:text-gray-400" aria-hidden="true" aria-label="Search" />
+      </button>
+    </div>
   )
 }
 
@@ -175,7 +161,7 @@ export default memo(SearchBar)
 //   )
 //   const elasticSearch = (
 //     <SearchProvider config={elasticConfig}>
-//       <div className="flex flex-row w-full rounded-sm sm:pr-4 mobile-serach-ui">
+//       <div className="flex flex-row w-full rounded-sm sm:pr-4 mobile-search-ui">
 //         <div
 //           className="relative items-start w-full pr-3 text-left text-gray-400 sm:p-1 hover:text-gray-500 search-input-icon"
 //           aria-label="Search"
@@ -195,7 +181,7 @@ export default memo(SearchBar)
 //     </SearchProvider>
 //   )
 //   const instantSearch = (
-//     <div className="flex flex-row w-full rounded-sm instant-search-bar sm:pr-4 mobile-serach-ui search-input-icon">
+//     <div className="flex flex-row w-full rounded-sm instant-search-bar sm:pr-4 mobile-search-ui search-input-icon">
 //       <InstantSearchBar maxBasketItemsCount={maxBasketItemsCount} deviceInfo={deviceInfo} />
 //     </div>
 
