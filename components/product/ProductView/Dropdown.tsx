b41c5e558308e05a77cbbd8693d8ea859961e077
Amit Kumar <amit.kumar@bettercommerce.io>
2023-11-17 12:12:21 +0530

    Update

--- components/product/ProductView/Dropdown.tsx
+++ components/product/ProductView/Dropdown.tsx
@@ -8,7 +8,7 @@ import { getProductFromAttributes } from '@components/utils/attributesGenerator'
 
 export default function Dropdown({
   items = [],
-  onChange = () => {},
+  onChange = () => { },
   label = '',
   fieldCode = '',
   currentAttribute = '',
@@ -100,87 +100,56 @@ export default function Dropdown({
   }
 
   return (
-    <>
-      <div className="py-1 my-2 border-t border-b">
-        <Listbox value={selected} onChange={handleOnChange} disabled={isDisabled}>
-          <Listbox.Label
-            className={`${
-              isDisabled ? 'opacity-40' : ''
-            } inline-block text-lg pr-3 uppercase font-bold text-gray-600 text-left`}
+    <div className="py-1 my-2 border-t border-b">
+      <Listbox value={selected} onChange={handleOnChange} disabled={isDisabled}>
+        <Listbox.Label className={`${isDisabled ? 'opacity-40' : ''} inline-block text-lg pr-3 uppercase font-bold text-gray-600 text-left`}>
+          {label}
+        </Listbox.Label>
+        <div className="relative inline-block w-9/12 mt-1">
+          <Listbox.Button className={`${isDisabled ? 'opacity-40' : ''} relative w-full bg-white pl-3 pr-10 py-1 text-left cursor-default focus:outline-none focus:ring-1 sm:text-md`}>
+            <span className="flex items-center">
+              <span style={{ minHeight: '20px' }} className="block ml-3 font-medium text-black truncate">
+                {selected.currentAttribute ? generateItemOption(selected.currentAttribute, selected.stock) : ' '}
+              </span>
+            </span>
+            <span className="absolute inset-y-0 right-0 flex items-center pr-2 ml-3 pointer-events-none">
+              <ChevronDownIcon className="w-6 h-6 text-black" aria-hidden="true" />
+            </span>
+          </Listbox.Button>
+
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
           >
-            {label}
-          </Listbox.Label>
-          <div className="relative inline-block w-9/12 mt-1">
-            <Listbox.Button
-              className={`${
-                isDisabled ? 'opacity-40' : ''
-              } relative w-full bg-white pl-3 pr-10 py-1 text-left cursor-default focus:outline-none focus:ring-1 sm:text-md`}
-            >
-              <span className="flex items-center">
-                <span
-                  style={{ minHeight: '20px' }}
-                  className="block ml-3 font-medium text-black truncate"
-                >
-                  {selected.currentAttribute
-                    ? generateItemOption(selected.currentAttribute, selected.stock)
-                    : ' '}
-                </span>
-              </span>
-              <span className="absolute inset-y-0 right-0 flex items-center pr-2 ml-3 pointer-events-none">
-                <ChevronDownIcon
-                  className="w-6 h-6 text-black"
-                  aria-hidden="true"
-                />
-              </span>
-            </Listbox.Button>
-
-            <Transition
-              as={Fragment}
-              leave="transition ease-in duration-100"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <Listbox.Options className="absolute z-10 w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-56 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                {items.map((item: any) => {
-                  const stockAmount = getStockPerAttribute(
-                    fieldCode,
-                    item.fieldValue
-                  ).stock
-                  return (
-                    <Listbox.Option
-                      key={item.fieldValue}
-                      className={({ active }) => classNames(active ? 'text-white bg-indigo-600' : 'text-gray-900', 'cursor-default select-none relative py-2 pl-3 pr-9')}
-                      value={{currentAttribute: item.fieldValue, stock: stockAmount,}}>
-                      {({ selected, active }) => (
-                        <>
-                          <div className="flex items-center">
-                            <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'ml-3 block truncate')}>
-                              {item.fieldValue
-                                ? generateItemOption(item.fieldValue, stockAmount)
-                                : null}
-                            </span>
-                          </div>
-
-                          {selected ? (
-                            <span
-                              className={classNames(
-                                active ? 'text-white' : 'text-indigo-600',
-                                'absolute inset-y-0 right-0 flex items-center pr-4'
-                              )}
-                            >
-                              <CheckIcon className="w-5 h-5" aria-hidden="true" />
-                            </span>
-                          ) : null}
-                        </>
-                      )}
-                    </Listbox.Option>
-                  )
-                })}
-              </Listbox.Options>
-            </Transition>
-          </div>
-        </Listbox>
-      </div>
-    </>    
+            <Listbox.Options className="absolute z-10 w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-56 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {items.map((item: any) => {
+                const stockAmount = getStockPerAttribute(fieldCode, item.fieldValue).stock
+                return (
+                  <Listbox.Option key={item.fieldValue} className={({ active }) => classNames(active ? 'text-white bg-indigo-600' : 'text-gray-900', 'cursor-default select-none relative py-2 pl-3 pr-9')}
+                    value={{ currentAttribute: item.fieldValue, stock: stockAmount, }}>
+                    {({ selected, active }) => (
+                      <>
+                        <div className="flex items-center">
+                          <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'ml-3 block truncate')}>
+                            {item.fieldValue ? generateItemOption(item.fieldValue, stockAmount) : null}
+                          </span>
+                        </div>
+                        {selected ? (
+                          <span className={classNames(active ? 'text-white' : 'text-indigo-600', 'absolute inset-y-0 right-0 flex items-center pr-4')}>
+                            <CheckIcon className="w-5 h-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Listbox.Option>
+                )
+              })}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
+    </div>
   )
 }
