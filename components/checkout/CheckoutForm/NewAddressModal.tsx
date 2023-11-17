9878baeb4cbf04f121376f4c7f91e8123eacbea2
Amit Kumar <amit.kumar@bettercommerce.io>
2023-11-17 11:48:16 +0530

    Update console error

--- components/checkout/CheckoutForm/NewAddressModal.tsx
+++ components/checkout/CheckoutForm/NewAddressModal.tsx
@@ -303,13 +303,13 @@ const NewAddressModal = (props: INewAddressModalProps) => {
                               xmlns="http://www.w3.org/2000/svg"
                               fill="none"
                               viewBox="0 0 24 24"
-                              stroke-width="1.5"
+                              strokeWidth="1.5"
                               stroke="currentColor"
                               className="w-6 h-6"
                             >
                               <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
                                 d="M6 18L18 6M6 6l12 12"
                               />
                             </svg>
