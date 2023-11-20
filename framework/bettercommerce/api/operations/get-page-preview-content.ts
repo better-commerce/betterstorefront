061db64ec3270c67599c34249bad50d3967edfbd
Amit Kumar <amit.kumar@omnicx.com>
2023-11-20 12:35:07 +0000

    Merged PR 26114: Release Nov 20-3
    Update Cached Copy

--- framework/bettercommerce/api/operations/get-page-preview-content.ts
+++ framework/bettercommerce/api/operations/get-page-preview-content.ts
@@ -3,12 +3,12 @@ import { PAGE_PREVIEW_CONTENT_ENDPOINT } from '@components/utils/constants'
 import { BETTERCMS_BASE_URL } from '@framework/utils/constants'
 
 export default function getPagePreviewContentOperation() {
-    async function getPagePreviewContent({ id, slug = '/', workingVersion = false, channel }: any) {
+    async function getPagePreviewContent({ id, slug = '/', workingVersion = false, channel, cachedCopy = true }: any) {
         try {
             const response: any = await fetcher({
                 url: `${PAGE_PREVIEW_CONTENT_ENDPOINT}`,
                 method: 'get',
-                params: { id: id, slug: slug, workingVersion: workingVersion },
+                params: { id: id, slug: slug, workingVersion: workingVersion, cachedCopy: cachedCopy },
                 headers: {
                     DomainId: process.env.NEXT_PUBLIC_DOMAIN_ID,
                     Channel: channel
