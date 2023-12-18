3704f26f421a405c444df69d3f6d59d8e18ad244
Amit Sharma <amit.sharma@bettercommerce.io>
2023-12-18 08:14:59 +0000

    Merged PR 26638: 8718_AS_Brands_Landing_Page
    Brands landing pages moved to static pages
    
    PR Refrence Link:https://omnicx.visualstudio.com/FFX/_git/ffxstorefront/pullrequest/26482
    
    ![image.png](https://omnicx.visualstudio.com/2d4b1671-ff8e-4f9e-b5e1-2f1a90092ed2/_apis/git/repositories/f4f7ef1b-b799-4fdb-a5ab-24e18abf3a29/pullRequests/26638/attachments/image.png)
    
    Related work items: #8718

--- components/utils/constants.ts
+++ components/utils/constants.ts
@@ -80,6 +80,7 @@ export const NEXT_GET_ORDER_DETAILS = '/api/customer/order-details'
 export const NEXT_GET_CART = `/api/get-cart`
 export const NEXT_ASSOCIATE_CART = '/api/customer/associate-cart'
 export const NEXT_MERGE_CART = '/api/merge-cart'
+export const STATIC_BRAND_PATHS = `api/${process.env.NEXT_PUBLIC_API_VERSION}/infra/staticpath/brand`
 export const NEXT_GET_COLLECTION_BY_ID = '/api/collection/get-collection-by-id'
 export const NEXT_GET_USER_CART = '/api/get-user-cart'
 export const NEXT_GET_CATALOG_PRODUCTS = '/api/catalog/products'
