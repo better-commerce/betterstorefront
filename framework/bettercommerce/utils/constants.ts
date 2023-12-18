cf372bdc1a2d8a531d0b7ade36039167594635da
Tarun Singh <tarun.singh@bettercommerce.io>
2023-12-18 08:16:16 +0000

    Merged PR 26712: 9314_TS_Broken_Page_Redirect_to_500
    **added redirect to page 500 on page break:**
    
    **before:**
    ![image (2).png](https://omnicx.visualstudio.com/2d4b1671-ff8e-4f9e-b5e1-2f1a90092ed2/_apis/git/repositories/f4f7ef1b-b799-4fdb-a5ab-24e18abf3a29/pullRequests/26712/attachments/image%20%282%29.png)
    
    **after:**
    ![image.png](https://omnicx.visualstudio.com/2d4b1671-ff8e-4f9e-b5e1-2f1a90092ed2/_apis/git/repositories/f4f7ef1b-b799-4fdb-a5ab-24e18abf3a29/pullRequests/26712/attachments/image.png)
    
    **REFERENCE PR :** https://omnicx.visualstudio.com/FFX/_git/ffxstorefront/commit/de79b1e7a2398cfc9e46c1df96c06263834a2f7a?refName=refs/heads/feature/9240_GS_PDP_Error_Redirect_To_500&path=/pages/products/%5b...slug%5d.tsx&tab=details
    
    **DISCLAIMER:**
    sir please check issue with loadash
    for now i have used parseInt in place of toNumber from loadash
    
    Related work items: #9314

--- framework/bettercommerce/utils/constants.ts
+++ framework/bettercommerce/utils/constants.ts
@@ -1,5 +1,7 @@
 import { stringToBoolean } from './parse-util'
+// import { toNumber } from 'lodash'; cannot aplly toMumber from loadash as it breaks the build process ,Error :Dynamic Code Evaluation (e. g. 'eval', 'new Function', 'WebAssembly.compile') not allowed in Edge Runtime
 
+export const STATIC_PAGE_CACHE_INVALIDATION_IN_MINS =process.env.STATIC_PAGE_CACHE_INVALIDATION_IN_MINS? parseInt(process.env.STATIC_PAGE_CACHE_INVALIDATION_IN_MINS, 10) : 2
 export const STATIC_PAGE_CACHE_INVALIDATION_IN_60_SECONDS = 60
 export const STATIC_PAGE_CACHE_INVALIDATION_IN_200_SECONDS = 200
 export const CLIENT_ID = process.env.BETTERCOMMERCE_CLIENT_ID
