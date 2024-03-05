78638a8d482178c73517b33a8cf4c27021ddf6d5
Amit Kumar <91612827+amitkumar1403@users.noreply.github.com>
2024-03-05 17:25:02 +0530

    Remove lazy load

--- components/ui/Hero/Hero.tsx
+++ components/ui/Hero/Hero.tsx
@@ -31,7 +31,11 @@ const Hero: React.FC<HeroProps> = ({ banners = [], deviceInfo }: HeroProps) => {
   return (
     <>
       <h1 className="sr-only">Home Page</h1>
-      <Swiper navigation={true} loop={true} className="relative bg-gray-900 mySwiper" >
+      <Swiper
+        navigation={true}
+        loop={true}
+        className="relative bg-gray-900 mySwiper"
+      >
         {banners?.sort((a: { displayOrder: number }, b: { displayOrder: number }) => a.displayOrder > b.displayOrder ? 1 : -1).map((banner: any, bid: number) => (
           <>
             <SwiperSlide key={bid}>
@@ -40,21 +44,20 @@ const Hero: React.FC<HeroProps> = ({ banners = [], deviceInfo }: HeroProps) => {
                   {isOnlyMobile ? (
                     <>
                       {banner?.mobileUrl != '' ? (
-                        <img src={generateUri(banner?.mobileUrl, 'h=500&fm=webp') || IMG_PLACEHOLDER} className="object-cover object-center w-full" alt={banner?.alt} width={690} height={500} loading="lazy" />
+                        <img src={generateUri(banner?.mobileUrl, 'h=500&fm=webp') || IMG_PLACEHOLDER} className="object-cover object-center w-full" alt={banner?.alt} width={690} height={500} />
                       ) : (
                         <>
-                          <img src={generateUri(banner?.url, 'h=700&fm=webp') || IMG_PLACEHOLDER} alt={banner?.alt || 'banner-image'} style={css} width={1903} height={700} className="sm:max-h-screen image banner-Image" loading="lazy" />
+                          <img src={generateUri(banner?.url, 'h=700&fm=webp') || IMG_PLACEHOLDER} alt={banner?.alt || 'banner-image'} style={css} width={1903} height={700} className="sm:max-h-screen image banner-Image" />
                         </>
                       )}
                     </>
                   ) : (
-                    <img src={generateUri(banner?.url, 'h=700&fm=webp') || IMG_PLACEHOLDER} alt={banner?.alt || 'banner-image'} style={css} width={1903} height={700} className="sm:max-h-screen image banner-Image" loading="lazy" />
+                    <img src={generateUri(banner?.url, 'h=700&fm=webp') || IMG_PLACEHOLDER} alt={banner?.alt || 'banner-image'} style={css} width={1903} height={700} className="sm:max-h-screen image banner-Image" />
                   )}
                   <div className="sr-only">Banner Image</div>
                 </div>
               </Link>
             </SwiperSlide>
-
           </>
         ))}
       </Swiper>
