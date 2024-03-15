99b84a6e73826dd4605a7888b38b8f588d5f186a
Amit Kumar <91612827+amitkumar1403@users.noreply.github.com>
2024-03-15 18:45:36 +0530

    Update 404 and 500 page layout and UI

--- components/common/Layout/LayoutError.tsx
+++ components/common/Layout/LayoutError.tsx
@@ -1,48 +1,49 @@
-import cn from 'classnames'
-import React, { ComponentType, FC, useEffect, useState } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import dynamic from 'next/dynamic'
 import { useRouter } from 'next/router'
-import { CommerceProvider } from '@framework'
-import { useUI } from '@components/ui/context'
-import type { Page } from '@commerce/types/page'
-import { Navbar, Footer } from '@components/common'
-import type { Category } from '@commerce/types/site'
-import CartSidebarView from '@components/cart/CartSidebarView'
-import { WishlistSidebarView } from '@components/wishlist'
-import { useAcceptCookies } from '@lib/hooks/useAcceptCookies'
-import { Sidebar, Button, Modal, LoadingDots } from '@components/ui'
-import s from './Layout.module.css'
-import NotifyUserPopup from '@components/ui/NotifyPopup'
-import Router from 'next/router'
-import ProgressBar from '@components/ui/ProgressBar'
 import { CookieBanner } from '@schlomoh/react-cookieconsent'
-import { IExtraProps } from './Layout'
-import { stringToBoolean } from '@framework/utils/parse-util'
+import Router from 'next/router'
+import Head from 'next/head'
+import { CommerceProvider } from '@framework'
+import type { Page } from '@commerce/types/page'
+import type { Category } from '@commerce/types/site'
+import { IDeviceInfo, useUI } from '@components/ui/context'
+import { CURRENT_THEME } from '@components/utils/constants'
 import { GENERAL_COOKIE_TEXT } from '@components/utils/textVariables'
-import BulkAddSidebarView from '@components/bulk-add/BulkAddSidebarView'
-
+import { stringToBoolean } from '@framework/utils/parse-util'
+import { WishlistSidebarView } from '@components/wishlist'
+import { Sidebar, Modal, LoadingDots } from '@components/ui'
+import LoginSideBarView from '@components/account/Login/LoginSideBarView'
+const BulkAddSidebarView = dynamic(() => import('@components/bulk-add/BulkAddSidebarView'))
+const LoginSidebarView = dynamic(() => import('@components/account/Login/LoginSideBarView'))
+const MainNav2Logged = dynamic(() => import('@new-components/Header/MainNav2Logged'))
+const ShippingView = dynamic(() => import('@components/checkout-old/ShippingView'))
+const CartSidebarView = dynamic(() => import('@components/cart/CartSidebarView'))
+const PaymentMethodView = dynamic(() => import('@components/checkout-old/PaymentMethodView'))
+const CheckoutSidebarView = dynamic(() => import('@components/checkout-old/CheckoutSidebarView'))
+const NotifyUserPopup = dynamic(() => import('@components/ui/NotifyPopup'))
+const ProgressBar = dynamic(() => import('@components/ui/ProgressBar'))
+const FooterClean = dynamic(() => import('@components/common/Footer/FooterClean'))
+const AlertRibbon = dynamic(() => import('@components/ui/AlertRibbon'))
 const Loading = () => (
   <div className="fixed z-50 flex items-center justify-center p-3 text-center w-80 h-80">
     <LoadingDots />
   </div>
 )
-
-const dynamicProps = {
-  loading: Loading,
-}
-
-const FeatureBar: ComponentType<any> = dynamic(() => import('@components/common/FeatureBar'),
-  {
-    ...dynamicProps,
-  }
+const primaryButtonStyle = { backgroundColor: 'black' }
+const secondaryButtonStyle = { backgroundColor: 'gray' }
+const Content = () => (
+  <>
+    <h3></h3>
+    <p>{GENERAL_COOKIE_TEXT}</p>
+  </>
 )
-
 interface Props {
   children: any
   pageProps: {
     pages?: Page[]
     categories: Category[]
-    navTree: any
+    navTree?: any
     reviewData: any
   }
   nav: []
@@ -50,18 +51,18 @@ interface Props {
   isLocationLoaded: boolean
   config: any
   keywords: []
+  pluginConfig: []
 }
 
-const ModalView: FC<{ modalView: string; closeModal(): any }> = ({
-  modalView,
-  closeModal,
-}) => {
+const ModalView: FC<
+  React.PropsWithChildren<{ modalView: string; closeModal(): any }>
+> = ({ modalView, closeModal }) => {
   return (
     <Modal onClose={closeModal}>{modalView === 'NOTIFY_USER' && null}</Modal>
   )
 }
 
-const ModalUI: FC = () => {
+const ModalUI: FC<React.PropsWithChildren<unknown>> = () => {
   const { displayModal, closeModal, modalView, notifyUser, productId } = useUI()
   if (notifyUser) return <NotifyUserPopup />
   if (displayModal)
@@ -69,80 +70,66 @@ const ModalUI: FC = () => {
   return null
 }
 
-const SidebarView: FC<
-  { sidebarView: string; closeSidebar(): any } & IExtraProps
-> = ({ sidebarView, closeSidebar, deviceInfo, maxBasketItemsCount }) => {
+const SidebarView: FC<React.PropsWithChildren<{ sidebarView: string; closeSidebar(): any } & IExtraProps>> = ({ sidebarView, closeSidebar, deviceInfo, maxBasketItemsCount, config, pluginConfig = [] }) => {
   return (
-    <Sidebar
-      onClose={closeSidebar}
-      deviceInfo={deviceInfo}
-      maxBasketItemsCount={maxBasketItemsCount}
-    >
-      {sidebarView === 'CART_VIEW' && (
-        <CartSidebarView
-          deviceInfo={deviceInfo}
-          maxBasketItemsCount={maxBasketItemsCount}
-        />
-      )}
+    <Sidebar onClose={closeSidebar} deviceInfo={deviceInfo} maxBasketItemsCount={maxBasketItemsCount} >
+      {sidebarView === 'CART_VIEW' && (<CartSidebarView deviceInfo={deviceInfo} maxBasketItemsCount={maxBasketItemsCount} config={config} />)}
+      {sidebarView === 'LOGIN_SIDEBAR_VIEW' && <LoginSideBarView pluginConfig={pluginConfig} />}
       {sidebarView === 'BULK_ADD_VIEW' && <BulkAddSidebarView />}
       {sidebarView === 'WISHLIST_VIEW' && <WishlistSidebarView />}
+      {sidebarView === 'CHECKOUT_VIEW' && <CheckoutSidebarView />}
+      {sidebarView === 'PAYMENT_VIEW' && <PaymentMethodView />}
+      {sidebarView === 'SHIPPING_VIEW' && <ShippingView />}
     </Sidebar>
   )
 }
 
-const SidebarUI: FC = ({ deviceInfo, maxBasketItemsCount }: any) => {
+const SidebarUI: FC<React.PropsWithChildren<unknown & IExtraProps>> = ({ deviceInfo, maxBasketItemsCount, config, pluginConfig }: any) => {
   const { displaySidebar, closeSidebar, sidebarView } = useUI()
   return displaySidebar ? (
-    <SidebarView
-      sidebarView={sidebarView}
-      closeSidebar={closeSidebar}
-      deviceInfo={deviceInfo}
-      maxBasketItemsCount={maxBasketItemsCount}
-    />
+    <SidebarView sidebarView={sidebarView} closeSidebar={closeSidebar} deviceInfo={deviceInfo} maxBasketItemsCount={maxBasketItemsCount} config={config} pluginConfig={pluginConfig} />
   ) : null
 }
 
-interface LayoutProps {
-  nav: []
-  footer: []
+export interface IExtraProps {
+  readonly deviceInfo: IDeviceInfo
+  readonly maxBasketItemsCount: number
+  readonly isIncludeVAT?: boolean
+  onIncludeVATChanged?: any
+  keywords?: any
+  config?: any
+  pluginConfig?: any
 }
 
-const LayoutError: FC<Props & IExtraProps> = ({
-  children,
-  config,
-  pageProps: { categories = [], navTree, reviewData = {}, ...pageProps },
-  pluginConfig = [],
-  keywords,
-  isLocationLoaded,
-  deviceInfo,
-  maxBasketItemsCount,
-}) => {
+const LayoutError: FC<Props & IExtraProps> = ({ children, config, pageProps: { categories = [], navTree, reviewData = {}, ...pageProps }, keywords, isLocationLoaded, deviceInfo, maxBasketItemsCount = 0, nav }) => {
   const [isLoading, setIsLoading] = useState(false)
-  //const [data, setData] = useState(navTreeFromLocalStorage)
-
-  const { includeVAT, setIncludeVAT } = useUI()
+  const { setIsCompared } = useUI()
+  const { displayAlert, includeVAT, setIncludeVAT } = useUI()
   const isIncludeVAT = stringToBoolean(includeVAT)
-  const [isIncludeVATState, setIsIncludeVATState] =
-    useState<boolean>(isIncludeVAT)
+  const [isIncludeVATState, setIsIncludeVATState] = useState<boolean>(isIncludeVAT)
 
-  //check if nav data is avaialbel in LocalStorage, then dont fetch from Server/API
   useEffect(() => {
     Router.events.on('routeChangeStart', () => setIsLoading(true))
-    Router.events.on('routeChangeComplete', () => setIsLoading(false))
+    Router.events.on('routeChangeComplete', () => {
+      setIsLoading(false)
+      setIsCompared('false')
+    })
+
+    if (!document.title) {
+      document.title = document.location.host
+    }
 
     return () => {
-      Router.events.off('routeChangeStart', () => {})
-      Router.events.off('routeChangeComplete', () => {})
+      Router.events.off('routeChangeStart', () => { })
+      Router.events.off('routeChangeComplete', () => { })
     }
   }, [])
 
-  const { acceptedCookies, onAcceptCookies } = useAcceptCookies()
   const { locale = 'en-US', ...rest } = useRouter()
 
   const sortedData = navTree?.nav?.sort(
     (a: any, b: any) => a.displayOrder - b.displayOrder
   )
-
   const includeVATChanged = (value: boolean) => {
     setIncludeVAT(`${value}`)
 
@@ -150,52 +137,40 @@ const LayoutError: FC<Props & IExtraProps> = ({
       setIsIncludeVATState(value)
     }, 50)
   }
-  const primaryButtonStyle = { backgroundColor: 'black' }
-  const secondaryButtonStyle = { backgroundColor: 'gray' }
-  const Content = () => (
-    <>
-      <h3></h3>
-      <p>{GENERAL_COOKIE_TEXT}</p>
-    </>
-  )
-  return (
-    <CommerceProvider locale={locale}>
-      {isLoading && <ProgressBar />}
-      <div className={cn(s.root)}>
-        <Navbar
-          onIncludeVATChanged={includeVATChanged}
-          currencies={config?.currencies}
-          config={sortedData}
-          configSettings={config?.configSettings}
-          pluginConfig={pluginConfig}
-          languages={config?.languages}
-          key="navbar"
-          deviceInfo={deviceInfo}
-          maxBasketItemsCount={maxBasketItemsCount}
-        />
-        <main className="">{children}</main>
-        <Footer
-          config={navTree?.footer}
-          deviceInfo={deviceInfo}
-          maxBasketItemsCount={maxBasketItemsCount}
-        />
-        <ModalUI />
-        <SidebarUI />
-        <div className="cookie-bannner">
-            <CookieBanner
-              enableManagement
-              managementButtonText="Manage Cookies"
-              headingColor="white"
-              managementContent={<Content />}
-              cookieCategories={['analytics', 'advertisement']}
-              infoContent={<Content />}
-              primaryButtonStyle={primaryButtonStyle}
-              secondaryButtonStyle={secondaryButtonStyle}
-            />
-          </div>
-      </div>
-    </CommerceProvider>
-  )
-}
 
+  return (
+    <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="57x57" href={`https://cdnbs.bettercommerce.tech/theme/${CURRENT_THEME}/favicon/apple-icon-57x57.png`} />
+        <link rel="apple-touch-icon" sizes="60x60" href={`https://cdnbs.bettercommerce.tech/theme/${CURRENT_THEME}/favicon/apple-icon-60x60.png`} />
+        <link rel="apple-touch-icon" sizes="72x72" href={`https://cdnbs.bettercommerce.tech/theme/${CURRENT_THEME}/favicon/apple-icon-72x72.png`} />
+        <link rel="apple-touch-icon" sizes="76x76" href={`https://cdnbs.bettercommerce.tech/theme/${CURRENT_THEME}/favicon/apple-icon-76x76.png`} />
+        <link rel="apple-touch-icon" sizes="114x114" href={`https://cdnbs.bettercommerce.tech/theme/${CURRENT_THEME}/favicon/apple-icon-114x114.png`} />
+        <link rel="apple-touch-icon" sizes="120x120" href={`https://cdnbs.bettercommerce.tech/theme/${CURRENT_THEME}/favicon/apple-icon-120x120.png`} />
+        <link rel="apple-touch-icon" sizes="144x144" href={`https://cdnbs.bettercommerce.tech/theme/${CURRENT_THEME}/favicon/apple-icon-144x144.png`} />
+        <link rel="apple-touch-icon" sizes="152x152" href={`https://cdnbs.bettercommerce.tech/theme/${CURRENT_THEME}/favicon/apple-icon-152x152.png`} />
+        <link rel="apple-touch-icon" sizes="180x180" href={`https://cdnbs.bettercommerce.tech/theme/${CURRENT_THEME}/favicon/apple-icon-180x180.png`} />
+        <link rel="icon" type="image/png" sizes="192x192" href={`https://cdnbs.bettercommerce.tech/theme/${CURRENT_THEME}/favicon/android-icon-192x192.png`} />
+        <link rel="icon" type="image/png" sizes="32x32" href={`https://cdnbs.bettercommerce.tech/theme/${CURRENT_THEME}/favicon/favicon-32x32.png`} />
+        <link rel="icon" type="image/png" sizes="96x96" href={`https://cdnbs.bettercommerce.tech/theme/${CURRENT_THEME}/favicon/favicon-96x96.png`} />
+        <link rel="icon" type="image/png" sizes="16x16" href={`https://cdnbs.bettercommerce.tech/theme/${CURRENT_THEME}/favicon/favicon-16x16.png`} />
+        <link rel="icon" href={`https://cdnbs.bettercommerce.tech/theme/${CURRENT_THEME}/favicon/favicon.ico`} />
+      </Head>
+      <CommerceProvider locale={locale}>
+        {isLoading && <ProgressBar />}
+        <div className={`text-base sm:pt-24 pt-16 bg-white dark:bg-neutral-900 text-neutral-900 dark:text-neutral-200`}>
+          <MainNav2Logged onIncludeVATChanged={includeVATChanged} currencies={config?.currencies} config={sortedData} configSettings={config?.configSettings} languages={config?.languages} deviceInfo={deviceInfo} maxBasketItemsCount={maxBasketItemsCount} keywords={keywords} />
+          {displayAlert && <AlertRibbon />}
+          {children}
+          <FooterClean navItems={navTree?.footer} />
+          <ModalUI />
+          <SidebarUI deviceInfo={deviceInfo} maxBasketItemsCount={maxBasketItemsCount} config={config} />
+          <div className="cookie-bannner">
+            <CookieBanner enableManagement managementButtonText="Manage Cookies" headingColor="white" managementContent={<Content />} cookieCategories={['analytics', 'advertisement']} infoContent={<Content />} primaryButtonStyle={primaryButtonStyle} secondaryButtonStyle={secondaryButtonStyle} />
+          </div>
+        </div>
+      </CommerceProvider>
+    </>
+  );
+}
 export default LayoutError
