064a318ae0c270624274d92a4ba0b9b97db82a07
Amit Kumar <91612827+amitkumar1403@users.noreply.github.com>
2024-03-18 13:11:41 +0530

    Update z-index

--- components/cart/CartSidebarView/CartSidebarView.tsx
+++ components/cart/CartSidebarView/CartSidebarView.tsx
@@ -534,7 +534,7 @@ const CartSidebarView: FC<React.PropsWithChildren<IExtraProps>> = ({ deviceInfo,
         <Dialog as="div" className="fixed inset-0 overflow-hidden z-99" onClose={handleClose} >
           <div className="absolute inset-0 overflow-hidden z-99">
             <Transition.Child as={Fragment} enter="ease-in-out duration-500" enterFrom="opacity-0" enterTo="opacity-100" leave="ease-in-out duration-500" leaveFrom="opacity-100" leaveTo="opacity-0" >
-              <Dialog.Overlay className="w-full h-screen" onClick={handleClose} />
+              <Dialog.Overlay className="w-full h-screen bg-black/80 z-999" onClick={handleClose} />
             </Transition.Child>
             <div className="fixed inset-y-0 right-0 flex max-w-full pl-10">
               <Transition.Child as={Fragment} enter="transform transition ease-in-out duration-500 sm:duration-700" enterFrom="translate-x-full" enterTo="translate-x-0" leave="transform transition ease-in-out duration-500 sm:duration-700" leaveFrom="translate-x-0" leaveTo="translate-x-full" >
