2d295cbd9586f82945b9f67942ab53391d3c3ed3
BT-TARUNSINGH\Tarun.Singh <tarun.singh@bettercommerce.io>
2024-03-18 12:36:34 +0530

    Build errors removed

--- components/new-components/ProductCard.tsx
+++ components/new-components/ProductCard.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 import { StarIcon } from "@heroicons/react/24/solid";
 import { ArrowsPointingOutIcon } from "@heroicons/react/24/outline";
 import { useUI } from "@components/ui";
-import { BTN_PRE_ORDER, GENERAL_ADD_TO_BASKET, IMG_PLACEHOLDER } from "@components/utils/textVariables";
+import { IMG_PLACEHOLDER } from "@components/utils/textVariables";
 import { Messages, NEXT_CREATE_WISHLIST, NEXT_REMOVE_WISHLIST } from "@components/utils/constants";
 import cartHandler from "@components/services/cart";
 import wishlistHandler from "@components/services/wishlist";
@@ -21,6 +21,7 @@ const ModalQuickView = dynamic(() => import('@components/new-components/ModalQui
 const NcImage = dynamic(() => import('@components/new-components/shared/NcImage/NcImage'))
 const ButtonSecondary = dynamic(() => import('@components/new-components/shared/Button/ButtonSecondary'))
 const Button = dynamic(() => import('@components/ui/IndigoButton'))
+import { useTranslation } from '@commerce/utils/use-translation'
 export interface ProductCardProps {
   className?: string;
   data?: any;
@@ -29,7 +30,8 @@ export interface ProductCardProps {
   maxBasketItemsCount?: any;
 }
 
-const ProductCard: FC<ProductCardProps> = ({ className = "", data, isLiked, deviceInfo, maxBasketItemsCount }) => {
+const ProductCard: FC<ProductCardProps> = ({ className = "", data, isLiked, deviceInfo, maxBasketItemsCount }) => { 
+  const translate = useTranslation()
   const [showModalQuickView, setShowModalQuickView] = useState(false);
   const [quickViewData, setQuickViewData] = useState(null)
   const { basketId, cartItems, isGuestUser, setCartItems, user, setAlert, removeFromWishlist, addToWishlist, openWishlist, wishListItems, openLoginSideBar } = useUI()
@@ -104,7 +106,7 @@ const ProductCard: FC<ProductCardProps> = ({ className = "", data, isLiked, devi
 
   const buttonTitle = () => {
     let buttonConfig: any = {
-      title: GENERAL_ADD_TO_BASKET,
+      title: translate('label.basket.addToBasketText'),
       validateAction: async () => {
         const cartLineItem: any = cartItems?.lineItems?.find((o: any) => {
           if (matchStrings(o.productId, data?.recordId, true) || matchStrings(o.productId, data?.productId, true)) {
@@ -140,7 +142,7 @@ const ProductCard: FC<ProductCardProps> = ({ className = "", data, isLiked, devi
       shortMessage: '',
     }
     if (!data?.currentStock && data?.preOrder?.isEnabled) {
-      buttonConfig.title = BTN_PRE_ORDER
+      buttonConfig.title = translate('label.product.preOrderText')
       buttonConfig.isPreOrderEnabled = true
       buttonConfig.buttonType = 'button'
       buttonConfig.shortMessage = data?.preOrder?.shortMessage
@@ -156,7 +158,7 @@ const ProductCard: FC<ProductCardProps> = ({ className = "", data, isLiked, devi
         <Button size="small" className="hidden sm:block" title={buttonConfig.title} action={buttonConfig.action} buttonType={buttonConfig.type || 'cart'} />
         <ButtonSecondary className="ms-1.5 bg-white hover:!bg-gray-100 hover:text-slate-900 transition-colors shadow-lg" fontSize="text-xs" sizeClass="py-2 px-4" onClick={() => handleQuickViewData(data)} >
           <ArrowsPointingOutIcon className="w-3.5 h-3.5" />
-          <span className="ms-1">Quick view</span>
+          <span className="ms-1">{translate('label.product.quickViewText')}</span>
         </ButtonSecondary>
       </div>
     );
@@ -186,7 +188,7 @@ const ProductCard: FC<ProductCardProps> = ({ className = "", data, isLiked, devi
             <div className="flex items-center mb-0.5">
               <StarIcon className="w-5 h-5 pb-[1px] text-amber-400" />
               <span className="text-sm ms-1 text-slate-500 dark:text-slate-400">
-                {data?.rating || ""} ({data?.reviewCount || 0} reviews)
+                {data?.rating || ""} ({data?.reviewCount || 0} {translate('common.label.reviews')})
               </span>
             </div>
           </div>
