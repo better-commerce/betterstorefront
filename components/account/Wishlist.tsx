dccd54081ce253102067c436043cead556970b1a
Amit Kumar <91612827+amitkumar1403@users.noreply.github.com>
2024-03-21 14:06:02 +0530

    Fix Wishlist html

--- components/account/Wishlist.tsx
+++ components/account/Wishlist.tsx
@@ -91,50 +91,26 @@ export default function Wishlist({ deviceInfo }: any) {
   }
 
   return (
-    <div className="bg-white">
-      {/* Mobile menu */}
-      <main className="">
-        <div className="max-w-4xl">
-          <div className="">
+    <section aria-labelledby="recent-heading" className="max-w-4xl">
+      {!data.length && !isLoading && (
+        <div className="flex flex-col w-full py-2 max-acc-container sm:px-0">
+          <div className="my-0 font-semibold text-secondary-full-opacity text-m-16 text-24">{translate('label.wishlist.emptyWishlistText')}</div>
+          <p className="text-xs sm:text-sm text-primary opacity-60">{translate('label.wishlist.saveItemsText')}.{' '}</p>
+          <div className="flex w-full mt-5 sm:flex-col">
+            <Link legacyBehavior passHref href="/search" className="w-50 flex items-center justify-center px-4 py-3 -mr-0.5 rounded-sm sm:px-6 btn-primary">
+              <Button className="w-52 nc-Button relative h-auto inline-flex items-center justify-center rounded-full transition-colors text-sm sm:text-base font-medium py-3 px-4 sm:py-3.5 sm:px-6  ttnc-ButtonPrimary disabled:bg-opacity-90 bg-slate-900 dark:bg-slate-100 hover:bg-slate-800 text-slate-50 dark:text-slate-800 shadow-xl  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-6000 dark:focus:ring-offset-0">{translate('label.orderDetails.startShoppingBtnText')}</Button>
+            </Link>
           </div>
-          <section aria-labelledby="recent-heading" className="mt-1">
-            {!data.length && !isLoading && (
-              <div className="flex flex-col w-full py-2 max-acc-container sm:px-0">
-                <div className="my-0 font-semibold text-secondary-full-opacity text-m-16 text-24">
-                  {translate('label.wishlist.emptyWishlistText')}
-                </div>
-                <p className="text-xs sm:text-sm text-primary opacity-60">
-                  {translate('label.wishlist.saveItemsText')}.{' '}
-                </p>
-                <div className="flex w-full mt-5 sm:flex-col">
-                  <Link
-                    legacyBehavior
-                    passHref
-                    href="/search"
-                    className="w-50 flex items-center justify-center px-4 py-3 -mr-0.5 rounded-sm sm:px-6 btn-primary"
-                  >
-                    <Button className="w-52 nc-Button relative h-auto inline-flex items-center justify-center rounded-full transition-colors text-sm sm:text-base font-medium py-3 px-4 sm:py-3.5 sm:px-6  ttnc-ButtonPrimary disabled:bg-opacity-90 bg-slate-900 dark:bg-slate-100 hover:bg-slate-800 text-slate-50 dark:text-slate-800 shadow-xl  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-6000 dark:focus:ring-offset-0">{translate('label.orderDetails.startShoppingBtnText')}</Button>
-                  </Link>
-                </div>
-              </div>
-            )}
-            {isLoading && <LoadingDots />}
-            <div className="space-y-16 sm:space-y-24">
-              <div className="flow-root px-0 mt-2 sm:mt-4 sm:px-0">
-                <div className="grid grid-cols-1 sm:gap-5 sm:mx-0 md:grid-cols-2 product-listing-main lg:grid-cols-3">
-                  {data.map((product: any, wid: number) => {
-                    return (
-                      <div key={`wishlist-${wid}`}>
-                        <ProductCard data={product} deviceInfo={deviceInfo} maxBasketItemsCount={maxBasketItemsCount} />
-                      </div>
-                    )
-                  })}
-                </div>
-              </div>
-            </div>
-          </section>
         </div>
-      </main>
-    </div>
+      )}
+      {isLoading && <LoadingDots />}
+      <div className="grid grid-cols-1 sm:gap-5 sm:mx-0 md:grid-cols-2 product-listing-main lg:grid-cols-3">
+        {data?.map((product: any, wid: number) => (
+          <div key={`wishlist-${wid}`}>
+            <ProductCard data={product} deviceInfo={deviceInfo} maxBasketItemsCount={maxBasketItemsCount} />
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
