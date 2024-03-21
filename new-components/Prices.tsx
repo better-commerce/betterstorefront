d5c4b5aeeae2ed90f4b68149e0e44bce6e7c9df9
Amit Kumar <91612827+amitkumar1403@users.noreply.github.com>
2024-03-21 11:33:16 +0530

    Update product card

--- new-components/Prices.tsx
+++ new-components/Prices.tsx
@@ -15,14 +15,14 @@ const Prices: FC<PricesProps> = ({ className = "", price, listPrice, contentClas
   return (
     <div className={`${className}`}>
       {price?.raw?.withTax != 0 ? (<>
-        <div className="font-semibold text-green">
+        <div className="text-sm font-semibold text-green">
           {isIncludeVAT ? price?.formatted?.withTax : price?.formatted?.withoutTax}
           {isIncludeVAT ? (listPrice?.raw?.withTax > 0 && listPrice?.raw?.withTax > price?.raw?.withTax && (
             <span className="px-1 font-normal text-gray-400 line-through">{listPrice?.formatted?.withTax}</span>
           )
           ) : (
             listPrice?.raw?.withoutTax > 0 && listPrice?.raw?.withoutTax > price?.raw?.withoutTax && (
-              <span className="px-1 font-normal text-gray-400 line-through">{listPrice?.formatted?.withoutTax}</span>
+              <span className="px-1 text-xs font-normal text-gray-400 line-through">{listPrice?.formatted?.withoutTax}</span>
             )
           )}
           <span className="pl-1 text-xs font-light text-right text-gray-400">{isIncludeVAT ? translate('label.orderSummary.incVATText') : translate('label.orderSummary.excVATText')}</span>
