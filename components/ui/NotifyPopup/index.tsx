ccef3930970e1252de9d81c32dd3f8171b307586
Vinay Sharma <vinay.sharma@bettercommerce.io>
2024-03-22 18:27:29 +0530

    Fix Button UI

--- components/ui/NotifyPopup/index.tsx
+++ components/ui/NotifyPopup/index.tsx
@@ -130,7 +130,7 @@ export default function NotifyUserPopup() {
                 </div>
               </div>
               {isPostedMessage ? null : (
-                <div className="px-4 py-3 bg-gray-50 sm:px-6 sm:flex sm:flex-row-reverse">
+                <div className="px-4 py-3 bg-gray-50 sm:px-6 sm:flex sm:flex-row-reverse gap-2">
                   <button
                     type="button"
                     disabled={!isValidEmail}
@@ -139,13 +139,13 @@ export default function NotifyUserPopup() {
                         ? { opacity: '75%', pointerEvents: 'none' }
                         : {}
                     }
-                    className="inline-flex justify-center w-full px-4 py-2 font-medium text-white !bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm"
+                    className="nc-Button relative h-auto inline-flex items-center justify-center rounded-full transition-colors text-sm sm:text-base font-medium py-3 px-4 sm:py-3.5 sm:px-6  ttnc-ButtonPrimary disabled:bg-opacity-90 bg-slate-900 dark:bg-slate-100 hover:bg-slate-800 !text-slate-50 dark:text-slate-800 shadow-xl  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-6000 dark:focus:ring-offset-0"
                     onClick={() => handleModal()}
                   >
                     {translate('label.product.notifyMeText')}</button>
                   <button
                     type="button"
-                    className="inline-flex justify-center w-full px-4 py-2 mt-3 font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    className="nc-Button relative h-auto inline-flex items-center justify-center rounded-full transition-colors text-sm font-medium py-2.5 px-4 sm:px-6  ttnc-ButtonSecondary bg-white text-slate-700 dark:bg-slate-900 dark:text-slate-300 hover:bg-gray-100 dark:hover:bg-slate-800  border border-slate-300 dark:border-slate-700  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-6000 dark:focus:ring-offset-0"
                     onClick={closeNotifyUser}
                     ref={cancelButtonRef}
                   >
