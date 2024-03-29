ad4b88c7d46f8c8b6980243d9f3a67fb7b903da9
Amit Kumar <91612827+amitkumar1403@users.noreply.github.com>
2024-03-29 15:05:41 +0530

    Fix Mobile search icon position

--- components/shared/Search/SearchBar.tsx
+++ components/shared/Search/SearchBar.tsx
@@ -42,7 +42,7 @@ const SearchBar: FC<React.PropsWithChildren<ISearchBarProps>> = ({ id = 'search'
   const { showSearchBar, setShowSearchBar } = useUI()
   const defaultSearch = (
     <>
-      <div className="relative items-center justify-center w-10 h-10 rounded-full sm:top-0 lg:flex sm:w-12 sm:h-12 text-slate-700 dark:text-slate-300 hover:bg-slate-100 dark:hover:bg-slate-800 focus:outline-none" onClick={onClick}>
+      <div className="relative items-center justify-center w-10 h-10 rounded-full sm:top-0 top-2 lg:flex sm:w-12 sm:h-12 text-slate-700 dark:text-slate-300 hover:bg-slate-100 dark:hover:bg-slate-800 focus:outline-none" onClick={onClick}>
         <svg width={22} height={22} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" >
           <path d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
           <path d="M22 22L20 20" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
