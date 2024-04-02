c3879e955417f8aff11187e0c5711460634b89ac
Furqan Ahmed <furqan.ahmed@bettercommerce.io>
2024-04-02 10:40:03 +0000

    Merged PR 28094: 10762_FA_Register-redirect
    Updated the redirection of a user after successful Registration. User will get redirected directly to "Home Page".
    
    [New Registration redirect Testing.mp4](https://omnicx.visualstudio.com/2d4b1671-ff8e-4f9e-b5e1-2f1a90092ed2/_apis/git/repositories/f4f7ef1b-b799-4fdb-a5ab-24e18abf3a29/pullRequests/28094/attachments/New%20Registration%20redirect%20Testing.mp4)
    
    Related work items: #10762

--- components/Product/Filters/FilterTopBar.tsx
+++ components/Product/Filters/FilterTopBar.tsx
@@ -46,7 +46,7 @@ export default function FiltersRightOpen({ products = { filters: [] }, handleSor
                     {appliedFilter?.name && (
                       <span className="flex px-2 py-1 font-medium text-gray-600 border font-14 border-slate-300 bg-slate-50 rounded-2xl">
                         {secondValue}
-                        <span onClick={() => removeFilter(appliedFilter)} className="relative flex items-center justify-center flex-shrink-0 w-4 h-4 ml-3 text-white bg-red-500 rounded-full cursor-pointer top-0.5">
+                        <span onClick={() => removeFilter(appliedFilter)} className="relative flex items-center justify-center flex-shrink-0 w-4 h-4 ml-3 text-slate-700 bg-slate-300 rounded-full cursor-pointer top-0.5">
                           <svg xmlns="http://www.w3.org/2000/svg" className="w-3 h-3" viewBox="0 0 20 20" fill="currentColor" >
                             <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
                           </svg>
