85b36ae7c75403fbacf6328e5e24c612c403be47
Amit Kumar <91612827+amitkumar1403@users.noreply.github.com>
2024-04-05 18:28:53 +0530

    Update script

--- components/ui/Map/MultiMarker.tsx
+++ components/ui/Map/MultiMarker.tsx
@@ -4,16 +4,10 @@ const MapWithMarkers = ({ locations, setMap }:any) => {
   const mapRef = useRef<any>(null);
 
   useEffect(() => {
-  if (!window.google) {
-    console.error('Google Maps API is not loaded');
-    return;
-  }
-
-  const map = new window.google.maps.Map(mapRef.current);
-
-  setMap(map);
-}, [locations, setMap]);
-
+    if (!window.google) return;
+    const map = new window.google.maps.Map(mapRef.current);
+    setMap(map);
+  }, [locations]);
 
   return (
     <div ref={mapRef} className='w-full h-[400px] rounded-2xl border border-slate-200'>
