175da38ac3b477470f97c47be162ef4735c22081
Amit Kumar <91612827+amitkumar1403@users.noreply.github.com>
2024-04-05 19:10:29 +0530

    Update Feature toggle

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import { CURRENT_THEME } from '@components/utils/constants'
+import { CURRENT_THEME, OMNILYTICS_DISABLED } from '@components/utils/constants'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 import Script from 'next/script'
 const featureToggle = require(`../public/theme/${CURRENT_THEME}/features.config.json`);
@@ -14,12 +14,14 @@ export default class MyDocument extends NextDocument /*Document*/ {
         <Head>
           <link rel="preload" href={`/theme/${CURRENT_THEME}/css/global.css`} as="style" />
           <link rel="stylesheet" href={`/theme/${CURRENT_THEME}/css/global.css`} />
-          <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA1v3pkeBrwwbC-0KPCK5Uuhn77iHg2AjY&libraries=places"></script>
+          {featureToggle?.features?.enableStoreLocator &&
+            <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA1v3pkeBrwwbC-0KPCK5Uuhn77iHg2AjY&libraries=places"></script>
+          }
         </Head>
         <body className="custom_class promo-banner-inactive">
           <Main />
           <NextScript />
-          {featureToggle?.features?.enableEngage &&
+          {OMNILYTICS_DISABLED ? null :
             <Script src="https://engage-asset.bettercommerce.io/_plugins/min/bc/v1/js/ch.js" strategy="beforeInteractive" />
           }
         </body>
