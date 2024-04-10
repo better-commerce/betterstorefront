657370117bcb5221476e27862d38158490ee80b9
Amit Kumar <91612827+amitkumar1403@users.noreply.github.com>
2024-04-10 13:10:16 +0530

    Fix Localization for store pages

--- pages/yourstore/improve-recommendations.tsx
+++ pages/yourstore/improve-recommendations.tsx
@@ -1,11 +1,23 @@
-import Layout from "@components/Layout/Layout";
-import LayoutAccount from "@components/Layout/LayoutAccount";
-import RecentlyViewedProduct from "@components/Product/RelatedProducts/RecentlyViewedProducts";
-import { SITE_ORIGIN_URL } from "@components/utils/constants";
+import { BETTERCOMMERCE_DEFAULT_LANGUAGE, SITE_ORIGIN_URL } from "@components/utils/constants";
 import withDataLayer, { PAGE_TYPES } from "@components/withDataLayer";
+import { GetServerSideProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import NextHead from 'next/head'
 import { useRouter } from "next/router";
+import LayoutAccount from "@components/Layout/LayoutAccount";
+import BrowsingHistoryProducts from "@components/Product/RelatedProducts/BrowsingHistory";
+
 const PAGE_TYPE = PAGE_TYPES.YourStore
+
+export const getServerSideProps: GetServerSideProps = async (context: any) => {
+  const { locale } = context
+  return {
+    props: {
+      ...(await serverSideTranslations(locale ?? BETTERCOMMERCE_DEFAULT_LANGUAGE!)),
+    },
+  }
+}
+
 function ImproveRecommendations({ deviceInfo, config }: any) {
   const router = useRouter()
   return (
@@ -13,19 +25,20 @@ function ImproveRecommendations({ deviceInfo, config }: any) {
       <NextHead>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
         <link rel="canonical" id="canonical" href={SITE_ORIGIN_URL + router.asPath} />
-        <title>Improve Your Recommendation</title>
-        <meta name="title" content="Improve Your Recommendation" />
-        <meta name="description" content="Improve Your Recommendation" />
-        <meta name="keywords" content="Improve Your Recommendation" />
-        <meta property="og:image" content="Improve Your Recommendation" />
-        <meta property="og:title" content="Improve Your Recommendation" key="ogtitle" />
-        <meta property="og:description" content="Improve Your Recommendation" key="ogdesc" />
+        <title>Your Store</title>
+        <meta name="title" content="Your Store" />
+        <meta name="description" content="Your Store" />
+        <meta name="keywords" content="Your Store" />
+        <meta property="og:image" content="Your Store" />
+        <meta property="og:title" content="Your Store" key="ogtitle" />
+        <meta property="og:description" content="Your Store" key="ogdesc" />
       </NextHead>
       <div className="container py-6 mx-auto cart-recently-viewed sm:py-10">
-        <RecentlyViewedProduct deviceInfo={deviceInfo} config={config} productPerRow={4} />
+        <BrowsingHistoryProducts deviceInfo={deviceInfo} config={config} productPerRow={4} />
       </div>
     </>
   )
 }
+
 ImproveRecommendations.LayoutAccount = LayoutAccount
 export default withDataLayer(ImproveRecommendations, PAGE_TYPE, true, LayoutAccount)
