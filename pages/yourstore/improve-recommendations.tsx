c2d4fa76a7ab8b865ee04d743be8e9b28ef3f1ef
Amit Kumar <91612827+amitkumar1403@users.noreply.github.com>
2024-04-10 16:37:25 +0530

    Update improve recom UI

--- pages/yourstore/improve-recommendations.tsx
+++ pages/yourstore/improve-recommendations.tsx
@@ -6,6 +6,10 @@ import NextHead from 'next/head'
 import { useRouter } from "next/router";
 import LayoutAccount from "@components/Layout/LayoutAccount";
 import BrowsingHistoryProducts from "@components/Product/RelatedProducts/BrowsingHistory";
+import { generateUri } from "@commerce/utils/uri-util";
+import { IMG_PLACEHOLDER } from "@components/utils/textVariables";
+import Link from "next/link";
+import { Switch } from "@headlessui/react";
 
 const PAGE_TYPE = PAGE_TYPES.YourStore
 
@@ -17,6 +21,15 @@ export const getServerSideProps: GetServerSideProps = async (context: any) => {
     },
   }
 }
+
+const recommendations = [
+  { "name": "Pink Ombre Satin Wide Leg Trouser", "image": "https://www.imagedelivery.space/demostore/products/ss220991.jpg?fm=webp&h=400", "isRecommended": true, "link": "https://fashion.bettercommerce.tech/products/multi-blur-print-midi-dress-with-draped-skirt-ss23072" },
+  { "name": "Pink Metallic Corset Mini Dress with Ruched Skirt", "image": "https://www.imagedelivery.space/demostore/products/ss22344_pink_2.jpg?fm=webp&h=400", "isRecommended": true, "link": "https://fashion.bettercommerce.tech/products/pink-metallic-corset-mini-dress-with-ruched-skirt-ss22344" },
+  { "name": "Black and White Polka Dot Taffeta Mini Dress with Volume Skirt", "image": "https://www.imagedelivery.space/demostore/products/ss23068_1_(2).jpg?fm=webp&h=400", "isRecommended": false, "link": "https://fashion.bettercommerce.tech/products/black-white-polka-dot-taffeta-mini-dress-with-volume-skirt-1-ss23202" },
+  { "name": "White Lurex Draped Bodice Mini Dress with Gathered Skirt", "image": "https://www.imagedelivery.space/demostore/products/ss23130-_white1.jpg?fm=webp&h=400", "isRecommended": true, "link": "https://fashion.bettercommerce.tech/products/white-lurex-draped-bodice-mini-dress-with-gathered-skirt-ss23130" },
+  { "name": "Liberty Boyfriend Fit Lined Blazer in Teal", "image": "https://www.imagedelivery.space/demostore/products/exsd21253-teal_1.jpg?fm=webp&h=400", "isRecommended": false, "link": "https://fashion.bettercommerce.tech/products/teal-boyfriend-fit-lined-blazer-exsd21253" },
+  { "name": "Animal Printed One Shoulder Sequin Dress With Draped Skirt", "image": "https://www.imagedelivery.space/demostore/products/aw22526-animal1.jpg?fm=webp&h=400", "isRecommended": true, "link": "https://fashion.bettercommerce.tech/products/animal-printed-one-shoulder-sequin-dress-with-draped-skirt-aw22526" }
+]
 
 function ImproveRecommendations({ deviceInfo, config }: any) {
   const router = useRouter()
@@ -34,7 +47,34 @@ function ImproveRecommendations({ deviceInfo, config }: any) {
         <meta property="og:description" content="Your Store" key="ogdesc" />
       </NextHead>
       <div className="container py-6 mx-auto cart-recently-viewed sm:py-10">
-        <BrowsingHistoryProducts deviceInfo={deviceInfo} config={config} productPerRow={4} />
+        <div className="flex flex-col justify-start mt-4 sm:mt-6">
+          <h1 className="flex items-center text-2xl font-semibold md:text-3xl">Improve Your Recommendation</h1>
+          <p>Provide better recommendation by setting up what you want to see.</p>
+        </div>
+        <div className="flex flex-col mt-4 border w-fill border-slate-200 rounded-2xl sm:mt-8">
+          <div className="grid grid-cols-1">
+            {recommendations?.map((data: any, dataIdx: number) => (
+              <div className="grid items-center grid-cols-12 gap-3 p-2 border-b border-slate-300" key={dataIdx}>
+                <div className="col-span-1">
+                  <div className="flex-shrink-0">
+                    <img width={140} height={60} src={generateUri(data?.image, 'h=200&fm=webp') || IMG_PLACEHOLDER} alt={data?.name || 'cart-item'} className="object-cover object-center w-10 rounded-lg sm:w-16 image" />
+                  </div>
+                </div>
+                <div className="col-span-10 ">
+                  <Link href={data?.link} passHref>
+                    <h3 className="font-semibold text-black font-16 hover:text-sky-600">{data?.name}</h3>
+                  </Link>
+                </div>
+                <div className="col-span-1">
+                  <Switch checked={data?.isRecommended} className={`${data?.isRecommended ? 'bg-white' : 'bg-gray-300'} relative inline-flex h-[18px] w-[35px] shrink-0 cursor-pointer rounded-full border border-slate-300 transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`} >
+                    <span className="sr-only">is Enable</span>
+                    <span aria-hidden="true" className={`${data?.isRecommended ? 'translate-x-4' : 'translate-x-0'} pointer-events-none inline-block h-[15px] w-[15px] transform rounded-full bg-black shadow-lg ring-0 transition duration-200 ease-in-out`} />
+                  </Switch>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </>
   )
