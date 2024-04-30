257eacd920551d603586f05cd675289c0cb24abf
Gagan <gagandeep.singh@bettercommerce.io>
2024-04-30 10:13:00 +0530

    Translate text in noProductFound component.

--- components/noProductFound/index.tsx
+++ components/noProductFound/index.tsx
@@ -1,15 +1,17 @@
+import { useTranslation } from '@commerce/utils/use-translation'
+
 export default function NoProductFound() {
+  const translate = useTranslation()
   return (
     <div className="flex flex-col items-center mt-10 justify-">
       <div className="bg-gray-400 w-12 h-12 flex items-center justify-center transform rotate-45">
         <span className="text-white font-bold -rotate-45">!</span>
       </div>
       <p className="text-gray-600 font-medium text-center text-lg mt-6">
-        Sorry! We couldn't find any matching items.
+        {translate('label.search.noMatchingProductFoundText')}
       </p>
       <p className="text-gray-500 text-center text-sm mt-4">
-        Don't give up - check the spelling, Filters or try less specific search
-        terms
+        {translate('label.search.noMatchingProductFoundDescText')}
       </p>
     </div>
   )
