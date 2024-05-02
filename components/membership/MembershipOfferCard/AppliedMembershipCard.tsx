99866c17cf22539ac165c08c35688fef85331c7f
Gagan <gagandeep.singh@bettercommerce.io>
2024-05-02 12:17:09 +0530

    Review changes

--- components/membership/MembershipOfferCard/AppliedMembershipCard.tsx
+++ components/membership/MembershipOfferCard/AppliedMembershipCard.tsx
@@ -1,12 +1,9 @@
 const AppliedMembershipCard = ({ promo, currencySymbol, moneySaved }:any) => {
+  console.log(promo)
   return (
     <div className="bg-gray-200 p-6 rounded-lg mt-2 text-center">
-      <p className="text-black font-semibold mb-4">{promo?.promoName}</p>
-      <p className="text-black font-semibold mb-6">{`Saved ${currencySymbol}${moneySaved} in you current Basket`}</p>
-      <button  className="bg-white text-indigo-900 font-semibold py-2 px-4 rounded-md mb-4">
-        DISCOUNT APPLIED
-      </button>
-      
+      <p className="text-black font-semibold mb-4">{`${promo?.discountPct}% discount applied!`}</p>
+      <p className="text-black font-semibold mb-6">{`You Saved ${currencySymbol}${moneySaved}`}</p>
     </div>
   );
 };
