5bf52059ee106ee5065cc9b67ead08178fbf4471
BT-TARUNSINGH\Tarun.Singh <tarun.singh@bettercommerce.io>
2024-05-02 11:58:27 +0530

    "Changed colors of text and image in AppliedMembershipCard component"
    Explanation:
    The code diff shows that the text color for the promo name and the saved amount have been changed from white to black. Additionally, the

--- components/membership/MembershipOfferCard/AppliedMembershipCard.tsx
+++ components/membership/MembershipOfferCard/AppliedMembershipCard.tsx
@@ -1,11 +1,8 @@
 const AppliedMembershipCard = ({ promo, currencySymbol, moneySaved }:any) => {
   return (
     <div className="bg-gray-200 p-6 rounded-lg mt-2 text-center">
-      <div className="flex items-center justify-center text-white text-2xl font-bold mb-4">
-        <img src="/theme/blue/image/logo.png?fm=webp&h=200" width="60" height="60" alt="Store" className="brand-logo" />
-      </div>
-      <p className="text-white font-semibold mb-4">{promo?.promoName}</p>
-      <p className="text-yellow-300 font-semibold mb-6">{`Saved ${currencySymbol}${moneySaved} in you current Basket`}</p>
+      <p className="text-black font-semibold mb-4">{promo?.promoName}</p>
+      <p className="text-black font-semibold mb-6">{`Saved ${currencySymbol}${moneySaved} in you current Basket`}</p>
       <button  className="bg-white text-indigo-900 font-semibold py-2 px-4 rounded-md mb-4">
         DISCOUNT APPLIED
       </button>
