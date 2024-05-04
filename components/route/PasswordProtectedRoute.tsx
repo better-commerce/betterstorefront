43ebc9d37a2ca95b060925ae43113cadc9f1cb75
Gagan <gagandeep.singh@bettercommerce.io>
2024-05-04 19:11:13 +0530

    Anonymous access protection - Turn to localstorage instead of cookies

--- components/route/PasswordProtectedRoute.tsx
+++ components/route/PasswordProtectedRoute.tsx
@@ -9,7 +9,7 @@ import Loader from '@components/Loader'
 
 // Other Imports
 import { stringToBoolean } from '@framework/utils/parse-util'
-import Cookies from 'js-cookie'
+//import Cookies from 'js-cookie'
 import { Cookie } from '@framework/utils/constants'
 
 declare const window: any
@@ -50,7 +50,8 @@ const PasswordProtectedRoute = ({ config, children }: any) => {
         )?.value || 'False'
       )
 
-      const authenticated = Cookies.get(`${window.location.hostname}-${Cookie.Key.PASSWORD_PROTECTION_AUTH}`)
+      //const authenticated = Cookies.get(`${window.location.hostname}-${Cookie.Key.PASSWORD_PROTECTION_AUTH}`)
+      const authenticated = localStorage.getItem(`${window.location.hostname}-${Cookie.Key.PASSWORD_PROTECTION_AUTH}`)!
       const isAuthenticated = stringToBoolean(authenticated)
       if (isPasswordProtectionEnabled && !isAuthenticated) {
         Router.push('/password-protection').then(() => {
--- pages/password-protection.tsx
+++ pages/password-protection.tsx
@@ -10,8 +10,8 @@ import { useCallback, useEffect, useState } from 'react'
 import Link from 'next/link'
 import { Logo } from '@components/ui'
 import { Cookie } from '@framework/utils/constants'
-import Cookies from 'js-cookie'
-import { getExpiry, getMinutesInDays } from '@components/utils/setSessionId'
+//import Cookies from 'js-cookie'
+//import { getExpiry, getMinutesInDays } from '@components/utils/setSessionId'
 import Router from 'next/router'
 import { SITE_NAME } from '@components/utils/constants'
 
@@ -36,9 +36,10 @@ function PasswordProtectionPage({ config }: any) {
     }),
     onSubmit: (values, { setSubmitting }) => {
       if (values?.password === passwordProtectionSetting?.livePassword) {
-        Cookies.set(`${window.location.hostname}-${Cookie.Key.PASSWORD_PROTECTION_AUTH}`, 'true', {
+        /*Cookies.set(`${window.location.hostname}-${Cookie.Key.PASSWORD_PROTECTION_AUTH}`, 'true', {
           expires: getExpiry(getMinutesInDays(30)),
-        })
+        })*/
+        localStorage.setItem(`${window.location.hostname}-${Cookie.Key.PASSWORD_PROTECTION_AUTH}`, 'true')
         setPasswordMatched(true)
       } else {
         setPasswordMatched(false)
