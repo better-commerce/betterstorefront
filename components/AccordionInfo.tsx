a90eacc976fdb6b3d727afbc11de12d52fa5cc84
Vinay Sharma <vinay.sharma@omnicx.com>
2024-05-07 11:42:15 +0000

    Merged PR 28600: 11499_VS_PDP_UI_Update
    Related work items: #11499

--- components/AccordionInfo.tsx
+++ components/AccordionInfo.tsx
@@ -69,7 +69,7 @@ const AccordionInfo: FC<Props> = ({
             {({ open }) => (
               <>
                 <Disclosure.Button className="flex items-center justify-between w-full px-4 py-2 font-medium text-left rounded-lg bg-slate-100/80 hover:bg-slate-200/60 dark:bg-slate-800 dark:hover:bg-slate-700 focus:outline-none focus-visible:ring focus-visible:ring-slate-500 focus-visible:ring-opacity-75 ">
-                  <span>{item.name}</span>
+                  <span className="text-accordion">{item.name}</span>
                   {!open ? (
                     <PlusIcon className="w-4 h-4 text-slate-600 dark:text-slate-400" />
                   ) : (
--- components/Product/InlineList.tsx
+++ components/Product/InlineList.tsx
@@ -9,12 +9,12 @@ function renderRadioOptions(items: any, itemsCount: any, selectedValue: any, siz
     <div className="grid grid-cols-5 sm:grid-cols-7 gap-2 mt-2.5">
       {defaultItems.map((item: any, idx: any) => (
         <RadioGroup.Option key={idx} value={item.fieldValue} title={item.fieldLabel} onClick={() => { setSizeInit('false') }} style={{ backgroundColor: item.fieldValue }}
-          className={cn(' pdp-color-swatch-item relative z-1 rounded-xl h-10 border border-gray-200 items-center justify-center cursor-pointer outline-none ring-gray-600 ring-inset-1 hover:ring-1', { 'ring-1 z-1': selectedValue === item.fieldValue, })} />
+          className={cn(' pdp-color-swatch-item relative z-1 rounded-xl h-10 border border-gray-200 items-center attr-color justify-center cursor-pointer outline-none ring-gray-600 ring-inset-1 hover:ring-1', { 'ring-1 z-1': selectedValue === item.fieldValue, })} />
       ))}
       {remainingItems.map((item: any, idx: any) => (
         <RadioGroup.Option
           key={idx} value={item.fieldValue} title={item.fieldLabel} style={{ backgroundColor: item.fieldValue }}
-          className={cn('relative rounded-2xl border flex items-center justify-center text-sm uppercase font-semibold select-none overflow-hidden z-0 cursor-pointer border-slate-300 dark:border-slate-600 hover:bg-primary-700 hover:text-black dark:hover:bg-neutral-700 pdp-color-swatch-item z-1 h-10 outline-none ring-gray-600 ring-inset-1 hover:ring-1', { 'ring-1 z-1': selectedValue === item.fieldValue, })}
+          className={cn('relative rounded-2xl border flex items-center attr-color justify-center text-sm uppercase font-semibold select-none overflow-hidden z-0 cursor-pointer border-slate-300 dark:border-slate-600 hover:bg-primary-700 hover:text-black dark:hover:bg-neutral-700 pdp-color-swatch-item z-1 h-10 outline-none ring-gray-600 ring-inset-1 hover:ring-1', { 'ring-1 z-1': selectedValue === item.fieldValue, })}
         />
       ))}
     </div>
@@ -46,7 +46,7 @@ export default function InlineList({ items = [], onChange = () => { }, label = "
   return (
     <>
       <div className="flex flex-col justify-start gap-1 mt-2">
-        <h4 className="text-gray-700 font-14 dark:text-white">{label} :
+        <h4 className="text-gray-700 font-14 dark:text-white font-label-14">{label} :
           {validation && <span className='relative inline-block w-5 h-5 ml-1 rounded-full top-1' style={{ background: colorName }}></span>}
         </h4>
       </div>
