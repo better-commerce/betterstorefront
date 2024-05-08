d6d87502deb7df131a94c8b468248cdb7a8cdf28
Mohit Das <mohit.das@bettercommerce.io>
2024-05-08 16:56:37 +0530

    add enable / disable omnilytics' events

--- components/services/analytics/useAnalytics.ts
+++ components/services/analytics/useAnalytics.ts
@@ -1,33 +1,29 @@
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 import eventDispatcher from './eventDispatcher'
-import { useUI } from '@components/ui/context'
+import { OMNILYTICS_DISABLED } from '@framework/utils/constants'
+
 declare global {
   interface Window {
-    dataLayer: any,
-    ch_session:any,
-    ch_page_view_before: any,
-    ch_collection_page_view_before:any,
-    ch_index_page_view_before:any,
-    ch_add_to_cart_before:any,
-    ch_product_view_before:any,
-    ch_checkout_initiate_before:any,
-    ch_index_page_view_after:any,
-    ch_product_view_after:any,
-    ch_remove_from_cart_before:any,
-    ch_purchase_complete_before:any
+    dataLayer: any
+    ch_session: any
+    ch_page_view_before: any
+    ch_collection_page_view_before: any
+    ch_index_page_view_before: any
+    ch_add_to_cart_before: any
+    ch_product_view_before: any
+    ch_checkout_initiate_before: any
+    ch_index_page_view_after: any
+    ch_product_view_after: any
+    ch_remove_from_cart_before: any
+    ch_purchase_complete_before: any
   }
 }
 
-export default function useAnalytics(
-  event: string,
-  data: any,
-  ipAddress?: string
-) {
+export default function useAnalytics(event: string, data: any) {
+  if (OMNILYTICS_DISABLED) return
+  
   // const windowClone: any = typeof window !== 'undefined' ? window : {}
-  const dataLayer =
-    typeof window !== 'undefined' &&
-    (<any>window).dataLayer &&
-    (<any>window).dataLayer[0].ipAddress
+  const dataLayer = typeof window !== 'undefined' && (<any>window).dataLayer && (<any>window).dataLayer[0].ipAddress
 
   useEffect(() => {
     //console.count(`inside use effect ${dataLayer}`)
