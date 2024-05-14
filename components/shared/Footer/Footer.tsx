e344955cc8365909d0c8937cfd0c9fc034989800
Amit Kumar <91612827+amitkumar1403@users.noreply.github.com>
2024-05-14 18:12:12 +0530

    Update Newsletter

--- components/shared/Footer/Footer.tsx
+++ components/shared/Footer/Footer.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import SocialsList1 from "@components/shared/SocialsList1/SocialsList1";
 import Link from "next/link";
 import { sanitizeHtmlContent } from "framework/utils/app-util";
+import Newsletter from "../Newsletter";
 
 
 const Footer = ({ navItems = [] }: any) => {
@@ -37,19 +38,22 @@ const Footer = ({ navItems = [] }: any) => {
   };
 
   return (
-    <div className="relative py-20 border-t nc-Footer lg:pt-28 lg:pb-24 border-neutral-200 dark:border-neutral-700 main-footer-section">
-      <div className="container grid grid-cols-2 gap-y-10 gap-x-5 sm:gap-x-8 md:grid-cols-4 lg:grid-cols-5 lg:gap-x-10 ">
-        <div className="grid grid-cols-4 col-span-2 gap-5 md:col-span-4 lg:md:col-span-1 lg:flex lg:flex-col">
-          <div className="col-span-2 md:col-span-1 footer-logo">
-            <Logo />
+    <>
+      <Newsletter />
+      <div className="relative py-20 border-t nc-Footer lg:pt-28 lg:pb-24 border-neutral-200 dark:border-neutral-700 main-footer-section">
+        <div className="container grid grid-cols-2 gap-y-10 gap-x-5 sm:gap-x-8 md:grid-cols-4 lg:grid-cols-5 lg:gap-x-10 ">
+          <div className="grid grid-cols-4 col-span-2 gap-5 md:col-span-4 lg:md:col-span-1 lg:flex lg:flex-col">
+            <div className="col-span-2 md:col-span-1 footer-logo">
+              <Logo />
+            </div>
+            <div className="flex items-center col-span-2 md:col-span-3 footer-social-links">
+              <SocialsList1 className="flex items-center space-x-2 lg:space-x-0 lg:flex-col lg:space-y-3 lg:items-start" />
+            </div>
           </div>
-          <div className="flex items-center col-span-2 md:col-span-3 footer-social-links">
-            <SocialsList1 className="flex items-center space-x-2 lg:space-x-0 lg:flex-col lg:space-y-3 lg:items-start" />
-          </div>
+          {navItems?.map(renderWidgetMenuItem)}
         </div>
-        {navItems?.map(renderWidgetMenuItem)}
       </div>
-    </div>
+    </>
   );
 };
 
