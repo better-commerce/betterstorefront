f8ed0561cb2541fb87a0b3d61de1c038a64b8436
Amit Kumar <91612827+amitkumar1403@users.noreply.github.com>
2024-05-15 19:30:22 +0530

    Update Category

--- components/category/FeaturedCategory.tsx
+++ components/category/FeaturedCategory.tsx
@@ -8,14 +8,14 @@ import Link from "next/link";
 export default function FeaturedCategory({ featuredCategory }: any) {
   const translate = useTranslation()
   return (
-    <div className="py-6">
+    <div className="pt-6 pb-3">
       <Swiper spaceBetween={10} slidesPerView={1} navigation={false} loop={false} breakpoints={{ 640: { slidesPerView: 3, }, 768: { slidesPerView: 5 }, 1024: { slidesPerView: 6.8 }, 1400: { slidesPerView: 8.4 }, }} className="px-20 mySwiper" >
         {featuredCategory?.map((featured: any, featuredIdx: number) => (
           <div key={featuredIdx}>
             {featured?.isFeatured == true && (
               <SwiperSlide key={featuredIdx}>
                 <div className="relative border group rounded-2xl bg-slate-100 border-slate-100 hover:bg-slate-50">
-                  <Link href={`/${featured?.link}`} className="flex justify-center w-full px-4 py-2 font-semibold text-center text-black bg-gray-100 border border-gray-100 rounded-lg hover:bg-gray-200 font-14">
+                  <Link href={`/${featured?.link}`} className="flex justify-center w-full px-0 py-2 font-semibold text-center text-black bg-gray-100 border border-gray-100 rounded-lg hover:bg-gray-200 font-14">
                     <span>{featured?.name}</span>
                   </Link>
                 </div>
@@ -25,6 +25,7 @@ export default function FeaturedCategory({ featuredCategory }: any) {
         )
         )}
       </Swiper>
+      <hr className='mt-2 border-slate-200 dark:border-slate-700 sm:mt-4' />
     </div>
   )
 }
