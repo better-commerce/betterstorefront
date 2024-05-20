25e3cbfe1a29898e8add7301554080eddabbb5f3
Amit Kumar <91612827+amitkumar1403@users.noreply.github.com>
2024-05-20 13:39:14 +0530

    Fix Specification Image

--- components/Product/AvailableOffers.tsx
+++ components/Product/AvailableOffers.tsx
@@ -42,7 +42,7 @@ export default function AvailableOffers({ currency, offers, key }: any) {
           <div className="col-span-7 sm:col-span-3 mob-left-7">
             <h2 className="font-semibold font-16 text-16 dark:text-black">
               <span className="font-medium opacity_056 text-primary dark:text-white font-label-14">
-              {translate('label.product.bestPriceText')}{' '}
+                {translate('label.product.bestPriceText')}{' '}
               </span>
               <span className="inline-block pl-1 text-sm text-black dark:text-white font-label-14">
                 {priceFormat(bestprice, undefined, currency?.currencySymbol)}
@@ -66,24 +66,24 @@ export default function AvailableOffers({ currency, offers, key }: any) {
             </div>
           </div>
           <div className='col-span-12'>
-          <ProductSaleCountdown startDate={offers?.bestAvailablePromotion?.fromDate} endDate={offers?.bestAvailablePromotion?.toDate} />
+            <ProductSaleCountdown startDate={offers?.bestAvailablePromotion?.fromDate} endDate={offers?.bestAvailablePromotion?.toDate} />
           </div>
         </div>
       </div>
       <div className="flex flex-col px-0 py-2 pr-0 mt-2 sm:pr-4 gap-x-4 sm:px-0 offeres m-hide-navigation">
         <h2 className="mb-2 font-semibold text-black uppercase opacity_056 text-primary font-18 dark:text-white offer-heading-text">
-          {translate('label.basket.moreOffersText')} 
+          {translate('label.basket.moreOffersText')}
         </h2>
         <Swiper
           className="inline-flex mx-4 space-x-0 sm:mx-0 lg:mx-0 lg:space-x-0 lg:grid lg:grid-cols-4 lg:gap-x-0 h-60"
-          slidesPerView={2.3}
+          slidesPerView={1}
           spaceBetween={2}
           navigation={true}
           loop={false}
           breakpoints={{
-            640: { slidesPerView: 2.3 },
-            768: { slidesPerView: 3 },
-            1024: { slidesPerView: 3.1 },
+            640: { slidesPerView: 1.5 },
+            768: { slidesPerView: 2 },
+            1024: { slidesPerView: 2 },
           }}
         >
           {offers?.availablePromotions?.map((saving: any, sid: number) => {
@@ -97,37 +97,24 @@ export default function AvailableOffers({ currency, offers, key }: any) {
             const percentagePay = currency.raw.withTax - percentDiscount
             return (
               saving?.additionalInfo8 == 'False' && (
-                <SwiperSlide
-                  className="h-auto px-1 z-1 !w-48"
-                  key={`promo-${sid}-best-available-${saving?.code}`}
-                >
-                  <div key={`promotions-${sid}-best-available-${saving?.code}`} className="relative inline-flex flex-col w-full h-28 text-left cursor-pointer rounded-xl group" onClick={() => setOfferData(saving)} >
+                <SwiperSlide className="h-auto px-1 z-1 !w-48" key={`promo-${sid}-best-available-${saving?.code}`} >
+                  <div key={`promotions-${sid}-best-available-${saving?.code}`} className="relative inline-flex flex-col w-full text-left cursor-pointer h-28 rounded-xl group" onClick={() => setOfferData(saving)} >
                     <div className="box-border h-full p-2 bg-transparent border cursor-pointer promo-bg sm:p-3">
                       <div className="flex">
                         <BankOfferIcon />
                         <div className="pl-2">
                           <p className="font-semibold text-md">{translate('label.basket.moreOffersText')}</p>
-
-                          <p className="font-semibold text-left text-black uppercase font-10 break-word-text">
-                            {saving.name}
-                          </p>
+                          <p className="font-semibold text-left text-black uppercase font-10 break-word-text"> {saving.name} </p>
                           {saving?.promoType == 1 && (
                             <span>
-                              {saving.additionalInfo2 == 'False' ||
-                              saving.additionalInfo2 == 'false' ? (
-                                <p className="text-xs font-medium text-black break-word-text">
-                                  {saving?.code}
-                                </p>
+                              {saving.additionalInfo2 == 'False' || saving.additionalInfo2 == 'false' ? (
+                                <p className="text-xs font-medium text-black break-word-text"> {saving?.code} </p>
                               ) : (
-                                <p className="mfont-medium text-letf text-black font-12 break-word-text">
-                                  {saving?.code}
-                                </p>
+                                <p className="font-medium text-left text-black font-12 break-word-text"> {saving?.code} </p>
                               )}
                             </span>
                           )}
-                          <p className="mt-1 text-sm font-medium text-gray-700 font-10 break-word-text">
-                            {saving?.additionalInfo6}
-                          </p>
+                          <p className="mt-1 text-sm font-medium text-gray-700 font-10 break-word-text"> {saving?.additionalInfo6} </p>
                         </div>
                       </div>
                     </div>
