476047392e5832799ffe2c9cdba267d2858ec61c
Amit Kumar <91612827+amitkumar1403@users.noreply.github.com>
2024-05-21 20:00:16 +0530

    Fix Build

--- framework/commerce/utils/use-device.ts
+++ framework/commerce/utils/use-device.ts
@@ -1,51 +1,53 @@
 // Package Imports
-import { isDesktop, isMobile } from "react-device-detect";
+import { isDesktop, isMobile } from 'react-device-detect'
 
-declare const window: any;
+declare const window: any
 
 interface DeviceDetection {
-    isMobile: boolean;
-    isDesktop: boolean;
-    isIPadorTablet: boolean;
-    deviceType: DeviceType;
+  isMobile: boolean
+  isDesktop: boolean
+  isIPadorTablet: boolean
+  deviceType: DeviceType
 }
 
 export enum DeviceType {
-    DESKTOP = 1,
-    MOBILE = 2,
-    TABLET = 3,
+  UNKNOWN = 0,
+  DESKTOP = 1,
+  MOBILE = 2,
+  TABLET = 3,
 }
 
 const useDevice = (): DeviceDetection => {
-    const UA = navigator.userAgent;
-    const isIPadorTablet = /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA);
+  const UA = navigator.userAgent
+  const isIPadorTablet = /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
 
-    /**
-     * Determine the mobile operating system.
-     * This function returns one of 'IOS', 'ANDROID', 'WINDOWS_PHONE', or 'UNKNOWN'.
-     *
-     * @returns {String}
-     */
-    const getDeviceType = () => {
-      var userAgent = navigator.userAgent || navigator.vendor || window.opera
-      const mobileRegex = /Android|webOS|BlackBerry|IEMobile|Opera Mini|iPhone|iPod|Windows Phone/i;
-      const tabletRegex = /iPad|Android(?!.*(mobile|mobi)).*?(Tablet|Tab)/i;
+  /**
+   * Determine the mobile operating system.
+   * This function returns one of 'IOS', 'ANDROID', 'WINDOWS_PHONE', or 'UNKNOWN'.
+   *
+   * @returns {String}
+   */
+  const getDeviceType = () => {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera
+    const mobileRegex =
+      /Android|webOS|BlackBerry|IEMobile|Opera Mini|iPhone|iPod|Windows Phone/i
+    const tabletRegex = /iPad|Android(?!.*(mobile|mobi)).*?(Tablet|Tab)/i
 
-      if ( mobileRegex.test(userAgent)) {
-        return DeviceType.MOBILE
-      }else if (tabletRegex.test(userAgent)) {
-        return DeviceType.TABLET
-      }else {
-        return DeviceType.DESKTOP
-      }
+    if (mobileRegex.test(userAgent)) {
+      return DeviceType.MOBILE
+    } else if (tabletRegex.test(userAgent)) {
+      return DeviceType.TABLET
+    } else {
+      return DeviceType.DESKTOP
     }
+  }
 
-    return {
-        isMobile,
-        isDesktop: (isMobile || isIPadorTablet) ? false : isDesktop,
-        isIPadorTablet,
-        deviceType: getDeviceType(),
-    }
-};
+  return {
+    isMobile,
+    isDesktop: isMobile || isIPadorTablet ? false : isDesktop,
+    isIPadorTablet,
+    deviceType: getDeviceType(),
+  }
+}
 
-export default useDevice;
+export default useDevice
