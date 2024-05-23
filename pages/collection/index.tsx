84a1c6a4544a3220fd4112f8c0689a311ddf9f62
Amit Kumar <91612827+amitkumar1403@users.noreply.github.com>
2024-05-23 15:38:53 +0530

    Remove Collection count

--- pages/collection/index.tsx
+++ pages/collection/index.tsx
@@ -21,14 +21,11 @@ import useAnalytics from '@components/services/analytics/useAnalytics'
 import { EVENTS_MAP } from '@components/services/analytics/constants'
 
 function CollectionList(props: any) {
-  const router =useRouter();
+  const router = useRouter();
   const translate = useTranslation()
 
   useAnalytics(EVENTS_MAP.EVENT_TYPES.CollectionViewed, {
-    entity: JSON.stringify({
-      id: props?.id || EmptyGuid,
-      name: props?.name || EmptyString,
-    }),
+    entity: JSON.stringify({ id: props?.id || EmptyGuid, name: props?.name || EmptyString, }),
     entityId: props?.id || EmptyGuid,
     entityName: props?.name || EmptyString,
     entityType: EVENTS_MAP.ENTITY_TYPES.Collection,
@@ -48,7 +45,7 @@ function CollectionList(props: any) {
         <meta property="og:title" content={translate('label.collection.collectionsText')} key="ogtitle" />
         <meta property="og:description" content={translate('label.collection.collectionsText')} key="ogdesc" />
         <meta property="og:site_name" content={SITE_NAME} key="ogsitename" />
-        <meta property="og:url" content={SITE_ORIGIN_URL + router.asPath}  key="ogurl" />
+        <meta property="og:url" content={SITE_ORIGIN_URL + router.asPath} key="ogurl" />
       </NextHead>
       <main className="container w-full mx-auto theme-account-container dark:bg-white">
         <section aria-labelledby="products-heading" className="mt-12">
@@ -56,24 +53,19 @@ function CollectionList(props: any) {
             {translate('label.collection.shopByCollectionText')}
           </h1>
           {props?.data.length > 0 && (
-            <div className="grid grid-cols-2 py-10 sm:gap-y-3 gap-y-3 sm:grid-cols-5 gap-x-3 lg:grid-cols-4 xl:gap-x-3">
+            <div className="grid grid-cols-2 py-10 sm:gap-y-3 sm:grid-cols-5 gap-x-3 gap-y-4 lg:grid-cols-4 xl:gap-x-3">
               {props.data.map((collection: any, key: any) => (
                 <Link key={key} passHref href={`/collection/${collection.slug}`}>
-                  <span key={collection.id} className="group">
+                  <span key={collection.id} className="block mb-8 group">
                     <div className="relative w-full pb-0 overflow-hidden bg-gray-100 rounded-lg aspect-w-1 aspect-h-1 sm:aspect-w-2 sm:aspect-h-3">
                       <div className="relative image-container">
                         <img src={IMG_PLACEHOLDER} alt={collection.name || 'image'} className="object-cover object-center w-full h-full group-hover:opacity-75 image" />
                       </div>
                     </div>
-                    <div className="flex-1">
-                      <h2 className="flex w-full pt-2 text-sm font-medium text-gray-900 sm:text-xl text-md">
+                    <div className="justify-center flex-1 w-full text-center">
+                      <h2 className="flex justify-center w-full pt-2 text-sm font-medium text-gray-900 sm:text-xl text-md">
                         {collection.name}
                       </h2>
-
-                      <h4 className="w-full pt-1 text-xs font-normal text-gray-500 sm:text-sm">
-                        {collection.noOfRecords}{' '}
-                        <span className="italic">{translate('label.collection.productAvailableText')}</span>
-                      </h4>
                     </div>
                   </span>
                 </Link>
@@ -81,13 +73,11 @@ function CollectionList(props: any) {
             </div>
           )}
           {props?.data.length == 0 && (
-            <>
-              <div className="flex flex-col py-32 text-center">
-                <h2 className="w-full mx-auto text-4xl font-bold text-gray-200">
-                  {translate('label.collection.noCollectionAvailableText')}
-                </h2>
-              </div>
-            </>
+            <div className="flex flex-col py-32 text-center">
+              <h2 className="w-full mx-auto text-4xl font-bold text-gray-200">
+                {translate('label.collection.noCollectionAvailableText')}
+              </h2>
+            </div>
           )}
         </section>
       </main>
@@ -106,7 +96,7 @@ export async function getStaticProps({
   const props: IPagePropsProvider = getPagePropType({ type: PagePropType.COMMON })
   const pageProps = await props.getPageProps({ cookies: {} })
   const collectionUID = Redis.Key.Collection
-  const cachedData = await getDataByUID([ collectionUID ])
+  const cachedData = await getDataByUID([collectionUID])
   let collectionUIDData: any = parseDataValue(cachedData, collectionUID) || []
   try {
     if (!containsArrayData(collectionUIDData)) {
