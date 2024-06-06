2f6cf5e5c84fc4db6e9860836637cd50ad0edc1c
imtarun1 <tarun.singh@bettercommerce.io>
2024-06-06 18:20:51 +0530

    re added an api

---
+++ framework/bettercommerce/api/content/lookbook.ts
@@ -0,0 +1,17 @@
+import fetcher from '../../fetcher'
+import { LOOKBOOK_ENDPOINT } from '@components/utils/constants'
+export default function getLookbook() {
+  async function getLookbookAsync() {
+    try {
+      const response: any = await fetcher({
+        url: LOOKBOOK_ENDPOINT,
+        method: 'GET',
+      })
+      return response.result
+    } catch (error: any) {
+      console.log(error)
+      // throw new Error(error.message)
+    }
+  }
+  return getLookbookAsync()
+}
