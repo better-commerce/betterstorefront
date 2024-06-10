961cb5b434c007ddd5c0660d267e98b0b9eb312e
btGauravGassi <118798729+btGauravGassi@users.noreply.github.com>
2024-06-10 19:16:11 +0530

    Reverting engage log changes

--- components/utils/dataLayer.ts
+++ components/utils/dataLayer.ts
@@ -61,7 +61,7 @@ const DataLayerSingleton = function () {
       visitorAffiliate: EmptyString,
       visitorEmail: visitorData?.email || EmptyString,
       visitorExistingCustomer: Boolean(visitorData?.username),
-      visitorId: visitorData?.userId || EmptyString,
+      visitorId: visitorData?.userId || EmptyGuid,
       visitorLoggedIn: Boolean(visitorData?.email),
       visitorSegment: EmptyString,
     }
