b5cc8a5253b8d0c8eb32d6119f654f312ea6b696
btGauravGassi <118798729+btGauravGassi@users.noreply.github.com>
2024-06-26 18:29:45 +0530

    resolve comments

--- framework/bettercommerce/api/operations/get-dataPack.ts
+++ framework/bettercommerce/api/operations/get-dataPack.ts
@@ -1,17 +1,18 @@
+import { logError } from '@framework/utils/app-util'
 import fetcher from '../../fetcher'
 import { B2B_COMPANY_USERS } from '@components/utils/constants'
 export default function getDataPacks() {
   async function getDataPackAsync({ query, cookies }: any) {
     try {
       const response: any = await fetcher({
-        url: `${B2B_COMPANY_USERS}${query.companyId}/data-packs`,
+        url: `${B2B_COMPANY_USERS}${query?.companyId}/data-packs`,
         method: 'get',
         data: query,
         cookies,
       })
       return response.result
     } catch (error: any) {
-      console.log(error, 'error')
+      logError(error)
       throw new Error(error)
     }
   }
