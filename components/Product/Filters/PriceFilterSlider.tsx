9c9ee81ac6a4e09d6e56fbb1de0bf785546d97b6
btGauravGassi <118798729+btGauravGassi@users.noreply.github.com>
2024-06-28 19:06:48 +0530

    Fix Price Filter

--- components/Product/Filters/PriceFilterSlider.tsx
+++ components/Product/Filters/PriceFilterSlider.tsx
@@ -2,7 +2,7 @@ import { useState, useCallback, useMemo } from 'react'
 import Range from 'rc-slider'
 import Tooltip from 'rc-tooltip'
 import 'rc-slider/assets/index.css'
-import lodash from 'lodash'
+import debounce from 'lodash/debounce'
 import { useTranslation } from '@commerce/utils/use-translation'
 
 interface PriceFilterSliderProps {
@@ -15,38 +15,37 @@ interface PriceFilterSliderProps {
 const PriceFilterSlider: React.FC<PriceFilterSliderProps> = ({
   handleFilters,
   sectionKey,
-  items,
+  items = [],
   routerFilters,
 }) => {
   const translate = useTranslation()
   const priceFilter = routerFilters?.find((filter: any) => { return filter?.Key === sectionKey })
   const step = 10
   const prices = [
-    ...items.filter((item) => item?.to !== null).map((item) => item?.to),
-    ...items.filter((item) => item?.from !== null).map((item) => item?.from),
+    ...items?.filter((item) => item?.to !== null).map((item) => item?.to),
+    ...items?.filter((item) => item?.from !== null).map((item) => item?.from),
   ]
   const maxPrice = Math.max(...prices)
   const minPrice = Math.min(...prices)
   const limits = useMemo(() => { return { minFrom: minPrice, maxTo: maxPrice }; }, [minPrice, maxPrice]);
   const marks = useMemo(() => {
-    const validMin = !isNaN(limits.minFrom) ? limits.minFrom : 0;
-    const validMax = !isNaN(limits.maxTo) ? limits.maxTo : 100;
+    const validMin = !isNaN(limits?.minFrom) ? limits?.minFrom : 0;
+    const validMax = !isNaN(limits?.maxTo) ? limits?.maxTo : 100;
     
     return {
       [validMin]: `${validMin}`, 
       [validMax]: `${validMax}`,
     };
-  }, [limits.minFrom, limits.maxTo]);
+  }, [limits?.minFrom, limits?.maxTo]);
   
   const [selectedRange, setSelectedRange] = useState<[number, any]>([
     priceFilter ? parseInt(priceFilter?.Value?.split('-')[0]) : limits?.minFrom,
     priceFilter ? (priceFilter?.Value?.split('-')[1])==='*'? limits?.maxTo + step : (parseInt(priceFilter?.Value?.split('-')[1])) : limits?.maxTo + step,
   ])
 
-
   // Debounce the entire slider change function
-  const debouncedSetRange = useCallback(lodash.debounce((range: any) => {
-    routerFilters.forEach((filter: any) => {
+  const debouncedSetRange = useCallback(debounce((range: any) => {
+    routerFilters?.forEach((filter: any) => {
       if (filter?.Key === sectionKey) {
         handleFilters(filter, 'REMOVE_FILTERS');
       }
@@ -57,12 +56,11 @@ const PriceFilterSlider: React.FC<PriceFilterSliderProps> = ({
   function onSliderChange(value: any) {
     setSelectedRange(value);
 
-    const upperRange = value[1] > limits.maxTo ? '*' : value[1];
+    const upperRange = value[1] > limits?.maxTo ? '*' : value[1];
     const resultRange = {
-      Key: 'price.raw.withTax',
+      Key: sectionKey,
       Value: `${value[0]}-${upperRange}`,
       IsSelected: true,
-      name: 'Price',
     };
 
     debouncedSetRange(resultRange);
@@ -73,8 +71,8 @@ const PriceFilterSlider: React.FC<PriceFilterSliderProps> = ({
       <div>
         <div className="w-full px-4">
           <Range
-            min={limits.minFrom}
-            max={limits.maxTo + step}
+            min={limits?.minFrom}
+            max={limits?.maxTo + step}
             marks={marks}
             step={step}
             range
@@ -107,7 +105,7 @@ const PriceFilterSlider: React.FC<PriceFilterSliderProps> = ({
           {translate('label.product.priceFilterSlider.minText')}: {selectedRange[0]}
           <br />
           {translate('label.product.priceFilterSlider.maxText')}:{' '}
-          {selectedRange[1] > limits.maxTo || selectedRange[1] === '*' ? translate('label.product.priceFilterSlider.noLimitText') : selectedRange[1]}
+          {selectedRange[1] > limits?.maxTo || selectedRange[1] === '*' ? translate('label.product.priceFilterSlider.noLimitText') : selectedRange[1]}
         </p>
       </div>
     )
