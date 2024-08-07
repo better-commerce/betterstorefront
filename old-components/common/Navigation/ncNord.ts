9dadc83bd64f1ef6c677410928387e44175c7f90
Amit Kumar <91612827+amitkumar1403@users.noreply.github.com>
2024-08-07 15:46:15 +0530

    Remove Client specific theme and old-component files

--- old-components/common/Navigation/ncNord.ts
+++
@@ -1,5 +0,0 @@
-export default function ncNanoId(prefix = "nc_") {
-  return (
-    prefix + Date.now() + "_" + Math.floor(Math.random() * 999999999999999)
-  );
-}
--- old-components/services/analytics/eventDispatcher.ts
+++
@@ -1,6 +0,0 @@
-export default function eventDispatcher(eventType: string, payload: any) {
-  let event = new CustomEvent(eventType, {
-    detail: { action: eventType, payload },
-  })
-  window.dispatchEvent(event)
-}
