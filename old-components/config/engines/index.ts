15396ca9bb7b19a3fa2f745ebb39f431226e9b37
Gagan <gagandeep.singh@bettercommerce.io>
2024-08-07 15:58:08 +0530

    Revert "Merge branch 'master' of https://omnicx.visualstudio.com/CustomerStores/_git/betterstorefront"
    This reverts commit 288b7abbbfa212ec329dc2eef0a5f6adaa2ceb00, reversing
    changes made to a34e0819d3e0724616bf8a3970cf5f0b95c91a10.

--- old-components/config/engines/index.ts
+++
@@ -1,9 +0,0 @@
-import FFXSearchDemo2 from './ffx-search-demo2.json'
-import BetterStoreSearch from './betterstore-search.json'
-
-const config = {
-  'ffx-search-demo2': FFXSearchDemo2,
-  'betterstore-search': BetterStoreSearch,
-}
-
-export { config as ElasticEngineConfig }
