9eb5324f30ffdc6fa017f2474a6aa8b59fe0d45f
Amit Kumar <91612827+amitkumar1403@users.noreply.github.com>
2024-08-07 11:55:03 +0530

    Update Layouts for fix favicons

--- components/Layout/CheckoutLayoutV2.tsx
+++ components/Layout/CheckoutLayoutV2.tsx
@@ -104,73 +104,73 @@ const CheckoutLayoutV2: FC<any> = ({ children }) => {
         <link
           rel="apple-touch-icon"
           sizes="57x57"
-          href={`https://cdnbs.bettercommerce.tech/theme/${CURRENT_THEME}/favicon/apple-icon-57x57.png`}
+          href={`/theme/${CURRENT_THEME}/favicon/apple-icon-57x57.png`}
         />
         <link
           rel="apple-touch-icon"
           sizes="60x60"
-          href={`https://cdnbs.bettercommerce.tech/theme/${CURRENT_THEME}/favicon/apple-icon-60x60.png`}
+          href={`/theme/${CURRENT_THEME}/favicon/apple-icon-60x60.png`}
         />
         <link
           rel="apple-touch-icon"
           sizes="72x72"
-          href={`https://cdnbs.bettercommerce.tech/theme/${CURRENT_THEME}/favicon/apple-icon-72x72.png`}
+          href={`/theme/${CURRENT_THEME}/favicon/apple-icon-72x72.png`}
         />
         <link
           rel="apple-touch-icon"
           sizes="76x76"
-          href={`https://cdnbs.bettercommerce.tech/theme/${CURRENT_THEME}/favicon/apple-icon-76x76.png`}
+          href={`/theme/${CURRENT_THEME}/favicon/apple-icon-76x76.png`}
         />
         <link
           rel="apple-touch-icon"
           sizes="114x114"
-          href={`https://cdnbs.bettercommerce.tech/theme/${CURRENT_THEME}/favicon/apple-icon-114x114.png`}
+          href={`/theme/${CURRENT_THEME}/favicon/apple-icon-114x114.png`}
         />
         <link
           rel="apple-touch-icon"
           sizes="120x120"
-          href={`https://cdnbs.bettercommerce.tech/theme/${CURRENT_THEME}/favicon/apple-icon-120x120.png`}
+          href={`/theme/${CURRENT_THEME}/favicon/apple-icon-120x120.png`}
         />
         <link
           rel="apple-touch-icon"
           sizes="144x144"
-          href={`https://cdnbs.bettercommerce.tech/theme/${CURRENT_THEME}/favicon/apple-icon-144x144.png`}
+          href={`/theme/${CURRENT_THEME}/favicon/apple-icon-144x144.png`}
         />
         <link
           rel="apple-touch-icon"
           sizes="152x152"
-          href={`https://cdnbs.bettercommerce.tech/theme/${CURRENT_THEME}/favicon/apple-icon-152x152.png`}
+          href={`/theme/${CURRENT_THEME}/favicon/apple-icon-152x152.png`}
         />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href={`https://cdnbs.bettercommerce.tech/theme/${CURRENT_THEME}/favicon/apple-icon-180x180.png`}
+          href={`/theme/${CURRENT_THEME}/favicon/apple-icon-180x180.png`}
         />
         <link
           rel="icon"
           type="image/png"
           sizes="192x192"
-          href={`https://cdnbs.bettercommerce.tech/theme/${CURRENT_THEME}/favicon/android-icon-192x192.png`}
+          href={`/theme/${CURRENT_THEME}/favicon/android-icon-192x192.png`}
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href={`https://cdnbs.bettercommerce.tech/theme/${CURRENT_THEME}/favicon/favicon-32x32.png`}
+          href={`/theme/${CURRENT_THEME}/favicon/favicon-32x32.png`}
         />
         <link
           rel="icon"
           type="image/png"
           sizes="96x96"
-          href={`https://cdnbs.bettercommerce.tech/theme/${CURRENT_THEME}/favicon/favicon-96x96.png`}
+          href={`/theme/${CURRENT_THEME}/favicon/favicon-96x96.png`}
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href={`https://cdnbs.bettercommerce.tech/theme/${CURRENT_THEME}/favicon/favicon-16x16.png`}
+          href={`/theme/${CURRENT_THEME}/favicon/favicon-16x16.png`}
         />
-        <link rel="icon" href={`https://cdnbs.bettercommerce.tech/theme/${CURRENT_THEME}/favicon/favicon.ico`} />
+        <link rel="icon" href={`/theme/${CURRENT_THEME}/favicon/favicon.ico`} />
         {headElements}
       </Head>
       <main className="sm:fit gradient">
