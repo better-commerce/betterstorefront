58a73e3fccf9d1297f81c4b44fd86d61db5670ea
btGauravGassi <118798729+btGauravGassi@users.noreply.github.com>
2024-08-16 15:58:05 +0530

    Fix membership undefined

--- framework/bettercommerce/api/operations/get-orders.ts
+++ framework/bettercommerce/api/operations/get-orders.ts
@@ -4,7 +4,7 @@ export default function getOrders() {
   async function getOrdersAsync({ query, cookies }: any) {
     try {
       const response: any = await fetcher({
-        url: `${CUSTOMER_BASE_API}${query.id}/orders?hasMembership=${query.hasMembership}`,
+        url: `${CUSTOMER_BASE_API}${query.id}/orders?hasMembership=${query?.hasMembership ?? false}`,
         method: 'get',
         data: query,
         cookies,
