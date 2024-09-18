e1ddbf46c06b292a2773ceb9f1022cec255d7460
Amit Kumar <91612827+amitkumar1403@users.noreply.github.com>
2024-09-18 18:56:13 +0530

    Fix Border issue

--- components/customer/index.tsx
+++ components/customer/index.tsx
@@ -179,7 +179,7 @@ export default function CustomerForm({
                         !formItem?.show || (formItem?.show && formItem?.show(values)) ? (
                           <>
                             <label className="text-neutral-800 dark:text-neutral-200"> {formItem?.label} </label>
-                            <Field key={idx} name={formItem?.key} placeholder={formItem?.placeholder} onChange={handleChange} value={values[formItem?.key]}type={formItem?.type} className="block w-full px-4 py-3 mt-1 text-sm font-normal bg-white border-neutral-200 focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 dark:border-neutral-700 dark:focus:ring-primary-6000 dark:focus:ring-opacity-25 dark:bg-neutral-900 disabled:bg-neutral-200 dark:disabled:bg-neutral-800 rounded-2xl h-11 " />
+                            <Field key={idx} name={formItem?.key} placeholder={formItem?.placeholder} onChange={handleChange} value={values[formItem?.key]}type={formItem?.type} className="block w-full px-4 py-3 mt-1 text-sm font-normal bg-white border border-neutral-200 focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 dark:border-neutral-700 dark:focus:ring-primary-6000 dark:focus:ring-opacity-25 dark:bg-neutral-900 disabled:bg-neutral-200 dark:disabled:bg-neutral-800 rounded-2xl h-11 " />
                             {errors[formItem?.key] && touched[formItem?.key] ? (
                               <div className="mb-2 font-medium text-red-400 font-12">
                                 {errors[formItem?.key]}
