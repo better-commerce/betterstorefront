74c1c34af68636b1ff300f89d61418cad2c8440c
Amit Kumar <91612827+amitkumar1403@users.noreply.github.com>
2024-09-18 18:27:10 +0530

    Fix Header and trigger new build for config cange

--- components/Header/MainNav2Logged.tsx
+++ components/Header/MainNav2Logged.tsx
@@ -137,7 +137,7 @@ function handleWishlist() {
               {featureToggle?.features?.enableLanguage &&
                 <LangDropdown currencies={currencies} languages={languages} defaultLanguage={defaultLanguage} defaultCountry={defaultCountry} />
               }
-              <button className="items-center justify-center w-10 h-10 rounded-full lg:flex sm:w-12 sm:h-12 text-slate-700 dark:text-slate-300 hover:bg-slate-100 dark:hover:bg-slate-800 focus:outline-none">
+              <button className="items-center justify-center w-10 h-10 rounded-full lg:flex sm:w-12 sm:h-12 group text-slate-700 dark:text-slate-300 hover:bg-slate-100 dark:hover:bg-slate-800 focus:outline-none">
                 {renderMagnifyingGlassIcon()}
               </button>
 
