d2444ac734135e9ae7858dbc59917c1786ab4391
Amit Kumar <91612827+amitkumar1403@users.noreply.github.com>
2024-10-23 10:43:08 +0530

    Update B2B Quotes UI

--- components/account/B2BQuotes.tsx
+++ components/account/B2BQuotes.tsx
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from 'react'
 import { useTranslation } from '@commerce/utils/use-translation'
 import { DATE_FORMAT, QuoteStatus } from '@components/utils/constants'
 import moment from 'moment'
-import { EyeIcon } from '@heroicons/react/24/outline'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 
@@ -17,8 +16,8 @@ function B2BQuotes({ quotes }: any) {
 
   const filteredQuotes = quotesList?.filter((rfq: any) => {
     return (
-      rfq?.rfqNumber.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      rfq?.customQuoteNo?.toLowerCase().includes(searchTerm.toLowerCase())
+      rfq?.rfqNumber?.toLowerCase()?.includes(searchTerm.toLowerCase()) ||
+      rfq?.customQuoteNo?.toLowerCase()?.includes(searchTerm.toLowerCase())
     );
   });
 
@@ -39,22 +38,10 @@ function B2BQuotes({ quotes }: any) {
   return (
     <section className="w-full">
       <div className="flex items-center justify-between w-full gap-4 mb-4">
-        <h1 className="text-xl font-normal sm:text-2xl dark:text-black">
-          Quotes
-        </h1>
+        <h1 className="text-xl font-normal sm:text-2xl dark:text-black"> Quotes </h1>
         <div className='flex gap-3 justify-normal'>
-          <input
-            type="text"
-            placeholder="Search by Quote/RFQ No."
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            className="p-2 text-sm font-normal border border-gray-200 rounded-md w-72"
-          />
+          <input type="text" placeholder="Search by Quote/RFQ No." value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} className="p-2 text-sm font-normal border border-gray-200 rounded-md w-72" />
         </div>
-
-      </div>
-      <div className=''>
-
       </div>
       {!quotes ? (<Spinner />) : (
         <>
@@ -76,20 +63,14 @@ function B2BQuotes({ quotes }: any) {
                     return (
                       <tr key={`quote-list-${Idx}`} className="text-xs bg-white border-b shadow-none group border-slate-200 hover:shadow hover:bg-gray-100">
                         <td className="py-3 pl-3 pr-3 text-[13px] font-medium cursor-pointer text-sky-500 whitespace-nowrap sm:pl-4 hover:text-sky-600">
-                          <Link href={`/my-account/my-company/quotes/${quote?.quoteId}`}>
-                            {quote?.customQuoteNo}
-                          </Link>
+                          <Link href={`/my-account/my-company/quotes/${quote?.quoteId}`}>{quote?.customQuoteNo}</Link>
                         </td>
                         <td className="px-3 py-3 text-[13px] font-medium cursor-pointer text-sky-500 sm:pl-4 hover:text-sky-600 whitespace-nowrap">
-                          <Link href={`/my-account/request-for-quote/rfq/${quote?.rfqId}`}>
-                            {quote?.rfqNumber}
-                          </Link>
+                          <Link href={`/my-account/request-for-quote/rfq/${quote?.rfqId}`}>{quote?.rfqNumber}</Link>
                         </td>
                         <td className="flex flex-col gap-0 px-3 py-3 text-[13px] font-medium text-black">
                           <span> {quote?.userName}</span>
-                          <span className="font-Inter font-light leading-4 text-xs text-gray-500 tracking-[2%]">
-                            {quote?.email}
-                          </span>
+                          <span className="font-Inter font-light leading-4 text-xs text-gray-500 tracking-[2%]"> {quote?.email} </span>
                         </td>
                         <td className="px-3 py-3 text-[13px] text-gray-500 whitespace-nowrap">
                           {quote?.status != QuoteStatus.CONVERTED && quote?.status != QuoteStatus.PAYMENT_LINK_SENT &&
@@ -101,32 +82,28 @@ function B2BQuotes({ quotes }: any) {
                             (() => {
                               const validUntilDate = moment(new Date(quote?.validUntil));
                               const currentDate = moment();
-                              let validDays = validUntilDate.diff(currentDate, 'days') + 1; // Add 1 to include both dates
-
-                              // Ensure validDays does not go below 0
+                              let validDays = validUntilDate.diff(currentDate, 'days') + 1;
                               validDays = validDays < 0 ? 0 : validDays;
-
                               return `${validDays} ${translate('label.product.productSidebar.daysText')}`;
                             })()
                           }
                         </td>
-
                         <td align='right' className="px-3 py-3 text-[13px] font-semibold text-gray-500 whitespace-nowrap">
                           {
                             quote?.status == QuoteStatus.ABANDONED ?
-                              <span className='px-3 py-1 text-xs text-red-600 bg-red-200 border border-red-400 rounded-full'>Abandoned</span> :
-                              quote?.status == QuoteStatus.CANCELLED ?
-                                <span className='px-3 py-1 text-xs text-red-600 bg-red-200 border border-red-400 rounded-full'>Cancelled</span> :
-                                quote?.status == QuoteStatus.CONVERTED ?
-                                  <span className='px-3 py-1 text-xs border rounded-full text-emerald-600 bg-emerald-200 border-emerald-400'>Converted</span> :
-                                  quote?.status == QuoteStatus.DRAFT ?
-                                    <span className='px-3 py-1 text-xs text-gray-600 bg-gray-200 border border-gray-400 rounded-full'>Draft</span> :
-                                    quote?.status == QuoteStatus.NOT_QUOTE ?
-                                      <span className='px-3 py-1 text-xs text-gray-600 bg-gray-100 border border-gray-400 rounded-full'>Not Quote</span> :
-                                      quote?.status == QuoteStatus.PAYMENT_LINK_SENT ?
-                                        <span className='px-3 py-1 text-xs border rounded-full text-sky-600 bg-sky-200 border-sky-400'>Payment Link Sent</span> :
-                                        quote?.status == QuoteStatus.QUOTE_SENT ?
-                                          <span className='px-3 py-1 text-xs text-purple-600 bg-purple-100 border border-purple-400 rounded-full'>Quote Sent</span> : ''
+                            <span className='px-3 py-1 text-xs text-red-600 bg-red-200 border border-red-400 rounded-full'>Abandoned</span> :
+                            quote?.status == QuoteStatus.CANCELLED ?
+                            <span className='px-3 py-1 text-xs text-red-600 bg-red-200 border border-red-400 rounded-full'>Cancelled</span> :
+                            quote?.status == QuoteStatus.CONVERTED ?
+                            <span className='px-3 py-1 text-xs border rounded-full text-emerald-600 bg-emerald-200 border-emerald-400'>Converted</span> :
+                            quote?.status == QuoteStatus.DRAFT ?
+                            <span className='px-3 py-1 text-xs text-gray-600 bg-gray-200 border border-gray-400 rounded-full'>Draft</span> :
+                            quote?.status == QuoteStatus.NOT_QUOTE ?
+                            <span className='px-3 py-1 text-xs text-gray-600 bg-gray-100 border border-gray-400 rounded-full'>Not Quote</span> :
+                            quote?.status == QuoteStatus.PAYMENT_LINK_SENT ?
+                            <span className='px-3 py-1 text-xs border rounded-full text-sky-600 bg-sky-200 border-sky-400'>Payment Link Sent</span> :
+                            quote?.status == QuoteStatus.QUOTE_SENT ?
+                            <span className='px-3 py-1 text-xs text-purple-600 bg-purple-100 border border-purple-400 rounded-full'>Quote Sent</span> : ''
                           }
                         </td>
                       </tr>
@@ -153,5 +130,4 @@ function B2BQuotes({ quotes }: any) {
     </section>
   )
 }
-
 export default B2BQuotes
