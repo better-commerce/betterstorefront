601193389300fd64d906f4822688705ea3268cf6
Amit Kumar <91612827+amitkumar1403@users.noreply.github.com>
2025-01-22 17:07:49 +0530

    Fix Demo links

--- components/cms-banner/heroBannerImage.tsx
+++ components/cms-banner/heroBannerImage.tsx
@@ -14,8 +14,8 @@ const HeroBannerImage: React.FC<IHeroBannerImageProps> = ({ header, headIdx }: I
     <section className='relative w-full bg-header-color' key={headIdx}>
       <div className='grid grid-cols-1 gap-0 mx-auto container-ffx sm:overflow-hidden sm:grid-cols-12'>
         <div className='flex flex-col justify-center h-full py-6 text-left left-info sm:col-span-5 sm:py-0'>
-          <h1 className="text-white dark:text-white font-semibold sm:tracking-[7.2px] tracking-wider uppercase">{header?.hero_title}</h1>
-          <div className="py-2 text-white dark:text-white font-46 banner-subheading page-list-bullet" dangerouslySetInnerHTML={{ __html: header?.hero_description }}></div>
+          <h1 className="text-white heading-h1 dark:text-white font-semibold sm:tracking-[7.2px] tracking-wider uppercase">{header?.hero_title}</h1>
+          <div className="py-2 text-white dark:text-white font-46 banner-subheading page-list-bullet heading-h3" dangerouslySetInnerHTML={{ __html: header?.hero_description }}></div>
         </div>
         <div className='relative flex flex-col justify-center h-full right-info sm:pl-12 sm:col-span-7'>
           <Patterns />
