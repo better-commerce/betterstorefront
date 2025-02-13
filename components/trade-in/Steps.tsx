fcf036247e535b0f268f34c8c4d599f8f0b8d18c
Vinay Sharma <vinay.sharma@omnicx.com>
2025-02-13 05:17:04 +0000

    Merged PR 32296: 14498_VS_Fix TradeIn Journey Steps
    Fix TradeIn Journey Steps
    
    Related work items: #14498

--- components/trade-in/Steps.tsx
+++ components/trade-in/Steps.tsx
@@ -1,14 +1,31 @@
+import { useState } from "react";
+
 export default function Steps({ data, setCurrentStep, currentStep }: any) {
+  const [completedSteps, setCompletedSteps] = useState<boolean[]>(new Array(data.length).fill(false));
+
+  const handleStepChange = (index: number) => {
+    setCurrentStep(index);
+  };
+  const handleStepCompletion = () => {
+    const newCompletedSteps = [...completedSteps];
+    newCompletedSteps[currentStep] = true;
+    setCompletedSteps(newCompletedSteps);
+    if (currentStep < data.length - 1) {
+      setCurrentStep(currentStep + 1);
+    }
+  };
   return (
     <div className="flex flex-col w-8/12 mx-auto mt-10 mb-10">
       <div className='step-indicator'>
-        {data?.map((step: any, index: number) => (
+        {data.map((step: any, index: number) => (
           <>
-            <button onClick={() => setCurrentStep(index)} className={`step step${index + 1} ${currentStep >= index ? "active" : ""}`} >
-              <div className='step-icon'>{step?.step}</div>
-              <p className='text-sm !font-semibold'>{step?.name}</p>
+            <button onClick={() => handleStepChange(index)} 
+              disabled={index > currentStep && !completedSteps[index - 1]} // Prevents skipping steps
+              className={`step step${index + 1} ${currentStep >= index ? "active" : ""}`} >
+              <div className='step-icon'>{step.step}</div>
+              <p className='text-sm !font-semibold'>{step.name}</p>
             </button>
-            {index < data?.length - 1 && (
+            {index < data.length - 1 && (
               <div id={`StepLine${index + 1}`} className={`indicator-line ${currentStep >= index + 1 ? "active" : ""}`} ></div>
             )}
           </>
