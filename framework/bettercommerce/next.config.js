f3ae5e464a29beb7c194322ca5a4100bd0cfb5a1
Mohit Das <mohit.das@bettercommerce.io>
2025-02-21 06:47:01 +0000

    Merged PR 32413: Revert "Merged PR 32397: 14536_AS_cms_redirects
    Revert "Merged PR 32397: 14536_AS_cms_redirects
    
    Added cms redirects to existing api20 redirects.
    
    Implemented api to get all cms redirects and integrated both redirects of cms and api20.
    
    Related work items: #14536"
    
    Reverted commit `c69f313b`.

--- framework/bettercommerce/next.config.js
+++ framework/bettercommerce/next.config.js
@@ -6,7 +6,6 @@ const path = require('path')
 const CLIENT_ID = process.env.BETTERCOMMERCE_CLIENT_ID
 const SHARED_SECRET = process.env.BETTERCOMMERCE_SHARED_SECRET
 const BASE_URL = process.env.BETTERCOMMERCE_BASE_URL
-const BETTERCMS_BASE_URL = process.env.BETTERCMS_BASE_URL
 const AUTH_URL = process.env.BETTERCOMMERCE_AUTH_URL
 const INFRA_ENDPOINT = `api/${process.env.NEXT_PUBLIC_API_VERSION}/infra/config`
 const fs = require('fs')
@@ -78,7 +77,7 @@ const getRedirects = async function (token) {
     url: new URL('/api/v2/content/redirects', BASE_URL).href,
     headers: { DomainId: process.env.NEXT_PUBLIC_DOMAIN_ID, Authorization: 'Bearer ' + token, },
   })
-  const baseRedirects = response.data.result.map((item) => {
+  return response.data.result.map((item) => {
     let pathName = ''
     try {
       pathName = new URL(item.oldUrl).pathname
@@ -94,30 +93,6 @@ const getRedirects = async function (token) {
       }
     }
   })
-
-  const res = await axios({
-    method: 'get',
-    url: new URL('/api/v1/page/redirects', BETTERCMS_BASE_URL).href,
-    headers: { DomainId: process.env.NEXT_PUBLIC_DOMAIN_ID, Authorization: 'Bearer ' + token, },
-  })
-
-  const cmsRedirects = res.data.result.map((item) => {
-    let pathName = ''
-    try {
-      pathName = new URL(item?.sourceUrl).pathname
-    } catch (e) {
-      // Do nothing
-    }
-
-    if (pathName) {
-      return {
-        source: pathName, //new URL(item.oldUrl).pathname,
-        destination: item?.destinationUrl,
-        permanent: item?.redirectType?.includes('301'),
-      }
-    }
-  })
-  return baseRedirects?.concat(cmsRedirects)?.filter((item) => item?.source !== '/') || []
 }
 
 /**
