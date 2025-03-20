0e8b79eaca12a5215e11194bea6a217980ca2d9d
Gagandeep Singh <gagandeep.singh@bettercommerce.io>
2025-03-20 13:38:39 +0000

    Merged PR 32753: 0000_GS_Do not expose UserToken on network console

--- pages/account/ghostlogin.tsx
+++ pages/account/ghostlogin.tsx
@@ -19,9 +19,10 @@ import { LocalStorage } from '@components/utils/payment-constants'
 import { AlertType } from '@framework/utils/enums'
 import { saveUserToken } from '@framework/utils/app-util'
 import { useLogin } from '@framework/auth'
-import { UserAuthType } from '@framework/utils/constants'
+import { Cookie, UserAuthType } from '@framework/utils/constants'
 import Loader from '@components/Loader'
 import Layout from '@components/Layout/Layout'
+import { encrypt } from '@framework/utils/cipher'
 
 export default function GhostLoginPage({ user }: any) {
   const router = useRouter()
@@ -117,7 +118,7 @@ export default function GhostLoginPage({ user }: any) {
 GhostLoginPage.Layout = Layout
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { req } = context
+  const { req, res } = context
   const payload = {
     username: '',
     token: '',
@@ -162,9 +163,28 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
       return notFoundRedirect()
     }
 
+    let newUserSession = { ...userSession }
+    if (userSession?.userId && userSession?.userToken?.access_token) {
+
+      // Clip userToken information from the actual response that is sent to the UI layer.
+      const { userToken, ...rest } = userSession
+      let existingCookies: any = res.getHeader('Set-Cookie') || [];
+      if (!Array.isArray(existingCookies)) {
+        existingCookies = [existingCookies];
+      }
+      let newCookies = []
+      if (process.env.NODE_ENV === 'production') {
+        newCookies = [ `${Cookie.Key.USER_TOKEN}=${encrypt(userToken?.access_token)}; Path=/; HttpOnly; Secure; SameSite=Strict`, ]
+      } else {
+        newCookies = [ `${Cookie.Key.USER_TOKEN}=${encrypt(userToken?.access_token)}; Path=/;`, ]
+      }
+      res.setHeader('Set-Cookie', [...existingCookies, ...newCookies]);
+      newUserSession = { ...rest }
+    }
+
     return {
       props: {
-        user: userSession,
+        user: newUserSession,
       },
     }
   } catch (error) {
