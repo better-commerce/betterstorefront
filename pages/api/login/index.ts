3c076b1553915b53179723c85375f5d409c91f18
Gagan <gagandeep.singh@bettercommerce.io>
2025-03-20 20:04:26 +0530

    Fixed set cookie error

--- pages/api/login/index.ts
+++ pages/api/login/index.ts
@@ -3,7 +3,9 @@ import { apiMiddlewareErrorHandler } from '@framework/utils'
 import apiRouteGuard from '../base/api-route-guard'
 import { encrypt } from '@framework/utils/cipher'
 import { Cookie } from '@framework/utils/constants'
-
+import cookie, { CookieSerializeOptions } from 'cookie';
+import { getMinutesInDays } from '@components/utils/setSessionId'
+ 
 async function loginApiMiddleware(req: any, res: any) {
   const { email, password, authType }: any = req.body.data
   try {
@@ -13,17 +15,13 @@ async function loginApiMiddleware(req: any, res: any) {
       authType,
       cookies: req.cookies,
     })
-
+ 
     if (response?.userId && response?.userToken?.access_token) {
-
+ 
       // Clip userToken information from the actual response that is sent to the UI layer.
       const { userToken, ...rest } = response
-      const opts = {
-        httpOnly: true, // Cookie is not accessible via client-side JavaScript
-        secure: (process.env.NODE_ENV === 'production'), // Send only over HTTPS in production
-        sameSite: 'lax', // Helps protect against CSRF attacks
-      }
-      res.cookie(Cookie.Key.USER_TOKEN, encrypt(response?.userToken?.access_token), opts);
+      const serializedCookie = cookie.serialize(Cookie.Key.USER_TOKEN,encrypt(response.userToken.access_token), USER_TOKEN_COOKIE_OPTIONS);
+      res.setHeader('Set-Cookie', serializedCookie);
       res.status(200).json({ ...rest })
     } else {
       res.status(200).json(response)
@@ -32,5 +30,12 @@ async function loginApiMiddleware(req: any, res: any) {
     apiMiddlewareErrorHandler(req, res, error)
   }
 }
-
+ 
 export default apiRouteGuard(loginApiMiddleware)
+ 
+export const USER_TOKEN_COOKIE_OPTIONS: CookieSerializeOptions = {
+  httpOnly: true, // Cookie is not accessible via client-side JavaScript
+  secure: (process.env.NODE_ENV === 'production'), // Send only over HTTPS in production
+  sameSite: 'lax', // Helps protect against CSRF attacks
+  maxAge: getMinutesInDays(365) * 60 * 1000,
+}
