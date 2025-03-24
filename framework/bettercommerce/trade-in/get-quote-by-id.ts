9c2bfb9c905617068f01f9dc304547f8253e3463
Amit Kumar <91612827+amitkumar1403@users.noreply.github.com>
2025-03-24 17:16:38 +0530

    Upate log request

--- framework/bettercommerce/trade-in/get-quote-by-id.ts
+++ framework/bettercommerce/trade-in/get-quote-by-id.ts
@@ -11,7 +11,6 @@ export default function getQuoteById(id: string, cookies?: any) {
         method: 'GET',
         cookies,
         headers: { DomainId: process.env.NEXT_PUBLIC_DOMAIN_ID },
-logRequest: true,
       })
       return response
     } catch (error: any) {
