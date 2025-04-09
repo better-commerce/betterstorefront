beb6030d2cdbb9caec42adf7042a77ee97518bb2
Gagan <gagandeep.singh@bettercommerce.io>
2025-04-09 11:35:58 +0530

    Company users should return only relevant json properties that are required on FE

--- pages/api/b2b/get-users.ts
+++ pages/api/b2b/get-users.ts
@@ -6,11 +6,32 @@ const getCompanyUsersApiMiddleware = async (req: any, res: any) => {
   const { companyId }: any = req?.body
   try {
     const response = await useB2BCompanyUsers()(companyId, req?.cookies)
-
-    res.status(200).json(response?.result)
+    res.status(200).json(response?.result?.length ? response?.result?.map(getUserDetailTransform) : [])
   } catch (error) {
     apiMiddlewareErrorHandler(req, res, error)
   }
 }
 
 export default apiRouteGuard(getCompanyUsersApiMiddleware)
+
+export const getUserDetailTransform = (user: any) => ({
+  userId: user?.userId,
+  firstName: user?.firstName,
+  lastName: user?.lastName,
+  title: user?.title,
+  phoneNo: user?.phoneNo,
+  gender: user?.gender,
+  telephone: user?.telephone,
+  username: user?.username,
+  email: user?.email,
+  companyId: user?.companyId,
+  address: user?.address,
+  companyUserRole: user?.companyUserRole,
+  notifyByEmail: user?.notifyByEmail,
+  notifyBySMS: user?.notifyBySMS,
+  notifyByPost: user?.notifyByPost,
+  isWelcomeEmailSent: user?.isWelcomeEmailSent,
+  id: user?.id,
+  recordId: user?.recordId,
+  externalStatus: user?.externalStatus
+})
