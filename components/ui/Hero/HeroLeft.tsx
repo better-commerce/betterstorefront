323829c39f65597fd60d27df929b53881045e05f
Amit Kumar <91612827+amitkumar1403@users.noreply.github.com>
2025-04-11 16:42:28 +0530

    Update hero banner auto slide

--- components/ui/Hero/HeroLeft.tsx
+++ components/ui/Hero/HeroLeft.tsx
@@ -33,7 +33,15 @@ const HeroLeft: React.FC<HeroProps> = ({ banners = [], deviceInfo, featureToggle
   return (
     <>
       <h1 className="sr-only">{translate('label.home.HomePageText')}</h1>
-      <Swiper navigation={true} loop={true} className="relative bg-gray-900 mySwiper" >
+      <Swiper
+        navigation={true}
+        loop={true}
+        autoplay={{
+          delay: 4000, // 5000ms = 5 seconds
+          disableOnInteraction: false, // keeps autoplay running after user interaction
+        }}
+        className="relative bg-gray-900 mySwiper"
+      >
         {banners?.sort((a: { displayOrder: number }, b: { displayOrder: number }) => a.displayOrder > b.displayOrder ? 1 : -1).map((banner: any, bid: number) => (
           <SwiperSlide key={bid}>
             <Link href={banner?.link || '#'} className='relative'>
@@ -54,7 +62,6 @@ const HeroLeft: React.FC<HeroProps> = ({ banners = [], deviceInfo, featureToggle
                 ) : (
                   <img src={generateUri(banner?.url, 'h=700&fm=webp') || IMG_PLACEHOLDER} alt={banner?.alt || 'banner-image'} style={css} width={1903} height={700} className={`${featureToggle?.features?.enableForPCSite ? '!object-cover !object-left sm:min-h-[480px]' : 'sm:max-h-screen image banner-Image'}`} />
                 )}
-
               </div>
             </Link>
           </SwiperSlide>
