841ce5c4132600c92337c1fe0a0414a1a32222d0
Amit Kumar <91612827+amitkumar1403@users.noreply.github.com>
2025-04-11 16:48:51 +0530

    Update Header

--- components/Header/CartDropdown.tsx
+++ components/Header/CartDropdown.tsx
@@ -292,7 +292,7 @@ export default function CartDropdown({ featureToggle }: any) {
                 <>
                   <Popover.Button onClick={() => openMiniBasket(cartItems)} className={` ${open ? "" : "text-opacity-90"} ${featureToggle?.features?.enablePCTopHeader ? '' : 'w-10 h-10 sm:w-12 sm:h-12 hover:bg-slate-100 dark:hover:bg-slate-100 '} group rounded-full inline-flex items-center justify-center focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 relative`}>
                     {featureToggle?.features?.enablePCTopHeader ?
-                      <span className="relative flex items-center text-xs font-light text-white hover:underline">Basket <ShoppingCartIcon className="w-4 h-4" />
+                      <span className="relative flex items-center text-xs font-light -top-[2px] text-white hover:underline gap-1">Basket <ShoppingCartIcon className="w-3 h-3" />
                         {basketItemsCount > 0 && (
                           <div className="w-3.5 h-3.5 flex items-center justify-center bg-red-500 absolute top-0 -right-2.5 rounded-full text-[10px] leading-none text-white font-medium">
                             {basketItemsCount}
@@ -307,7 +307,6 @@ export default function CartDropdown({ featureToggle }: any) {
                           </div>
                         )}
                         <span className="sr-only">{translate('label.basket.itemsCartViewBagText')}</span>
-
                         <img alt="" src="/images/cartIcon.svg" className="w-6 h-6" />
                       </>
                     }
