670fcc591cb6b9dfbd5d100ffbd771883660f443
Amit Kumar <91612827+amitkumar1403@users.noreply.github.com>
2025-04-21 11:45:54 +0530

    Update Header console errors

--- components/Header/AvatarDropdown.tsx
+++ components/Header/AvatarDropdown.tsx
@@ -20,7 +20,7 @@ import { getBrowserName } from "@framework/utils/ui-util";
 import useAnalytics from "@components/services/analytics/useAnalytics";
 import { isMobile } from "react-device-detect";
 
-const AvatarDropdown = ({ pluginConfig = [], featureToggle, deviceInfo }:any) => {
+const AvatarDropdown = ({ pluginConfig = [], featureToggle, deviceInfo }: any) => {
   const { recordAnalytics } = useAnalytics()
   const { isDesktop } = deviceInfo
   const translate = useTranslation()
@@ -177,6 +177,8 @@ const AvatarDropdown = ({ pluginConfig = [], featureToggle, deviceInfo }:any) =>
   useEffect(() => {
     if (!isGuestUser && user?.userId) {
       setDisplayText(`${translate('common.label.hiText')}, ${user?.firstName}`);
+    } else {
+      setDisplayText('Sign in. Account & Orders') // Reset the display text
     }
   }, [isGuestUser, user, translate]);
   if (!isGuestUser && user?.userId) {
@@ -188,10 +190,10 @@ const AvatarDropdown = ({ pluginConfig = [], featureToggle, deviceInfo }:any) =>
         {({ open, close }) => (
           <>
             <Popover.Button className={`${featureToggle?.features?.enablePCTopHeader ? 'hover:underline' : 'w-10 h-10 sm:w-12 sm:h-12 hover:bg-slate-100 dark:hover:bg-slate-100'}rounded-full group text-slate-700 dark:text-slate-700 focus:outline-none flex items-center justify-center`}>
-              {featureToggle?.features?.enablePCTopHeader ? 
+              {featureToggle?.features?.enablePCTopHeader ?
                 <span className="flex items-center text-xs font-light text-white text-regular hover:underline">
                   {displayText} <ChevronDownIcon className="w-3 h-3" />
-                </span> 
+                </span>
                 :
                 <img alt="" src="/images/userIcon.svg" className="w-6 h-6 mx-auto group-hover:text-black" />
               }
