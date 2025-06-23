8a3238d388c5b07198500337ca881d9733db687a
Amit Kumar <91612827+amitkumar1403@users.noreply.github.com>
2025-06-23 17:09:15 +0530

    Fix Effective price in used

--- components/Product/RichProductView.tsx
+++ components/Product/RichProductView.tsx
@@ -73,11 +73,12 @@ export default function RichProductView({ product, selectedOption, isGuestUser, 
             )}
             {cashbackAmount && <div className="w-full max-w-3xl my-4 border shadow-sm rounded-xl bg-background">
               <div className="flex items-center gap-3 px-3 py-2 justify-between bg-[#EAEDF5]">
-
                 <h2 className="text-sm font-semibold text-[#1E1E1E]">Effective price
                   <span className="block text-xs italic font-normal text-gray-500">after <strong>{product?.price?.currencySymbol}{cashbackAmount}</strong> cashback and voucher</span>
                 </h2>
-                <span className="ml-2 text-xl font-bold text-red-700">{product?.price?.currencySymbol}{(bestPrice ? bestPrice - cashbackAmount : product?.price?.raw?.withTax - cashbackAmount)?.toFixed(2)}</span>
+                {selectedOption === "used" ? <span className="ml-2 text-xl font-bold text-red-700">{product?.price?.currencySymbol}{(bestPrice ? usedProduct?.at(0)?.price?.raw?.withTax - cashbackAmount : usedProduct?.at(0)?.price?.raw?.withTax - cashbackAmount)?.toFixed(2)}</span> :
+                  <span className="ml-2 text-xl font-bold text-red-700">{product?.price?.currencySymbol}{(bestPrice ? bestPrice - cashbackAmount : product?.price?.raw?.withTax - cashbackAmount)?.toFixed(2)}</span>
+                }
               </div>
 
               <div className="flex w-full gap-2 px-3 py-2 pb-4 mt-2">
@@ -290,7 +291,7 @@ export default function RichProductView({ product, selectedOption, isGuestUser, 
                     <span className={`w-5 h-5 border rounded-full flex items-center justify-center mr-2 ${selectedOption === "used" ? "border-blue-600 active-radio" : "border-gray-400"}`}>
                       {selectedOption === "used" && <span className="w-3 h-3 bg-blue-600 rounded-full"></span>}
                     </span>
-                  </label>                  
+                  </label>
                   {selectedOption === "used" && (
                     <>
                       {featureToggle?.features?.enableStoreStockCheck &&
