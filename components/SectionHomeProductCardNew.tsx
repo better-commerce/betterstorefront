917cb01f287b85b6277b994394c5350d8145de60
Vinay Sharma <vinay.sharma@bettercommerce.io>
2025-06-24 05:11:21 +0000

    Merged PR 34348: 15492_VS_Fix_Slider_Icon Position
    Related work items: #15492

--- components/SectionHomeProductCardNew.tsx
+++ components/SectionHomeProductCardNew.tsx
@@ -2,16 +2,25 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import dynamic from 'next/dynamic'
+import { useRef, useEffect } from 'react'
+import Prev from '@components/shared/NextPrevIcon/Prev'
+import Next from '@components/shared/NextPrevIcon/Next'
 const ProductCard = dynamic(() => import('@components/ProductCard'))
 export default function SectionHomeProductCardNew({ products, productPerColumn, deviceInfo, maxBasketItemsCount, defaultDisplayMembership, featureToggle, onlyImage }: any) {
   const { isMobile } = deviceInfo || { isMobile: false };
+  const swiperRef = useRef<any>(null);
 
   return (
-    <>
+    <div className="relative">
+      <div className="flex justify-between mb-2 slider-out-btn">
+        <Prev onClickPrev={() => swiperRef.current?.swiper?.slidePrev()} />
+        <Next onClickNext={() => swiperRef.current?.swiper?.slideNext()} />
+      </div>
       <Swiper
+        ref={swiperRef}
         slidesPerView={1.4}
         spaceBetween={10}
-        navigation={true}
+        navigation={false}
         loop={true}
         className={isMobile ? 'mob-navigation-hide' : ''}
         breakpoints={{
@@ -27,6 +36,6 @@ export default function SectionHomeProductCardNew({ products, productPerColumn, 
           </SwiperSlide>
         ))}
       </Swiper>
-    </>
+    </div>
   )
 }
