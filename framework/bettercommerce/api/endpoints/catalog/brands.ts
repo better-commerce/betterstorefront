c3468a62073d558afb3540d89f87330ca193cc1e
Amit Kumar <91612827+amitkumar1403@users.noreply.github.com>
2025-07-11 10:43:17 +0530

    Fix Brand page list issue not showing all brands

--- framework/bettercommerce/api/endpoints/catalog/brands.ts
+++ framework/bettercommerce/api/endpoints/catalog/brands.ts
@@ -4,7 +4,7 @@ import { CATALOG_ENDPOINT, EmptyObject } from '@components/utils/constants'
 export default async function getBrands(query: any, cookies = EmptyObject) {
   try {
     const response: any = await fetcher({
-      url: `${CATALOG_ENDPOINT}/all?page=${query.page || 1}&pageSize=200&sortBy=${
+      url: `${CATALOG_ENDPOINT}/all?page=${query.page || 1}&pageSize=500&sortBy=${
         query.sortBy || 'manufacturerName'
       }&sortOrder=${query.sortOrder || 'asc'}&brandIds=${query.brandIds || ''}`,
       method: 'get',
